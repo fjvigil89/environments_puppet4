ec9b9b8b0e22e99f67ffcc67d91af6925318b041
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-04-12 17:42:44 -0400

    Una prueba para el uso de los mdulos

--- manifests/node.pp
+++ manifests/node.pp
@@ -3,6 +3,10 @@ node 'default'{
 }
 node 'client-puppet.upr.edu.cu'{
   #include roles::file
-  include profile::emai
+  #include profile::emai
+  class {'roundcube':
+  imap_host =>  'ssl://localhost',
+  imap_port =>  993,
+  }
 
 }
