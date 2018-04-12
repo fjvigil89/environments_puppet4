8ced2cf3a414c5d261890ca22074a2f31dbd9285
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-04-12 16:38:06 -0400

    prueba

--- manifests/node.pp
+++ manifests/node.pp
@@ -3,5 +3,9 @@ node 'default'{
 }
 node 'client-puppet.upr.edu.cu'{
   #include roles::file
-  #include profile::email
+  #include profile::emai
+  class { 'roundcube':
+      imap_host => l 'ssl://localhost',
+        imap_port =>  993,
+  }
 }
