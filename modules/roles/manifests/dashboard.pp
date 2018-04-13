0a1e321275659688a3be13e238c939977092bfd9
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-04-13 11:19:03 -0400

    pasando dashboard_config a false en la configuraci'on

--- modules/roles/manifests/dashboard.pp
+++ modules/roles/manifests/dashboard.pp
@@ -1,14 +1,15 @@
 class roles::dashboard{
   class {'dashboard':
-  dashboard_ensure =>  'present',
-  dashboard_user =>  'puppet-dbuser',
-  dashboard_group =>  'puppet-dbgroup',
-  dashboard_password =>  'changeme',
-  dashboard_db =>  'dashboard_prod',
-  dashboard_charset =>  'utf8',
-  dashboard_site =>  $fqdn,
-  dashboard_port =>  '8080',
-  mysql_root_pw =>  '123',
-  passenger =>  true,
+  dashboard_ensure   => 'present',
+  dashboard_user     => 'puppet-dbuser',
+  dashboard_group    => 'puppet-dbgroup',
+  dashboard_password => 'changeme',
+  dashboard_db       => 'dashboard_prod',
+  dashboard_charset  => 'utf8',
+  dashboard_site     => $fqdn,
+  dashboard_port     => '8080',
+  mysql_root_pw      => '123',
+  dashboard_config   => false
+  passenger          => true,
   }
 }
