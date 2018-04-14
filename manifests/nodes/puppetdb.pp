cb089f8f876f55a2c28332cc9fa713d3a3835c35
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-04-14 17:52:05 -0400

    Modificando

--- manifests/nodes/puppetdb.pp
+++ manifests/nodes/puppetdb.pp
@@ -1,10 +1,10 @@
 node 'puppetdb.upr.edu.cu' {
   #include puppetdb_server
   # include ntp
-  class puppetdb_server {
+  #class puppetdb_server {
       class { 'puppetdb':
         listen_address =>  '0.0.0.0',
         manage_firewall =>  true,
       }
-  }
+      # }
 }
