d955ba40c435f2f430118a2ddd8629a37e7eafe7
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-04-17 14:16:09 -0400

    Modificando errores

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
 node default {
     
   notify{'default_node_inclusion':
     message =>  'Este nodo no tiene catalogos',
   }
 }
-
-=======
-
-node default {
-
-  notify{'default_node_inclusion':
-    message =>  'Upss !!! - El Nodo por defecto no ha sido asignado - Porfavor contacte con su administrador',
-  }
-
-}
->>>>>>> f267099546ef27452d83a0c0089a86e8300691f4
