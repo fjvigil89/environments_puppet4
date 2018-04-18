8f35700de90a29c2c4fcde442949280ecbae3947
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-04-18 13:14:31 -0400

    viendo errores de class

--- manifests/nodes/puppet_host.pp
+++ manifests/nodes/puppet_host.pp
@@ -1,3 +1,9 @@
 node 'client-puppet.upr.edu.cu'{
   #include ::puppetdevserver
+  class { '::basesys':
+        uprinfo_usage =>  'Puppet dev server',
+            application =>  'puppetserver',
+                application_tier =>  'dev',
+                    puppet_enabled =>  false;
+                    }
 }
