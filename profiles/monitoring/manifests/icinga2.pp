eb09d57e06d1a9889c5dedb7cebb6311bc29c769
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-04-19 12:41:10 -0400

    M

--- profiles/monitoring/manifests/icinga2.pp
+++ profiles/monitoring/manifests/icinga2.pp
@@ -16,54 +16,54 @@ class monitoring::icinga2 (
 
 
 # Create a DB icinga2 (IDO)
-include ::mysql_server
-mysql::sql {
-  user     =>  $monitoring::icinga2::icinga2_dbuser,
-  password =>  $monitoring::icinga2::icinga2_dbpass,
-  host     =>  $monitoring::icinga2::icinga2_dbhost,
-  grant    =>   ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'DROP', 'CREATE VIEW', 'CREATE', 'INDEX', 'EXECUTE', 'ALTER'],
-}
+#include ::mysql_server
+#mysql::sql {
+#  user     =>  $monitoring::icinga2::icinga2_dbuser,
+#  password =>  $monitoring::icinga2::icinga2_dbpass,
+#  host     =>  $monitoring::icinga2::icinga2_dbhost,
+#  grant    =>   ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'DROP', 'CREATE VIEW', 'CREATE', 'INDEX', 'EXECUTE', 'ALTER'],
+#}
 
 # Configure ido_mysql
-class {'icinga2::feature::idomysql':
-  user          => $monitoring::icinga2::icinga2_dbuser,
-  password      => $monitoring::icinga2::icinga2_dbpass,
-  host          => $monitoring::icinga2::icinga2_dbhost,
-  database      => $monitoring::icinga2::icinga2_dbname,
-  import_schema => true,
-  require       => Mysql::Db[$monitoring::icinga2::icinga2_dbname],
-}
+#class { 'icinga2::feature::idomysql':
+#  user          => $monitoring::icinga2::icinga2_dbuser,
+#  password      => $monitoring::icinga2::icinga2_dbpass,
+#  host          => $monitoring::icinga2::icinga2_dbhost,
+#  database      => $monitoring::icinga2::icinga2_dbname,
+#  import_schema => true,
+#  require       => Mysql::Db[$monitoring::icinga2::icinga2_dbname],
+#}
 
-class { '::icinga2':
-  confd       =>  false,
-  manage_repo =>  true,
-  features    =>  ['checker','mainlog','notification','statusdata','compatlog','command'],
-  constants   =>  {
-   'ZoneName' =>  'master',
-   'NodeName' =>  '${fqdn}',
-   ticketsalt =>  '5a3d695b8aef8f18452fc494593056a4',
-  },
-}
+#class { '::icinga2':
+#  confd       =>  false,
+#  manage_repo =>  true,
+#  features    =>  ['checker','mainlog','notification','statusdata','compatlog','command'],
+#  constants   =>  {
+#   'ZoneName' =>  'master',
+#   'NodeName' =>  '${fqdn}',
+#   ticketsalt =>  '5a3d695b8aef8f18452fc494593056a4',
+#  },
+#}
 
-class { '::icinga2::feature::api':
- accept_commands => true,
- accept_config   => true,
- endpoints       => {
-  '${fqdn}' => {
-    'host' =>  '${ipaddress}',
-  }
-},
-zones           => {
-  'master' =>  {
-    'endpoints' =>  ['${fqdn}'],
-  }
- }
-}
+#class { '::icinga2::feature::api':
+# accept_commands => true,
+# accept_config   => true,
+# endpoints       => {
+#  '${fqdn}' => {
+#    'host' =>  '${ipaddress}',
+#  }
+#},
+#zones           => {
+#  'master' =>  {
+#    'endpoints' =>  ['${fqdn}'],
+#  }
+# }
+#}
 
-icinga2::object::zone {'global-templates':
-global =>  true,
-}
-icinga2::object::zone {'director-global':
-global =>  true,
-}
+#icinga2::object::zone { 'global-templates':
+#  global =>  true,
+#}
+#icinga2::object::zone { 'director-global':
+#  global =>  true,
+#}
 }
