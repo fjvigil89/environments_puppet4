5a52ce57e838677be42268bc69f94b74bfa87e7d
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-04-19 15:26:32 -0400

    M

--- profiles/monitoring/manifests/icinga2.pp
+++ profiles/monitoring/manifests/icinga2.pp
@@ -31,7 +31,7 @@ class { '::icinga2':
   features      =>   ['checker','mainlog','notification','statusdata','compatlog','command'],
   constants     =>   {
     'ZoneName' =>   'master',
-    'NodeName' =>   '${facts['fqdn']}',
+    'NodeName' =>   ${facts['fqdn']},
     ticketsalt =>   '5a3d695b8aef8f18452fc494593056a4',
   },
 }
