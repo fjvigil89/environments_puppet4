b05166fc1c420593503e284375ada26591b8f995
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-04-20 13:37:16 -0400

    M

--- profiles/puppetdb_server/manifests/init.pp
+++ profiles/puppetdb_server/manifests/init.pp
@@ -7,8 +7,7 @@ class puppetdb_server {
   class { 'puppetdb':
     listen_address    => '0.0.0.0',
     listen_port       => '8001',
-    disable_ssl       => true,
-    #open_listen_port => true,
+    disable_ssl       => false,
   }
   #class {'puppetdb::master::config': }
 
