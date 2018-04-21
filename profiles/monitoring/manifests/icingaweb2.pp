410f3568c0b7450169fcf4fecd610e509fda7af5
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-04-21 12:16:03 -0400

    M

--- profiles/monitoring/manifests/icingaweb2.pp
+++ profiles/monitoring/manifests/icingaweb2.pp
@@ -177,13 +177,13 @@ apache::vhost { 'icingaweb.upr.edu.cu':
   docroot         => '/usr/share/icingaweb2/public',
   redirect_status => 'permanent',
   redirect_dest   => "http://icingaweb.upr.edu.cu",
+}
 
 file_line{ 'date.timezone':
   path   => '/etc/php5/apache2/php.ini',
   line   => 'date.timezone = America/Havana',
   match  => '^date.timezone =',
   notify =>  Class['apache'],
-}
 }
 }
 
