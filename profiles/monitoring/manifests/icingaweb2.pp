d37ad3553474864545d10eae31c4545e35564388
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-04-24 15:14:59 -0400

    m

--- profiles/monitoring/manifests/icingaweb2.pp
+++ profiles/monitoring/manifests/icingaweb2.pp
@@ -154,32 +154,32 @@ exec { "Generate combined .pem file for ${puppetdb_host}":
 
 include ::apache
 
-class { '::php::globals':
-  php_version =>  '7.0',
-  config_root =>  '/etc/php/7.0',
-}
-class { '::php':
-  manage_repos =>  true
-}
+#class { '::php::globals':
+#  php_version =>  '7.0',
+#  config_root =>  '/etc/php/7.0',
+#}
+#class { '::php':
+#  manage_repos =>  true
+#}
 
 #include ::apache::mod::php
-class roles::webserver::service::website () {
+#class roles::webserver::service::website () {
 
-include roles::webserver::apache
-apache::vhost { 'icingaweb.upr.edu.cu':
-  servername      => 'icingaweb.upr.edu.cu',
-  port            => '80',
-  docroot         => '/usr/share/icingaweb2/public',
-  redirect_status => 'permanent',
-  redirect_dest   => "http://icingaweb.upr.edu.cu",
-}
-}
-file_line{ 'date.timezone':
-  path   => '/etc/php5/apache2/php.ini',
-  line   => 'date.timezone = America/Havana',
-  match  => '^date.timezone =',
-  notify =>  Class['apache'],
-}
+#include roles::webserver::apache
+#apache::vhost { 'icingaweb.upr.edu.cu':
+#  servername      => 'icingaweb.upr.edu.cu',
+#  port            => '80',
+#  docroot         => '/usr/share/icingaweb2/public',
+#  redirect_status => 'permanent',
+#  redirect_dest   => "http://icingaweb.upr.edu.cu",
+#}
+#}
+#file_line{ 'date.timezone':
+#  path   => '/etc/php5/apache2/php.ini',
+#  line   => 'date.timezone = America/Havana',
+#  match  => '^date.timezone =',
+#  notify =>  Class['apache'],
+#}
 }
 
 
