7ea54674ac60dde63371870d9d4a877666f8c967
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-04-24 18:01:01 -0400

    m

--- profiles/monitoring/manifests/icingaweb2.pp
+++ profiles/monitoring/manifests/icingaweb2.pp
@@ -158,10 +158,10 @@ include ::apache
 
 # Install and define php-fpm
 include phpfpm
-class { '::apache::mod::prefork': }
+#class { '::apache::mod::prefork': }
 class { '::apache::mod::php': }
 class { '::apache':
-  mpm_module      => false,
+  mpm_module      => 'worker',
   confd_dir       => '/etc/apache2/conf-enabled',
   purge_configs   => false,
   purge_vhost_dir => true,
