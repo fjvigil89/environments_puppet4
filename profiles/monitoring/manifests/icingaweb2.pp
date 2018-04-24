479de07ffb5eda9c6bc080c40c5a30a8aa2934d3
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-04-24 17:24:16 -0400

    D

--- profiles/monitoring/manifests/icingaweb2.pp
+++ profiles/monitoring/manifests/icingaweb2.pp
@@ -159,17 +159,11 @@ include ::apache
 # Install and define php-fpm
 include phpfpm
 
-#class { 'apache::mod::php': }
-# Apache virtual Host
-#include ::apache::mod::php
-#class roles::webserver::service::website () {
-#include ::roles::webserver::apache
 apache::vhost { 'icingaweb.upr.edu.cu':
   servername  => 'icingaweb.upr.edu.cu',
   port        => '80',
   docroot     => '/usr/share/icingaweb2/public',
 }
-#}
 # Define TimeZone in php
 file_line { 'date.timezone':
   path   => '/etc/php/7.0/apache2/php.ini',
