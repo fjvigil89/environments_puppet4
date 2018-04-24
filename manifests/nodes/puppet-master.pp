7430f1d0e246d522bbec6e86daa014eca21c53c3
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-04-24 11:49:28 -0400

    M

--- manifests/nodes/puppet-master.pp
+++ manifests/nodes/puppet-master.pp
@@ -1,8 +1,6 @@
 $puppetdb_host = 'puppetdb.upr.edu.cu'
 node 'puppet-master.upr.edu.cu' {
-  # class {'puppetdb::master::config':
-  #puppetdb_server      => $puppetdb_host,
-  #puppetdb_port        => 8081,
-  #puppetdb_disable_ssl => true,
-  # }
+  class {'puppetdb::master::config':
+  puppetdb_server => $puppetdb_host,
+  }
 }
