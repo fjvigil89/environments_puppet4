f5053b19381dbe639ad9e5c3614dfaa93202470c
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-05-02 15:09:02 -0400

    change puppetdb environment

--- manifests/nodes/puppet-master.pp
+++ manifests/nodes/puppet-master.pp
@@ -1,6 +1,8 @@
 #$puppetdb_host = 'puppetdb.upr.edu.cu'
 node 'puppet-master.upr.edu.cu' {
-  class { 'puppetdb': }
+  class { 'puppetdb': 
+   listen_address   =>  '0.0.0.0',
+  }
     class { 'puppetdb::master::config': }
   #class {'puppetdb::master::config':
   #puppetdb_server => $puppetdb_host,
