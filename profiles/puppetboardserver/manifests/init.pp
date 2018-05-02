32190eeb4205c08b5638314f58e7d739cd525285
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-02 17:51:22 -0400

    Update puppetboard config and configure apache

--- profiles/puppetboardserver/manifests/init.pp
+++ profiles/puppetboardserver/manifests/init.pp
@@ -9,23 +9,47 @@ class puppetboardserver(
 ) {
 
   # Add configuration below
+	class {'apache':
+     default_vhost    => false,
+     server_signature => 'Off',
+     server_tokens    => 'Prod',
+     trace_enable     => 'Off',
+   }
 
   # Configure puppetboard
-  class { '::puppetboard':
-    puppetdb_host     => $puppetdb_host,
-    puppetdb_port     => '8080',
-    manage_git        => true,
-    manage_virtualenv => true,
-    enable_catalog    => true,
-    revision          => '35486e8',
+#  class { '::puppetboard':
+#    puppetdb_host     => $puppetdb_host,
+#    puppetdb_port     => '8080',
+#    manage_git        => true,
+#    manage_virtualenv => true,
+#    enable_catalog    => true,
+#    revision          => '35486e8',
+#  }
+
+ 	class { 'apache::mod::wsgi':
+     wsgi_socket_prefix => '/var/run/wsgi',
+   }
+
+   #Configure Puppetboard
+   class { 'puppetboard':
+     puppetdb_host       => $puppetdb_host,
+     puppetdb_port       => '8080',
+     default_environment => '*',
+     manage_git          => 'latest',
+     manage_virtualenv   => 'latest',
+     reports_count       => 50
+   }->
+  python::pip { 'Flask':
+    virtualenv => '/srv/puppetboard/virtenv-puppetboard',
+  }->
+  python::pip { 'Flask-WTF':
+    virtualenv => '/srv/puppetboard/virtenv-puppetboard',
+  }->
+  python::pip { 'WTForms':
+    virtualenv => '/srv/puppetboard/virtenv-puppetboard',
+  }->
+  python::pip { 'pypuppetdb':
+    virtualenv => '/srv/puppetboard/virtenv-puppetboard',
   }
 
-  # Access Puppetboard through pboard.example.com
-  #  class { 'puppetboard::apache::vhost':
-  #  vhost_name  => 'pboard.upr.edu.cu',
-  #  port        => '80',
-  #}
-
-  # Access Puppetboard through 
-  class { '::puppetboardserver::apache':;}
 }
