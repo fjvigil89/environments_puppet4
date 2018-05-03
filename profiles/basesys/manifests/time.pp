714c6b26cdf4b22143a1429d5a3e4600cf3f83cb
Desarrollo Team <r01td00@gmail.com>
2018-05-03 13:00:16 -0400

    Uso de sustem base en host de prueba

--- profiles/basesys/manifests/time.pp
+++ profiles/basesys/manifests/time.pp
@@ -7,17 +7,17 @@ class basesys::time {
   if($::basesys::time_enabled) {
     class {
       '::ntp':
-        servers         => $basesys::ntp_server,
-        config_template => $basesys::ntpconf,
+        servers         => $basesys::params::ntp_server,
+        config_template => $basesys::params::ntpconf,
     }
     # NOTE: NTPd start niet na boot voor RHEL7
     #       https://access.redhat.com/solutions/1315793
     #
     #       Om het eenvoudig te houden verwijderen we
     #       chrony op alle systemen.
-    #package {
-    #  'chrony':
-    #    ensure => absent;
-    #}
+    package {
+      'chrony':
+        ensure => absent;
+    }
   }
 }
