00a11727405b47d0630f5a336d21792c05fff564
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-05-03 14:08:50 -0400

    update director module

--- profiles/monitoring/manifests/icingaweb2.pp
+++ profiles/monitoring/manifests/icingaweb2.pp
@@ -157,9 +157,9 @@ class {'icingaweb2::module::monitoring':
 include ::icingaweb2::module::puppetdb
 $puppetdb_host    = 'puppetdb.upr.edu.cu'
 $my_certname      =  $facts['fqdn']
-#$ssldir           = '/etc/puppetlabs/puppet/ssl'
+$ssldir           = '/etc/puppetlabs/puppet/ssl'
 #$web_ssldir       = '/etc/icingaweb2/modules/puppetdb/ssl'
-#$ssl_subdir       = "{web_ssldir}/${puppetdb_host}"
+$ssl_subdir       = "{web_ssldir}/${puppetdb_host}"
 #$private_keys_dir = '/etc/icingaweb2/modules/puppetdb/ssl/puppetdb.upr.edu.cu/private_keys'
 #$certs_dir        = '/etc/icingaweb2/modules/puppetdb/ssl/puppetdb.upr.edu.cu/certs'
 
