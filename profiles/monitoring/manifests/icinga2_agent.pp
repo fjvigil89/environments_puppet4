1d73d4f6db8ff8d07849c3d4d3bae4c09bebca6c
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-05-07 16:48:30 -0400

    arreglando error en icinga2_agent

--- profiles/monitoring/manifests/icinga2_agent.pp
+++ profiles/monitoring/manifests/icinga2_agent.pp
@@ -21,17 +21,17 @@ class monitoring::icinga2_agent(
  icinga2::object::zone { 'director-global':
   global => true,
  }
- class { '::icinga2::feature::api':
-  pki             => 'icinga2',
-  #ticket_salt     => '5a3d695b8aef8f18452fc494593056a4',
-  accept_config   => true,
-  accept_commands => true,
-  zones           => {
-     'NodeName'   => {
-      'endpoints' => ['NodeName'],
-      'parent'    =>   'master',
-    },
-    'master' =>   {}
-  }
- }
+ # class { '::icinga2::feature::api':
+ # pki             => 'icinga2',
+ # #ticket_salt     => '5a3d695b8aef8f18452fc494593056a4',
+ # accept_config   => true,
+ # accept_commands => true,
+ # zones           => {
+ #    'NodeName'   => {
+ #     'endpoints' => ['NodeName'],
+ #     'parent'    =>   'master',
+ #   },
+ #   'master' =>   {}
+ # }
+ #}
 }
