65adcb200daba2396ab4d4d77ed1a9b0bca20fe6
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-09 17:39:15 -0400

    instalando el gitlab test 33

--- manifests/nodes/gitlab.pp
+++ manifests/nodes/gitlab.pp
@@ -1,11 +1,11 @@
 node 'gitlab.upr.edu.cu'{
 	#include gitlab
 
-	class { '::basesys':
-        	 uprinfo_usage  => 'servidor gitlab',
-        	 application      => 'puppet',
-        	 puppet_enabled   => false;
-        }
+	#class { '::basesys':
+       # 	 uprinfo_usage  => 'servidor gitlab',
+       # 	 application      => 'puppet',
+       # 	 puppet_enabled   => false;
+       # }
 
         include gitlabserver
 }
