8f049caf17dd2754b4cc0608609da4575d3054f7
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-09 17:14:59 -0400

    instalando el gitlab test 72

--- profiles/gitlabserver/manifests/init.pp
+++ profiles/gitlabserver/manifests/init.pp
@@ -1,45 +1,29 @@
-class gitlabserver (
-	String $url_externa 			= $::gitlabserver::params::url_externa,
-	Boolean $nginx_redirect 		= true,
-	Boolean $ldap_server			= true,
-	String $label				= $::gitlabserver::params::label,
-	String $host                   		= $::gitlabserver::params::host,
-	$port                   		= $::gitlabserver::params::port,
-	String $uid                    		= $::gitlabserver::params::uid,
-	String $method                 		= $::gitlabserver::params::method,	
-	String $bind_dn                		= $::gitlabserver::params::bind_dn,
-        String $password               		= $::gitlabserver::params::password,
-        $active_directory       		= true,
-        $allow_username_or_email_login 		= false,
-        $block_auto_created_users 		= false,
-        String $base                           	= $::gitlabserver::params::base,
-
-)inherits ::gitlabserver::params{
+class gitlabserver{
 
 	class { 'gitlab':
-	  external_url => $gitlabserver::url_externa,
+	  external_url => 'http://gitlab.upr.edu.cu',
 	  nginx        => {
 		 #ssl_certificate     => '/etc/gitlab/ssl/gitlab.example.com.crt',
 		 #ssl_certificate_key => '/etc/gitlab/ssl/gitlab.example.com.key'
-		 redirect_http_to_https	=> $gitlabserver::nginx_redirect
+		 redirect_http_to_https	=> yes
 		  },
 	  gitlab_rails => {
    		 'webhook_timeout' => 10,
     		 'gitlab_default_theme' => 2,
-		 'ldap_enabled'	=> $gitlabserver::ldap_server,
+		 'ldap_enabled'	=> yes,
 		 'ldap_servers' => { 
 		 'main'  => {
-		      label				=> $gitlabserver::label,
-		      host				=> $gitlabserver::host,
-		      port				=> $gitlabserver::port,
-		      uid				=> $gitlabserver::uid,
-		      method				=> $gitlabserver::method,
-		      bind_dn				=> $gitlabserver::bind_dn,
-		      password				=> $gitlabserver::password,
-		      active_directory			=> $gitlabserver::active_directory,
-		      allow_username_or_email_login	=> $gitlabserver::allow_username_or_email_login,
-		      block_auto_created_users		=> $gitlabserver::block_auto_created_users,
-		      base				=> $gitlabserver::base,
+		      label				=> 'LDAP',
+		      host				=> 'ad.upr.edu.cu',
+		      port				=> 389,
+		      uid				=> 'sAMAccountName',
+		      method				=> 'plain',
+		      bind_dn				=> 'CN=git,OU=_Servicios,DC=upr,DC=edu,DC=cu',
+		      password				=> 'mistake*tig.20',
+		      active_directory			=> true,
+		      allow_username_or_email_login	=> false,
+		      block_auto_created_users		=> false,
+		      base				=> 'DC=upr,DC=edu,DC=cu',
 		      #group_base			=> 'MYGROUPBASE',
 		      #user_filter			=> '',				
 			}
--- roles/gitlab/manifests/init.pp
+++ roles/gitlab/manifests/init.pp
@@ -5,15 +5,5 @@ class gitlab {
     	 puppet_enabled   => false;
   	}
 
-	class{ '::gitlabserver':
-	url_externa  => 'http://gitlab.upr.edu.cu',
-        label       => 'LDAP',
-        host       => 'ad.upr.edu.cu',
-        port       => 389,
-        uid        => 'sAMAccountName',
-        method     => 'plain',
-        bind_dn    => 'CN=git,OU=_Servicios,DC=upr,DC=edu,DC=cu',
-        password   => 'mistake*tig.20',
-        base       => 'DC=upr,DC=edu,DC=cu',
-	}
+	inclued gitlabserver
 }
