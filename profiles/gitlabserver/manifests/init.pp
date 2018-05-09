c41bba6a35b012afef6e63ed3b961ba1895206cf
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-09 09:19:16 -0400

    Creando el modulo de gitlab en puppet

--- profiles/gitlabserver/manifests/init.pp
+++ profiles/gitlabserver/manifests/init.pp
@@ -1,7 +1,16 @@
-class gitlabserver {
+class gitlabserver (
+$url_externa = 'http://localhost'
+){
 
 	class { 'gitlab':
-	  external_url => 'http://gitlab.upr.edu.cu',
+	  external_url => $url_externa,
+	  gitlab_rails => {
+   		 'webhook_timeout' => 10,
+    		 'gitlab_default_theme' => 2,
+		  },
+	  logging      => {
+		 'svlogd_size' => '200 * 1024 * 1024',
+		  },
 	}
 
 }
