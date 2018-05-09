e8b654355d6e91661a896fa66a9683dfdf73ea41
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-09 09:42:42 -0400

    Creando el modulo de gitlab en puppet

--- profiles/gitlabserver/manifests/init.pp
+++ profiles/gitlabserver/manifests/init.pp
@@ -7,10 +7,28 @@ $url_externa = 'http://localhost'
 	  gitlab_rails => {
    		 'webhook_timeout' => 10,
     		 'gitlab_default_theme' => 2,
+		 'ldap_enabled'	=> true,
+		 'ldap_servers' => { 'main':
+		      label				=> 'LDAP',
+		      host				=> 'ad.upr.edu.cu',
+		      port				=> 636,  #389
+		      uid				=> 'sAMAccountName',
+		      method				=> 'ssl', # "tls" or "ssl" or "plain"
+		      bind_dn				=> 'CN=git,OU=_Servicios,DC=upr,DC=edu,DC=cu',
+		      password				=> 'mistake*tig.20',
+		      active_directory			=> true,
+		      allow_username_or_email_login	=> false,
+		      block_auto_created_users		=> false,
+		      base				=> 'DC=upr,DC=edu,DC=cu',
+		      #group_base			=> 'MYGROUPBASE',
+		      user_filter			=> '',				
+			
+			},
 		  },
 	  logging      => {
 		 'svlogd_size' => '200 * 1024 * 1024',
 		  },
+		
 	}
 
 }
