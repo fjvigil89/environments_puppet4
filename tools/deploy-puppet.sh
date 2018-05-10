210cdd4530edb26d7bbfd52790e7d1c00aac482c
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-10 12:39:30 -0400

    Cambios para ssh para puppet

--- tools/deploy-puppet.sh
+++ tools/deploy-puppet.sh
@@ -2,4 +2,4 @@
 
 ENV=${1:-production}
 echo Deploying $ENV
-ssh root@puppet-master.upr.edu.cu:/opt/puppetlabs/puppet/bin/r10k deploy environment ${ENV} -p -v
+ssh -t root@puppet-master.upr.edu.cu"cd /opt/puppetlabs/puppet/bin/r10k deploy environment ${ENV} -p -v"
