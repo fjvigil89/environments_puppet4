ab271b283ea35b013abba23b2d55f9bbe07a30ad
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-05-10 11:59:20 -0400

    agregando git a gitlab

--- roles/gitlab/manifests/init.pp
+++ roles/gitlab/manifests/init.pp
@@ -1,9 +1,9 @@
 class gitlab {
 	class { '::basesys':
-    	 uprinfo_usage  => 'servidor gitlab',
-   	 application      => 'puppet',
-    	 puppet_enabled   => false;
-  	}
-
+    uprinfo_usage  => 'servidor gitlab',
+    application    => 'puppet',
+    puppet_enabled => false;
+  }
 	include gitlabserver
+  include git
 }
