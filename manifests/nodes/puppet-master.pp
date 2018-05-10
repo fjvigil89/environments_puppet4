13be5b5cb15b2b464053cae2a2b1a38f34c79370
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-05-10 14:12:37 -0400

    Add puppet-lint package to puppet-master

--- manifests/nodes/puppet-master.pp
+++ manifests/nodes/puppet-master.pp
@@ -1,9 +1,15 @@
 node 'puppet-master.upr.edu.cu' {
 
 class { 'puppetdb': 
-   listen_address   =>  '0.0.0.0',
-  }
+  listen_address   =>  '0.0.0.0',
+}
   class { 'puppetdb::master::config': }
-
+  
   include puppetboardserver
+  
+ # To install puppet-lint
+  package { 'puppet-lint':
+  ensure   => '1.1.0',
+  provider => 'gem',
+  }
 }
