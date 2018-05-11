fb2451d03ae4192bb8b6e744112d2dc3bafa7241
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-11 14:37:03 -0400

    Instalando pofixt en ubuntu y arreglando zona horaria para todos las familias SO

--- profiles/puppetboardserver/manifests/init.pp
+++ profiles/puppetboardserver/manifests/init.pp
@@ -7,36 +7,36 @@ class puppetboardserver(
   String $puppetdb_host      = 'localhost',
 ) {
 
-  # Add configuration below
-	class {'apache':
-		purge_configs => false,
-  	mpm_module    => 'prefork',
-  	default_vhost => true,
-  	default_mods  => false,
-   }
+#Add configuration below
+  class {'apache':
+  purge_configs => false,
+  mpm_module    => 'prefork',
+  default_vhost => true,
+  default_mods  => false,
+  }
 
-  # Configure puppetboard
-#  class { '::puppetboard':
-#    puppetdb_host     => $puppetdb_host,
-#    puppetdb_port     => '8080',
-#    manage_git        => true,
-#    manage_virtualenv => true,
-#    enable_catalog    => true,
-#    revision          => '35486e8',
-#  }
+# Configure puppetboard
+# class { '::puppetboard':
+# puppetdb_host     => $puppetdb_host,
+# puppetdb_port     => '8080',
+# manage_git        => true,
+# manage_virtualenv => true,
+# enable_catalog    => true,
+# revision          => '35486e8',
+# }
 
- 	class { 'apache::mod::wsgi':
-     wsgi_socket_prefix => '/var/run/wsgi',
-   }
+  class { 'apache::mod::wsgi':
+  wsgi_socket_prefix => '/var/run/wsgi',
+  }
 
-   #Configure Puppetboard
-   class { 'puppetboard':
-     puppetdb_host       => $puppetdb_host,
-     puppetdb_port       => 8080,
-     default_environment => '*',
-     manage_git          => true,
-     manage_virtualenv   => true,
-     reports_count       => 50
-   }
-   class { 'puppetboard::apache::conf': }
+# Configure Puppetboard
+  class { 'puppetboard':
+  puppetdb_host       => $puppetdb_host,
+  puppetdb_port       => 8080,
+  default_environment => '*',
+  manage_git          => true,
+  manage_virtualenv   => true,
+  reports_count       => 50
+  }
+  class { 'puppetboard::apache::conf': }
 }
