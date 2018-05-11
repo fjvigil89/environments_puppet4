15041002f4398ecfbea25d12e2e6c51938dcbb84
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-05-11 18:52:52 -0400

    modifying checks

--- profiles/monitoring/manifests/checks.pp
+++ profiles/monitoring/manifests/checks.pp
@@ -16,32 +16,32 @@ class monitoring::checks {
   # Este archivo se generó al ejecutar configure y make.
   # El archivo generado ./plugins-scripts/check_mysql_health luego se convirtió aquí
   # incluido en Puppet.
-  file { "${plugin_dir}/check_mysql_health":
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/monitoring/checks/check_mysql_health',
-  }
+  #file { "${plugin_dir}/check_mysql_health":
+  #  ensure => file,
+  #  owner  => 'root',
+  #  group  => 'root',
+  #  mode   => '0755',
+  #  source => 'puppet:///modules/monitoring/checks/check_mysql_health',
+  #}
 
   # Laatste versie te vinden op https://github.com/mcrauwel/go-check-orchestrator/releases/latest/
-  file { "${plugin_dir}/check_orchestrator":
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/monitoring/checks/check_orchestrator',
-  }
+  #file { "${plugin_dir}/check_orchestrator":
+  #  ensure => file,
+  #  owner  => 'root',
+  #  group  => 'root',
+  #  mode   => '0755',
+  #  source => 'puppet:///modules/monitoring/checks/check_orchestrator',
+  #}
 
   # Laatste versie te vinden op https://github.com/aswen/nagios-plugins/blob/master/check_puppet_agent
   # Heeft sudo nodig
-  file { "${plugin_dir}/check_puppet_agent_2018":
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/monitoring/checks/check_puppet_agent_2018',
-  }
+  #file { "${plugin_dir}/check_puppet_agent_2018":
+  #  ensure => file,
+  #  owner  => 'root',
+  #  group  => 'root',
+  #  mode   => '0755',
+  #  source => 'puppet:///modules/monitoring/checks/check_puppet_agent_2018',
+  #}
 
   # https://www.thomas-krenn.com/en/wiki/IPMI_Sensor_Monitoring_Plugin
   #$ipc_run_package = $::osfamily ? {
@@ -58,21 +58,21 @@ class monitoring::checks {
   #}
 
   # installeer Nagios plugin check_pv
-  file { "${plugin_dir}/check_pv":
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/monitoring/checks/check_pv',
+  #file { "${plugin_dir}/check_pv":
+  #  ensure => file,
+  #  owner  => 'root',
+  #  group  => 'root',
+  #  mode   => '0755',
+  #  source => 'puppet:///modules/monitoring/checks/check_pv',
   }
 
-  file { "${plugin_dir}/spectre-meltdown-checker.sh":
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/monitoring/checks/spectre-meltdown-checker.sh',
-  }
+  #file { "${plugin_dir}/spectre-meltdown-checker.sh":
+  #  ensure => file,
+  #  owner  => 'root',
+  #  group  => 'root',
+  #  mode   => '0755',
+  #  source => 'puppet:///modules/monitoring/checks/spectre-meltdown-checker.sh',
+  #}
 
   $packages = [ $ipc_run_package, ]
   ensure_packages ($packages)
