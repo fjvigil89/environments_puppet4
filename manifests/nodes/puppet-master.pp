97e01ca281a5423a999649993493f8ee9a11b27f
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-05-11 14:01:10 -0400

    modifying lint puppet-master

--- manifests/nodes/puppet-master.pp
+++ manifests/nodes/puppet-master.pp
@@ -1,8 +1,8 @@
 node 'puppet-master.upr.edu.cu' {
- class {'puppetdb': 
-  listen_address => '0.0.0.0',
- }
- class { 'puppetdb::master::config': }
+  class {'puppetdb':
+    listen_address => '0.0.0.0',
+  }
+  class { 'puppetdb::master::config': }
 
   include puppetboardserver
 }
