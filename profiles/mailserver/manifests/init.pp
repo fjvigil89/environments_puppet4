229e72ecfb066ec2f3d10ff3e5e8a6bf8e148d37
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-11 16:18:31 -0400

    Instalando pofixt en ubuntu y arreglando zona horaria para todos las familias SO

--- profiles/mailserver/manifests/init.pp
+++ profiles/mailserver/manifests/init.pp
@@ -34,14 +34,15 @@ class mailserver () {
   class { 'postfix':
     config_file_template => "postfix/${::operatingsystem}/etc/postfix/main.cf.erb",
     config_file_hash     => {
-      'postfix' => {
-        config_file_path   => '/etc/postfix',
-        config_file_string => "${::fqdn}\n",
-        package_ensure     => 'latest',
-        config_dir_purge   => true,
-        
-      },
+    'postfix' => {
+      config_file_path   => '/etc/postfix',
+      config_file_string => "${::fqdn}\n",
     },
+  },
+    package_ensure   => 'latest',
+    config_dir_purge => true,
+    mydestination    => 'localhost',
+    relayhost        => 'mx-externo.upr.edu.cu:25',
   }
 }
 
