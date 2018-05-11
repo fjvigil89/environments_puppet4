ebe854fa699f4b2d82a349e7003c31de6b2f9184
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-05-11 16:37:11 -0400

    modifying icinga2_agent

--- profiles/monitoring/manifests/icinga2_agent.pp
+++ profiles/monitoring/manifests/icinga2_agent.pp
@@ -11,6 +11,7 @@ class monitoring::icinga2_agent(
 
  class {'::icinga2':
   manage_repo => $manage_repo,
+  features    =>  ['checker','mainlog'],
  }
 
  include ::icinga2::feature::api
@@ -25,7 +26,6 @@ class monitoring::icinga2_agent(
  include ::monitoring::checks
  class { '::icinga2::feature::api':
   pki             => 'icinga2',
- # #ticket_salt     => '5a3d695b8aef8f18452fc494593056a4',
   accept_config   => true,
   accept_commands => true,
   zones           => {
