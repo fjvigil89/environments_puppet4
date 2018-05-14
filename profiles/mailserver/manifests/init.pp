c5c83cece996c4bc71c3d37d464c0775ca87dd68
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-14 13:26:57 -0400

    configurando pofixt con ficheros

--- profiles/mailserver/manifests/init.pp
+++ profiles/mailserver/manifests/init.pp
@@ -1,16 +1,21 @@
 class mailserver {
   class { 'postfix':
-    config_file_template => "postfix/${::operatingsystem}/etc/postfix/main.cf.erb",
-    #config_file_hash     => {
-    #'postfix' => {
-    #config_file_path   => '/etc/postfix',
-    #config_file_string => "${::fqdn}\n",
-    #config_dir_source  =>  "puppet:///modules/postfix/${::operatingsystem}/etc/postfix",
-    #},
-    #}
-  package_ensure       => 'present',
-  config_dir_purge     => true,
-  config_dir_source =>   "puppet:///profiles/mailserver/${::operatingsystem}/etc/postfix",
+  config_file_template => "postfix/${::operatingsystem}/etc/postfix/main.cf.erb",
+  config_file_hash     => {
+    'main'          => {
+    config_file_path   => '/etc/postfix/main.cf',
+    config_file_string => "${::fqdn}\n",
+    config_file_source => "puppet:///profiles/mailserver/${::operatingsystem}/etc/postfix/main.cf",
+  },
+    'master' => {
+      config_file_path =>  '/etc/postfix/master.cf',
+      config_file_string =>  "${::fqdn}\n",
+      config_file_source =>  "puppet:///profiles/mailserver/${::operatingsystem}/etc/postfix/master.cf",
+    }
+  }
+    #package_ensure    => 'present',
+    #config_dir_purge  => true,
+    #config_dir_source => "puppet:///profiles/mailserver/${::operatingsystem}/etc/postfix",
 
   }
 
