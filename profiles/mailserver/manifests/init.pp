b027bdc1014b975b1000ef341b73b023c3b36bb2
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-14 14:25:00 -0400

    configurando roundcube

--- profiles/mailserver/manifests/init.pp
+++ profiles/mailserver/manifests/init.pp
@@ -1,13 +1,5 @@
 class mailserver {
-  class { 'roundcube':
-    imap_host   => 'ssl://localhost',
-    imap_port   => 993,
-    db_type     => 'pgsql',
-    db_name     => 'roundcube',
-    db_host     => 'localhost',
-    db_username => 'roundcube',
-    db_password => 'secret',
-  }
+  class { 'roundcube':}
 
 }
 
