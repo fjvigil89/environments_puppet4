85add0881d559433a7745cac7eff01fb3df91442
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-14 13:34:53 -0400

    configurando pofixt con ficheros

--- profiles/mailserver/manifests/init.pp
+++ profiles/mailserver/manifests/init.pp
@@ -2,17 +2,11 @@ class mailserver {
   class { 'postfix':
   config_file_template => "postfix/${::operatingsystem}/etc/postfix/main.cf.erb",
   config_file_hash     => {
-    'main'          => {
+    'portfix'          => {
     config_file_path   => '/etc/postfix',
     config_file_string => "${::fqdn}\n",
     config_file_source => "puppet:///profiles/mailserver/${::operatingsystem}/etc/postfix/main.cf",
   },
-    'master' => {
-      config_file_path =>  '/etc/postfix',
-      config_file_string =>  "${::fqdn}\n",
-      config_file_source =>  "puppet:///profiles/mailserver/${::operatingsystem}/etc/postfix/master.cf",
-    }
-  }
     #package_ensure    => 'present',
     #config_dir_purge  => true,
     #config_dir_source => "puppet:///profiles/mailserver/${::operatingsystem}/etc/postfix",
