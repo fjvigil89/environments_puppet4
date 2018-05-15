6d4b3b286e511628ee6e814682dd44503a61f12d
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-15 15:53:38 -0400

    agregando repos configurating

--- profiles/mailserver/manifests/init.pp
+++ profiles/mailserver/manifests/init.pp
@@ -1,11 +1,14 @@
 class mailserver {
 
-	rsyslog::snippet {
-    'postfix_local5:
+# lint:ignore:80chars
+ rsyslog::snippet {
+    'postfix_local5':
       content => "local5.*                          @logs.upr.edu.cu\nlocal5.*                          -/var/log/j-chkmail.log\n& ~";
     'postfix_local6':
       content => "local6.*                          @logs.upr.edu.cu\nlocal6.*                          -/var/log/j-chkmail.log\n& ~";
   }
+
+# lint:endignore
 
   #class {'postfix':
     #root_alias => 'localhost';
