f0c1dc08f2718f218c5ebe9c2cd287fd3199d026
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-05-15 15:23:20 -0400

    modifying roles for basesys

--- roles/icinga2_server/manifests/init.pp
+++ roles/icinga2_server/manifests/init.pp
@@ -9,7 +9,8 @@ class icinga2_server {
 class { '::basesys':
     uprinfo_usage  => 'icinga_server',
     application    => 'icinga',
-    puppet_enabled => false;
+    puppet_enabled => false,
+    mta_enabled    => false,
   }
 class { '::php_webserver':
     php_version    => '7.0',
