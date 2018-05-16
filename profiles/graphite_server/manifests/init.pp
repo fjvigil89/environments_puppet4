d5e43b8191912f3a25f5522720731c251902ef9c
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-05-16 12:59:35 -0400

    adding graphite conf

--- profiles/graphite_server/manifests/init.pp
+++ profiles/graphite_server/manifests/init.pp
@@ -4,14 +4,6 @@
 #
 
 class graphite_server {
-class { 'graphite':
-    gr_django_db_engine       => 'django.db.backends.mysql',
-    gr_django_db_name         => 'graphite',
-    gr_django_db_user         => 'graphite',
-    gr_django_db_password     => 'MYsEcReT!',
-    gr_django_db_host         => 'mysql.my.domain',
-    gr_django_db_port         => '3306',
-    gr_memcache_hosts         => ['127.0.0.1:11211']
-  }
+class { 'graphite': }
 }
 
