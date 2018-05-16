bb1c55a827151b25999d82c9ca9a018989aad97f
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-05-16 12:40:47 -0400

    adding graphite conf

--- profiles/graphite_server/manifests/init.pp
+++ profiles/graphite_server/manifests/init.pp
@@ -4,68 +4,64 @@
 #
 
 class graphite_server {
-  class { 'graphite': 
-  gr_max_updates_per_second => 100,
-  gr_timezone               => 'America/Havana',
-  secret_key                => 'CHANGE_IT!',
-  gr_storage_schemas        => [
-    {
-      name       => 'carbon',
-      pattern    => '^carbon\.',
-      retentions => '1m:90d'
-    },
-    {
-      name       => 'default',
-      pattern    => '.*',
-      retentions => '10s:7d,1m:365d,10m:5y'
-    },
-    {
-      name       => 'default',
-      pattern    => '.*',
-      retentions => '60:43200,900:350400'
-    }
+include '::apache'
+class { 'graphite':
+  gr_web_server => 'none'
+}->
+apache::vhost { 'graphite.upr.edu.cu':
+  port    => '80',
+  docroot => '/opt/graphite/webapp',
+  wsgi_application_group      => '%{GLOBAL}',
+  wsgi_daemon_process         => 'graphite',
+  wsgi_daemon_process_options => {
+    processes          => '5',
+    threads            => '5',
+    display-name       => '%{GROUP}',
+    inactivity-timeout => '120',
+  },
+  wsgi_import_script          => '/opt/graphite/conf/graphite.wsgi',
+  wsgi_import_script_options  => {
+    process-group     => 'graphite',
+    application-group => '%{GLOBAL}'
+  },
+  wsgi_process_group          => 'graphite',
+  wsgi_script_aliases         => {
+    '/' => '/opt/graphite/conf/graphite.wsgi'
+  },
+  headers => [
+    'set Access-Control-Allow-Origin "*"',
+    'set Access-Control-Allow-Methods "GET, OPTIONS, POST"',
+    'set Access-Control-Allow-Headers "origin, authorization, accept"',
   ],
-  gr_django_db_engine       => 'django.db.backends.mysql',
-  gr_django_db_name         => 'graphite',
-  gr_django_db_user         => 'graphite',
-  gr_django_db_password     => 'MYsEcReT!.UPR',
-  gr_django_db_host         => '120.0.0.1',
-  gr_django_db_port         => '3306',
-  gr_memcache_hosts         => ['127.0.0.1:11211']
-  }
-  #include '::apache'
+  directories => [{
+    path => '/media/',
+    order => 'deny,allow',
+    allow => 'from all'}
+  ]
+}
 
-  #apache::vhost { 'graphite.upr.edu.cu':
-  #port                        => '80',
-  #docroot                     => '/opt/graphite/webapp',
-  #suphp_engine                =>  'on',
-  #wsgi_application_group      => '%{GLOBAL}',
-  #wsgi_daemon_process         => 'graphite',
-  #wsgi_daemon_process_options => {
-  #  processes          => '5',
-  #  threads            => '5',
-  #  display-name       => '%{GROUP}',
-  #  inactivity-timeout => '120',
-  #},
-  #wsgi_import_script          => '/opt/graphite/conf/graphite_wsgi.py',
-  #wsgi_import_script_options  => {
-  #  process-group     => 'graphite',
-  #  application-group => '%{GLOBAL}'
-  #},
-  #wsgi_process_group          => 'graphite',
-  #wsgi_script_aliases         => {
-  #  '/' => '/opt/graphite/conf/graphite_wsgi.py'
-  #},
-  #headers                     => [
-  #  'set Access-Control-Allow-Origin "*"',
-  #  'set Access-Control-Allow-Methods "GET, OPTIONS, POST"',
-  #  'set Access-Control-Allow-Headers "origin, authorization, accept"',
-  #],
-  #directories                 => [{
-  #  path => '/media/',
-  #  order => 'deny,allow',
-  #  allow => 'from all'}
-  #]
-  #}
+
+class {'grafana':
+  graphite_host      => 'graphite.upr.edu.cu',
+  elasticsearch_host => 'elasticsearach.my.domain',
+  elasticsearch_port => 9200,
+}->
+apache::vhost { 'grafana.upr.edu.cu':
+  servername      => 'grafana.upr.edu.cu',
+  port            => 80,
+  docroot         => '/opt/grafana',
+  error_log_file  => 'grafana_error.log',
+  access_log_file => 'grafana_access.log',
+  directories     => [
+    {
+      path            => '/opt/grafana',
+      options         => [ 'None' ],
+      allow           => 'from All',
+      allow_override  => [ 'None' ],
+      order           => 'Allow,Deny',
+    }
+  ]
+}
+
 }
 
