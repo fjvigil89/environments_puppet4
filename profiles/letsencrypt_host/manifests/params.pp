4ea2ca3728fb36713a782a7cdaae022f5f0f48ae
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-16 12:09:03 -0400

    instalando el letsencrypt 1

--- profiles/letsencrypt_host/manifests/params.pp
+++ profiles/letsencrypt_host/manifests/params.pp
@@ -3,10 +3,9 @@
 #
 
 # lint:ignore:80chars
-
- class letsencrypt_host::params {
-  $dominios = ['upr.edu.cu'],
-  $webroot_paths  = ['/var/www/upr.edu.cu'],
-  $plugin = 'apache',
-  $email = 'admin@upr.edu.cu',
+class letsencrypt_host::params {
+  $dominios = ['upr.edu.cu']
+  $webroot_paths  = ['/var/www/upr.edu.cu']
+  $plugin = 'apache'
+  $email = 'admin@upr.edu.cu'
 }
