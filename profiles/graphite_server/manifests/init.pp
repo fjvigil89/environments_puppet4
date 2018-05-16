82753bca0796462d8520b123ea46b20e5526865d
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-05-16 12:55:29 -0400

    adding graphite conf

--- profiles/graphite_server/manifests/init.pp
+++ profiles/graphite_server/manifests/init.pp
@@ -5,26 +5,6 @@
 
 class graphite_server {
 class { 'graphite':
-    gr_max_updates_per_second => 100,
-    gr_timezone               => 'Europe/Berlin',
-    secret_key                => 'CHANGE_IT!',
-    gr_storage_schemas        => [
-      {
-        name       => 'carbon',
-        pattern    => '^carbon\.',
-        retentions => '1m:90d'
-      },
-      {
-        name       => 'special_server',
-        pattern    => '^longtermserver_',
-        retentions => '10s:7d,1m:365d,10m:5y'
-      },
-      {
-        name       => 'default',
-        pattern    => '.*',
-        retentions => '60:43200,900:350400'
-      }
-    ],
     gr_django_db_engine       => 'django.db.backends.mysql',
     gr_django_db_name         => 'graphite',
     gr_django_db_user         => 'graphite',
