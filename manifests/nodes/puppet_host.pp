60213e187104c8ffbb78c39bd381cdddc166c8c5
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-17 11:36:23 -0400

    configurando talk

--- manifests/nodes/puppet_host.pp
+++ manifests/nodes/puppet_host.pp
@@ -10,7 +10,7 @@ class { 'prosody':
     authentication    => 'ldap',
     custom_options    => {
                             'ldap_base'     => '"DC=upr,DC=edu,DC=cu"',
-                            'ldap_server'   => 'ad.upr.edu.cu:636',
+                            'ldap_server'   => '"ad.upr.edu.cu:636"',
                             'ldap_rootdn'   => '"DN=talk,OU=_Servicios,DC=upr,DC=edu,DC=cu"',
                             'ldap_password' => '"40a*talk.2k12"',
                             'ldap_scope'    => 'subtree',
