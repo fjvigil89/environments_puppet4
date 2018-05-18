617187e38ccdd532aad21d5db5da17f50d4be24d
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-18 12:06:11 -0400

    creando vhost el nuevo nodo wh-bk

--- manifests/nodes/wh-bk.pp
+++ manifests/nodes/wh-bk.pp
@@ -2,6 +2,29 @@ node 'wh-bk.upr.edu.cu'{
  
  class{'::wh_php_apache':;} 
  
+ apache::vhost { 'sync.upr.edu.cu':
+  port     => '443',
+  docroot  => '/home/Sync-UPR/master/public/',
+  ssl      => true,
+  docroot_owner => 'root',
+  docroot_group => 'root',
+ } 
+
+ apache::vhost { 'contable.upr.edu.cu':
+  port     => '443',
+  docroot  => '/home/Contable/master/public/',
+  ssl      => true,
+  docroot_owner => 'root',
+  docroot_group => 'root',
+ }
+
+ apache::vhost { 'apiassets.upr.edu.cu':
+  port     => '443',
+  docroot  => '/home/Api-Assets/master/public/',
+  ssl      => true,
+  docroot_owner => 'root',
+  docroot_group => 'root',
+ }
 
 }
 
