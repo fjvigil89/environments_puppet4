62c271bb55c206f0cf845b8f251c190a50688041
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-21 17:32:51 -0400

    agregando wh de apache para el wh

--- manifests/nodes/wh-bk.pp
+++ manifests/nodes/wh-bk.pp
@@ -1,13 +1,20 @@
 node 'wh-bk.upr.edu.cu'{
  
  class{'::wh_php_apache':;} 
- 
- apache::vhost { 'sync.upr.edu.cu':
-  port     => '443',
-  docroot  => '/home/Sync-UPR/master/public/',
-  ssl      => true,
-  docroot_owner => 'root',
-  docroot_group => 'root',
+
+ apache::vhost { 'sync.upr.edu.cu non-ssl':
+  servername      => 'sync.upr.edu.cu',
+  port            => '80',
+  docroot         => '/home/Sync-UPR/master/public',
+  redirect_status => 'permanent',
+  redirect_dest   => 'https://sync.upr.edu.cu/'
+ }
+
+ apache::vhost { 'sync.upr.edu.cu ssl':
+  servername => 'sync.upr.edu.cu',
+  port       => '443',
+  docroot    => '/home/Sync-UPR/master/public',
+  ssl        => true,
  } 
 
  apache::vhost { 'contable.upr.edu.cu':
