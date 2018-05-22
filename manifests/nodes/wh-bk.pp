259069d40e8ea19b70b7f39dbd66df4a1f84ed2a
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-22 16:49:41 -0400

    configurando php_web

--- manifests/nodes/wh-bk.pp
+++ manifests/nodes/wh-bk.pp
@@ -2,7 +2,7 @@ node 'wh-bk.upr.edu.cu'{
  
  class{'::wh_php_apache':;} 
 
- apache::vhost { 'sync.upr.edu.cu non-ssl':
+ apache::vhost { 'sync.upr.edu.cu':
   servername       => 'sync.upr.edu.cu',
   port             => '80',
   docroot          => '/home/Sync-UPR/master/public/',
@@ -10,28 +10,6 @@ node 'wh-bk.upr.edu.cu'{
   #redirect_dest    => 'https://sync.upr.edu.cu/',
  }
 
- apache::vhost { 'sync.upr.edu.cu ssl':
-  servername       => 'sync.upr.edu.cu',
-  port             => '443',
-  docroot          => '/home/Sync-UPR/master/public/',
-  ssl              => true,
- } 
-
- apache::vhost { 'contable.upr.edu.cu':
-  port     => '443',
-  docroot  => '/home/Contable/master/public/',
-  ssl      => true,
-  docroot_owner => 'root',
-  docroot_group => 'root',
- }
-
- apache::vhost { 'apiassets.upr.edu.cu':
-  port     => '80',
-  docroot  => '/home/Api-Assets/master/public/',
-  ssl      => false,
-  docroot_owner => 'root',
-  docroot_group => 'root',
- }
 
 }
 
