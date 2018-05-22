c620b1b50ce42a714287eba51f3601eb0b9d660f
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-22 16:38:22 -0400

    configurando php_web

--- manifests/nodes/wh-bk.pp
+++ manifests/nodes/wh-bk.pp
@@ -7,7 +7,7 @@ node 'wh-bk.upr.edu.cu'{
   port             => '80',
   docroot          => '/home/Sync-UPR/master/public/',
   redirect_status  => 'permanent',
-  redirect_dest    => 'https://sync.upr.edu.cu/'
+  redirect_dest    => 'https://sync.upr.edu.cu/',
   fallbackresource => '/index.php',
   file_type        => 'application/x-httpd-php',
  }
