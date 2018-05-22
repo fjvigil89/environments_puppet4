63dc2a0be071f403f2e2e32304b90dc7ee5f8a7f
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-22 16:27:46 -0400

    configurando php_web

--- roles/wh_php_apache/manifests/apache.pp
+++ roles/wh_php_apache/manifests/apache.pp
@@ -6,23 +6,6 @@ class wh_php_apache::apache{
 
  class { 'apache':
   default_vhost => true,
-  mpm_module => 'prefork',
  }
-
-  # Load mod_rewrite if needed and not yet loaded
-  if ! defined(Class['apache::mod::rewrite']) {
-  ¦ include ::apache::mod::rewrite
-  }
-
-  class { '::apache::mod::proxy':; }
-  -> class { '::apache::mod::proxy_fcgi':; }
-
-  if $mod_security_enabled {
-  ¦ class { '::apache::mod::security':
-  ¦ ¦ activated_rules      => $mod_security_rules,
-  ¦ ¦ modsec_secruleengine => $mod_security_mode,
-
-  ¦ }
-  } 
 
 }
