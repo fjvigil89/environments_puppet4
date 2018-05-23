74e8a977b4abb12a7bf122921113ed2a1987d8e7
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-23 09:48:50 -0400

    pasando nuevo opciones para el wh-bk

--- manifests/nodes/wh-bk.pp
+++ manifests/nodes/wh-bk.pp
@@ -9,9 +9,9 @@ node 'wh-bk.upr.edu.cu'{
   directories      => [ {
     path    => '/home/Sync-UPR/master/public',
     #options => ['Indexes','FollowSymLinks','MultiViews'],
-    directoryindex => 'index.php index.html ',
-    allowoverride  => All,
+    allowoverride  => 'All',
     allowfrom     => 'All',
+    directoryindex => 'index.php index.html ',
     },],
 
   # redirect_status  => 'permanent',
