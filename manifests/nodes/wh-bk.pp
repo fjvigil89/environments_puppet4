5851e7527aa6598a690cd4ae6aea999f1c90232b
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-23 09:59:45 -0400

    agregado contable al wh

--- manifests/nodes/wh-bk.pp
+++ manifests/nodes/wh-bk.pp
@@ -18,6 +18,24 @@ node 'wh-bk.upr.edu.cu'{
   #redirect_dest    => 'https://sync.upr.edu.cu/',
  }
 
+ 
+ apache::vhost { 'contable.upr.edu.cu':
+  servername       => 'contable.upr.edu.cu',
+  port             => '80',
+  docroot          => '/home/Contable/master/public/',
+  directories      => [ {
+    path    => '/home/Contable/master/public',
+    #options => ['Indexes','FollowSymLinks','MultiViews'],
+    allowoverride  => 'All',
+    allowfrom     => 'All',
+    directoryindex => 'index.php index.html ',
+    },],
+
+  # redirect_status  => 'permanent',
+  #redirect_dest    => 'https://contable.upr.edu.cu/',
+ }
+ 
+
 
 }
 
