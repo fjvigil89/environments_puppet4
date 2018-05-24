de2c3227d843abbdb18cf67bcbbf3943d8619058
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-05-24 16:27:34 -0400

    add mysqlserver to graphiteserv

--- profiles/graphite_server/manifests/init.pp
+++ profiles/graphite_server/manifests/init.pp
@@ -4,8 +4,11 @@
 #
 
 class graphite_server {
-include '::mysql::server'
-mysql::db { 'graphite':
+ class { '::mysql::server':
+  root_password           => 'graphite.2k18',
+  remove_default_accounts => true,
+ } 
+ mysql::db { 'graphite':
   user     => 'graphite',
   password => 'graphite*upr.2k18',
   host     => '127.0.0.1',
