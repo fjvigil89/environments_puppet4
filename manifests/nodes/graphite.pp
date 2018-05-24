d8bdf25c51ae51a0dd26d65fb216e297d647bfe9
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-05-24 15:09:36 -0400

    change graphite roles

--- manifests/nodes/graphite.pp
+++ manifests/nodes/graphite.pp
@@ -1,3 +1,9 @@
 node 'graphite.upr.edu.cu'{
-  include graphiteserver
+class { '::basesys':
+  ¦ uprinfo_usage  => 'graphite_server',
+  ¦ application    => 'graphite',
+  ¦ puppet_enabled => false,
+  ¦ mta_enabled    => false,
+  }
+
 }
