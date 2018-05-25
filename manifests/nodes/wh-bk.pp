76ee4bf6f9789c4eca2ed2cef72831015fbfd0c4
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-25 18:21:47 -0400

    configurando php_web

--- manifests/nodes/wh-bk.pp
+++ manifests/nodes/wh-bk.pp
@@ -2,7 +2,7 @@ node 'wh-bk.upr.edu.cu'{
  
  class{'::wh_php_apache':;} 
 
- apache::vhost { 'sync.upr.edu.cu':
+ apache::vhost { 'sync.upr.edu.cu non-ssl':
   servername       => 'sync.upr.edu.cu',
   serveraliases => ['www.sync.upr.edu.cu'], 
   port             => '80',
@@ -17,12 +17,19 @@ node 'wh-bk.upr.edu.cu'{
     directoryindex => 'index.php index.html ',
     },],
 
-  # redirect_status  => 'permanent',
-  #redirect_dest    => 'https://sync.upr.edu.cu/',
+  redirect_status  => 'permanent',
+  redirect_dest    => 'https://sync.upr.edu.cu/',
  }
 
+ apache::vhost { 'sync.upr.edu.cu ssl':
+  servername    => 'sync.upr.edu.cu',
+  serveraliases =>  ['www.sync.upr.edu.cu'],
+  port          => '443',
+  docroot       => '/home/Sync-UPR/master/public/',
+  ssl           => true,
+ }
  
- apache::vhost { 'contable.upr.edu.cu':
+ apache::vhost { 'contable.upr.edu.cu non-ssl':
   servername       => 'contable.upr.edu.cu',
   serveraliases => ['www.contable.upr.edu.cu'],
   port             => '80',
@@ -37,11 +44,17 @@ node 'wh-bk.upr.edu.cu'{
     directoryindex => 'app.php app.html ',
     },],
 
-  # redirect_status  => 'permanent',
-  #redirect_dest    => 'https://contable.upr.edu.cu/',
+  redirect_status  => 'permanent',
+  redirect_dest    => 'https://contable.upr.edu.cu/',
  }
  
-
+ apache::vhost { 'contable.upr.edu.cu ssl':
+  servername    => 'contable.upr.edu.cu',
+  serveraliases =>  ['www.contable.upr.edu.cu'],
+  port          => '443',
+  docroot       => '/home/Contable/master/web/',
+  ssl           => true,
+ }
 
 }
 
