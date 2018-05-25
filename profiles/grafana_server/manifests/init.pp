af42a8dd896585d99bf8557f7a379c964f3e4a5d
Arian Trujillo Diaz <arian@upr.edu.cu>
2018-05-25 14:50:28 -0400

    add grafana settings

--- profiles/grafana_server/manifests/init.pp
+++ profiles/grafana_server/manifests/init.pp
@@ -5,6 +5,65 @@
 # === Parameters
 #
 class grafana_server {
-
+class { '::mysql::server':
+ root_password           => 'graphite.2k18',
+ remove_default_accounts => true,
+} 
+mysql::db { 'grafana':
+user     => 'grafana',
+password => 'grafana*upr.2k18',
+host     => 'localhost',
+grant    =>  ['ALL'],
+}
+class { 'grafana':
+cfg => {
+app_mode => 'production',
+server   => {
+      http_port     => 8080,
+    },
+    database => {
+      type          => 'mysql',
+      host          => '127.0.0.1:3306',
+      name          => 'grafana',
+      user          => 'grafana',
+      password      => 'grafana*upr.2k18',
+    },
+    users    => {
+      allow_sign_up => false,
+    },
+  },
+ldap_cfg   => {
+   servers => [
+    { host            => 'ad.upr.edu.cu',
+      port            => 389,
+      use_ssl         => false,
+      search_filter   => '(sAMAccountName=%s)',
+      search_base_dns => [ 'ou=_GrupoRedes,dc=upr,dc=edu,dc=cu' ],
+      bind_dn         => 'icinga2',
+      bind_password   => 'web.2k17',
+    },
+  ],
+  'servers.attributes' => {
+    name      => 'givenName',
+    surname   => 'sn',
+    username  => 'sAMAccountName',
+    member_of => 'memberOf',
+    email     => 'email',
+  }
+},
+grafana_organization { 'UPRedes':
+  grafana_url      => 'http://localhost:3000',
+  grafana_user     => 'admin',
+  grafana_password => 'grafana',
+}
+grafana_dashboard { 'example_dashboard':
+  grafana_url       => 'http://localhost:3000',
+  grafana_user      => 'admin',
+  grafana_password  => 'grafana',
+  grafana_api_path  => '/grafana/api',
+  organization      => 'UPRedes',
+  content           => template('path/to/exported/file.json'),
+}
+}
 
 }
--- profiles/graphite_server/manifests/init.pp
+++ profiles/graphite_server/manifests/init.pp
@@ -4,20 +4,7 @@
 #
 
 class graphite_server {
-  # class { '::mysql::server':
-  # root_password           => 'graphite.2k18',
-  #remove_default_accounts => true,
-  # } 
-  #mysql::db { 'graphite':
-  # user     => 'graphite',
-  #password => 'graphite*upr.2k18',
-  #host     => 'localhost',
-  #  grant => ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'DROP', 'CREATE VIEW', 'CREATE', 'INDEX', 'EXECUTE', 'ALTER'],
-  #grant    =>  ['ALL'],
-  #}
-class { 'postgresql::server':
-}
-
+class { 'postgresql::server':}
 postgresql::server::db { 'graphite':
   user     => 'graphite',
   password => postgresql_password('graphite', 'graphite.2k18'),
