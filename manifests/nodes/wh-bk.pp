5649da4eeca79ab8ba5ab5e0f745c59e16f7b25b
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-28 10:15:22 -0400

    configurando exc puppet

--- manifests/nodes/wh-bk.pp
+++ manifests/nodes/wh-bk.pp
@@ -17,16 +17,14 @@ node 'wh-bk.upr.edu.cu'{
 
   redirect_status  => 'permanent',
   redirect_dest    => 'https://sync.upr.edu.cu/',
- }
-
+ }~>
  apache::vhost { 'sync.upr.edu.cu ssl':
   servername    => 'sync.upr.edu.cu',
   serveraliases =>  ['www.sync.upr.edu.cu'],
   port          => '443',
   docroot       => '/home/Sync-UPR/master/public/',
   ssl           => true,
- }
- 
+ }~> 
  apache::vhost { 'contable.upr.edu.cu non-ssl':
   servername       => 'contable.upr.edu.cu',
   serveraliases => ['www.contable.upr.edu.cu'],
@@ -42,20 +40,18 @@ node 'wh-bk.upr.edu.cu'{
 
   redirect_status  => 'permanent',
   redirect_dest    => 'https://contable.upr.edu.cu/',
- }
- 
+ }~> 
  apache::vhost { 'contable.upr.edu.cu ssl':
   servername    => 'contable.upr.edu.cu',
   serveraliases =>  ['www.contable.upr.edu.cu'],
   port          => '443',
   docroot       => '/home/Contable/master/web/',
   ssl           => true,
- }
+ }~>
  exec{"a2enmod_php7":
   refreshonly => true,
   command => '/usr/bin/sudo a2enmod php7.0',
- }
-
+ }~>
  exec{"service_apache2_restart":
   command => '/usr/bin/sudo service apache2 restart',
  }
