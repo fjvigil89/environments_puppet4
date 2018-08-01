03ef9612ba52fdfd8f01973c8cd92e52c36e1059
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-01 18:01:19 -0400

    RF

--- manifests/nodes/mx.pp
+++ manifests/nodes/mx.pp
@@ -2,9 +2,9 @@ node /^mx\d+$/ {
    class { '::basesys':
     uprinfo_usage  => 'servidor mx',
     application    => 'MX Postfix',
-    # puppet_enabled => false,
-    #repos_enabled  => false,
-    #mta_enabled    => false,
+    puppet_enabled => false,
+    repos_enabled  => false,
+    mta_enabled    => false,
   }
 
 }
