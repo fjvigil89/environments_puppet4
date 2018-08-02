ddd23bcb4a44fe7774bbf9234576a1c59c154e2a
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-02 18:13:41 -0400

    Add basesys

--- manifests/nodes/mx.pp
+++ manifests/nodes/mx.pp
@@ -16,26 +16,26 @@ node 'mx2.upr.edu.cu' {
   package { 'lsb-release':
     ensure => installed,
   }
-  #class { '::basesys':
-  #  uprinfo_usage  => 'servidor mx',
-  #  application    => 'MX Postfix',
+  class { '::basesys':
+    uprinfo_usage  => 'servidor mx',
+    application    => 'MX Postfix',
     #  puppet_enabled => false,
   #  repos_enabled  => false,
   #  mta_enabled    => false,
-  #}
+  }
 
 }
 node 'mx3.upr.edu.cu' {
   package { 'lsb-release':
     ensure => installed,
   }
-  #class { '::basesys':
-  #  uprinfo_usage  => 'servidor mx',
-  #  application    => 'MX Postfix',
+  class { '::basesys':
+    uprinfo_usage  => 'servidor mx',
+    application    => 'MX Postfix',
     #  puppet_enabled => false,
   #  repos_enabled  => false,
   #  mta_enabled    => false,
-  #}
+  }
 
 }
 
