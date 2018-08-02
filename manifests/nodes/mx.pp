1a4b7ee45d6e489962c3c20ca9157bad777efe53
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-02 17:40:44 -0400

    Fv

--- manifests/nodes/mx.pp
+++ manifests/nodes/mx.pp
@@ -2,13 +2,13 @@ node /^mx\d+$/ {
   package { 'lsb-release':
 	  ensure => installed,
   }
-  #   class { '::basesys':
-  #  uprinfo_usage  => 'servidor mx',
-  #  application    => 'MX Postfix',
-  #  puppet_enabled => false,
+     class { '::basesys':
+    uprinfo_usage  => 'servidor mx',
+    application    => 'MX Postfix',
+    #  puppet_enabled => false,
   #  repos_enabled  => false,
   #  mta_enabled    => false,
-  #}
+  }
 
 }
 
