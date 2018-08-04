bd644465cb90d423359cdd77bff26be882ad203b
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-08-04 17:11:37 -0400

    add services node

--- manifests/nodes/nodos_servicios.pp
+++ manifests/nodes/nodos_servicios.pp
@@ -1,12 +1,12 @@
 node /^nodo\d+$/ {  
   package { 'lsb-release':
           ensure => installed,
-  }#~>
-  #class { '::basesys':
-  #  uprinfo_usage  => 'servidor de Servicios',
-  #  application    => 'Proxmox Servicios',
-  #  puppet_enabled => false,
-  #  repos_enabled  => true,
-  #  mta_enabled    => false,
-  #}
+  }~>
+  class { '::basesys':
+    uprinfo_usage  => 'servidor de Servicios',
+    application    => 'Proxmox Servicios',
+    puppet_enabled => false,
+    repos_enabled  => true,
+    mta_enabled    => false,
+  }
 }
