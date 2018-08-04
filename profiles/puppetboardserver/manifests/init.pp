034f52598dc80b34a4858d820846e196cd2fc615
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-04 19:41:05 -0400

    Add configuration on puppetboard

--- profiles/puppetboardserver/manifests/init.pp
+++ profiles/puppetboardserver/manifests/init.pp
@@ -15,28 +15,24 @@ class puppetboardserver(
   default_mods  => false,
   }
 
-# Configure puppetboard
-# class { '::puppetboard':
-# puppetdb_host     => $puppetdb_host,
-# puppetdb_port     => '8080',
-# manage_git        => true,
-# manage_virtualenv => true,
-# enable_catalog    => true,
-# revision          => '35486e8',
-# }
-
   class { 'apache::mod::wsgi':
   wsgi_socket_prefix => '/var/run/wsgi',
   }
 
+  $ssl_dir = $::settings::ssldir
+  $puppetboard_certname = $::certname
 # Configure Puppetboard
   class { 'puppetboard':
-  puppetdb_host       => $puppetdb_host,
-  puppetdb_port       => 8080,
-  default_environment => '*',
-  manage_git          => true,
-  manage_virtualenv   => true,
-  reports_count       => 50
+    groups              => 'puppet',
+    puppetdb_key        => "${ssl_dir}/private_keys/${puppetboard_certname}.pem",
+    puppetdb_ssl_verify => "${ssl_dir}/certs/ca.pem",
+    puppetdb_cert       => "${ssl_dir}/certs/${puppetboard_certname}.pem",
+    puppetdb_host       => $puppetdb_host,
+    puppetdb_port       => 8080,
+    default_environment => '*',
+    manage_git          => true,
+    manage_virtualenv   => true,
+    reports_count       => 50
   }
   class { 'puppetboard::apache::conf': }
 }
