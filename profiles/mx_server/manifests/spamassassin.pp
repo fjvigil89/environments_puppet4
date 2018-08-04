0b210f7b7d6571f263fc50375ac0b3964cd34796
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-04 14:33:41 -0400

    Fix error on spamassassin

--- profiles/mx_server/manifests/spamassassin.pp
+++ profiles/mx_server/manifests/spamassassin.pp
@@ -4,8 +4,6 @@
 # Configutarion for spamassassin
 #
 class mx_server::spamassassin {
-  }
-  #  ensure_packages('zoo','unzip','bzip2','libnet-ph-perl','libnet-snpp-perl','libnet-telnet-perl','nomarch','lzop')
   class { 'spamassassin':
   sa_update         => true,
   run_execs_as_user => 'amavis',
