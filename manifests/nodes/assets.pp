5097e9fd6ee38801438775cf54bf4b4566d3f725
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-08-04 17:30:04 -0400

    add assets node

---
+++ manifests/nodes/assets.pp
@@ -0,0 +1,12 @@
+node /^\ssetsd+$/ {  
+  package { 'lsb-release':
+          ensure => installed,
+  }~>
+  class { '::basesys':
+    uprinfo_usage  => 'servidor de Assets',
+    application    => 'Proxmox BeKa',
+    puppet_enabled => false,
+    repos_enabled  => true,
+    mta_enabled    => false,
+  }
+}
