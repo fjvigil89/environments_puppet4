6cbd2889d440fbdeb13b003377d8ad39e10b9e0d
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-06 18:07:11 -0400

    Fix error proxy node conf

--- manifests/nodes/proxy.pp
+++ manifests/nodes/proxy.pp
@@ -1,7 +1,4 @@
 node 'proxy-go.upr.edu.cu','proxy-tor.upr.edu.cu','proxy-pap.upr.edu.cu {  
-  package { 'lsb-release':
-          ensure => installed,
-  }~>
   class { '::basesys':
     uprinfo_usage  => 'Servidor Proxy',
     application    => 'Servidor Proxy Squid',
