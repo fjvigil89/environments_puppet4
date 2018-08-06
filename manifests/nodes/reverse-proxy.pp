4413f7658cc6309d1c885ebe183dd066d520b186
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-06 16:03:32 -0400

    Add proxys reverse node conf

---
+++ manifests/nodes/reverse-proxy.pp
@@ -0,0 +1,12 @@
+node 'nginx-inverso.upr.edu.cu','proxy-inverso.upr.edu.cu','toran-proxy.upr.edu.cu' {  
+  package { 'lsb-release':
+          ensure => installed,
+  }~>
+  class { '::basesys':
+    uprinfo_usage  => 'servidor proxy inverso',
+    application    => 'Reverse Proxy UPR',
+    puppet_enabled => false,
+    repos_enabled  => true,
+    mta_enabled    => false,
+  }
+}
