a79f0f052d524baaba1c9de635a37020972963ce
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-06 18:01:30 -0400

    Add proxys node conf

---
+++ manifests/nodes/proxy.pp
@@ -0,0 +1,12 @@
+node 'proxy-go.upr.edu.cu','proxy-tor.upr.edu.cu','proxy-pap.upr.edu.cu {  
+  package { 'lsb-release':
+          ensure => installed,
+  }~>
+  class { '::basesys':
+    uprinfo_usage  => 'Servidor Proxy',
+    application    => 'Servidor Proxy Squid',
+    puppet_enabled => false,
+    repos_enabled  => true,
+    mta_enabled    => false,
+  }
+}
