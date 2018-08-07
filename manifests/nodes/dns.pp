e21ce2082acdfcf89c2ccc3b46e3bc082785aa71
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-07 12:33:29 -0400

    Fx

--- manifests/nodes/dns.pp
+++ manifests/nodes/dns.pp
@@ -1,8 +1,2 @@
 node 'dns.upr.edu.cu'{
-  class { '::basesys':
-    uprinfo_usage  => 'servidor master dns',
-    application    => 'DNS UPR',
-    puppet_enabled => false,
-    repos_enabled  => false,
-    mta_enabled    => false,
 }
