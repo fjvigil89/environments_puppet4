084ab5d580983cfa38c324a48d775d6f2f77afcb
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-08 13:39:01 -0400

    Add version 1.1 graphite module icingaweb2

--- profiles/monitoring/manifests/icingaweb2.pp
+++ profiles/monitoring/manifests/icingaweb2.pp
@@ -234,7 +234,7 @@ file_line{ 'php_memory_limit':
 
 #Graphite Module
 class { 'icingaweb2::module::graphite':
-  git_revision => 'v0.9.0',
+  git_revision => 'v1.1.0',
   url          => 'http://graphite.upr.edu.cu'
 }
 
