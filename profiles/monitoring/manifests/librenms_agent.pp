77908053f772c3e30298ee94be8d844ce4dffcbc
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-11 11:35:58 -0400

    permit localhost snmp_agent

--- profiles/monitoring/manifests/librenms_agent.pp
+++ profiles/monitoring/manifests/librenms_agent.pp
@@ -9,7 +9,7 @@ class monitoring::librenms_agent {
 class { 'snmpd':
   package       => true,
   service       => true,
-  allowed_hosts => [ '10.2.1.250/32','10.2.9.0/24' ],
+  allowed_hosts => [ 'localhost','10.2.1.250/32','10.2.9.0/24' ],
   community     => 'UPRadmin4all',
   syslocation   => 'UPR, Nodo Central',
   syscontact    => 'di@upr.edu.cu'
