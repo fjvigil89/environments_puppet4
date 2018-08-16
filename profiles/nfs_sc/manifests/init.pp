b61e6ccdf40f6a70af86930a6dbd0adb7540ee27
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-16 17:48:03 -0400

    Fix more error

--- profiles/nfs_sc/manifests/init.pp
+++ profiles/nfs_sc/manifests/init.pp
@@ -8,8 +8,8 @@ class nfs_sc (
   Boolean $nfs_server_enabled = $::nfs_sc::params::nfs_server_enabled,
   Boolean $nfs_client_enabled = $::nfs_sc::params::nfs_client_enabled,
 
-  String $nfs_v4_client       = $::nfs_sc::params::nfs_v4_client,
-  String $nfs_v4_idmap_domain = $::nfs_sc::params::nfs_v4_client,
+  Boolean $nfs_v4_client      = $::nfs_sc::params::nfs_v4_client,
+  String $nfs_v4_idmap_domain = $::nfs_sc::params::nfs_v4_idmap_domain,
 
   String $nfs_server = $::nfs_sc::params::nfs_server,
   String $nfs_share  = $::nfs_sc::params::nfs_share,
