cdaee9f112fa56c6b5bbe017cea01c50bd9f991c
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-16 17:13:26 -0400

    Fix error

--- roles/nfs_client/manifests/init.pp
+++ roles/nfs_client/manifests/init.pp
@@ -4,7 +4,7 @@
 # Full description of class nfs_client here.
 
 class nfs_client {
-  class nfs_sc {'::nfs_sc':
+  class {'::nfs_sc':
     nfs_server => '10.2.25.1',
     nfs_share  => 'repositorio',
     nfs_mount  => '/repos_facultades',
