4fb0e9eb206389a3f74c1a26eb5d1029be5b3bdc
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-16 17:21:16 -0400

    Fg

--- roles/nfs_client/manifests/init.pp
+++ roles/nfs_client/manifests/init.pp
@@ -5,6 +5,9 @@
 
 class nfs_client {
   class {'::nfs_sc':
+    nfs_server_enabled => 'false',
+    nfs_client_enabled => 'true',
+
     nfs_server => '10.2.25.1',
     nfs_share  => 'repositorio',
     nfs_mount  => '/repos_facultades',
