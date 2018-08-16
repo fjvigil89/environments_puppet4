6d072000e6a783dc87d7e36a809c09fdbcc1b4ee
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-16 17:42:19 -0400

    Fix error string

--- profiles/nfs_sc/manifests/params.pp
+++ profiles/nfs_sc/manifests/params.pp
@@ -5,10 +5,10 @@
 
 class nfs_sc::params {
   # Server & Client parameters
-  $nfs_server_enabled = 'false'
-  $nfs_client_enabled = 'true'
+  $nfs_server_enabled = false
+  $nfs_client_enabled = true
   
-  $nfs_v4_client = 'true'
+  $nfs_v4_client = true
   $nfs_v4_idmap_domain = $::domain
 
   $nfs_server = 'localhost'
--- roles/nfs_client/manifests/init.pp
+++ roles/nfs_client/manifests/init.pp
@@ -5,8 +5,8 @@
 
 class nfs_client {
   class { '::nfs_sc': 
-    nfs_server_enabled => 'false',
-    nfs_client_enabled => 'true',
+    #    nfs_server_enabled => false,
+    #nfs_client_enabled => true,
 
     nfs_server => '10.2.25.1',
     nfs_share  => 'repositorio',
