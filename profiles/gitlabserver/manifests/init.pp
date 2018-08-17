104adff0385b2f46df75ed79a4d9292a1fbce644
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-17 13:20:11 -0400

    gitlab server conf test

--- profiles/gitlabserver/manifests/init.pp
+++ profiles/gitlabserver/manifests/init.pp
@@ -6,47 +6,44 @@
 class gitlabserver{
   include git
   class { 'gitlab':
-    external_url => 'http://gitlab.upr.edu.cu',
-    nginx        => {
-    #ssl_certificate     => '/etc/gitlab/ssl/gitlab.example.com.crt',
-    #ssl_certificate_key => '/etc/gitlab/ssl/gitlab.example.com.key'
-    redirect_http_to_https  => true
+    external_url         => 'http://gitlab.upr.edu.cu',
+    skip_auto_migrations => false,
+    nginx                => {
+      redirect_http_to_https  => true
     },
-  gitlab_rails   => {
-    'webhook_timeout'      => 10,
-    'gitlab_default_theme' => 2,
-    'ldap_enabled'         => true,
-    'ldap_servers'         => {
-      'main'                => {
-        label                         => 'LDAP',
-        host                          => 'ad.upr.edu.cu',
-        port                          => 389,
-        uid                           => 'sAMAccountName',
-        method                        => 'plain',
-        bind_dn                       => 'CN=git,OU=_Servicios,DC=upr,DC=edu,DC=cu',
-        password                      => 'mistake*tig.20',
-        active_directory              => true,
-        allow_username_or_email_login => false,
-        block_auto_created_users      => false,
-        base                          => 'DC=upr,DC=edu,DC=cu',
-        #group_base			=> 'MYGROUPBASE',
-        #user_filter			=> '',				
+    gitlab_rails         => {
+      'webhook_timeout'      => 10,
+      'gitlab_default_theme' => 2,
+      'ldap_enabled'         => true,
+      'ldap_servers'         => {
+        'main' => {
+          label                         => 'LDAP',
+          host                          => 'ad.upr.edu.cu',
+          port                          => 389,
+          uid                           => 'sAMAccountName',
+          method                        => 'plain',
+          bind_dn                       => 'CN=git,OU=_Servicios,DC=upr,DC=edu,DC=cu',
+          password                      => 'mistake*tig.20',
+          active_directory              => true,
+          allow_username_or_email_login => false,
+          block_auto_created_users      => false,
+          base                          => 'DC=upr,DC=edu,DC=cu',
         }
-    },
+      },
   },
-  logging        => {
+  logging      => {
     'svlogd_size' => '200 * 1024 * 1024',
     }, 
     secrets      => {
-    'gitlab_shell' => {
-      secret_token    => 'asecrettoken1234567890'},
-    'gitlab_rails' => {
-      secret_token => 'asecrettoken123456789010'},
-      'gitlab_ci'  => {
-        secret_token    => null,
-        secret_key_base => 'asecrettoken123456789011',
-        db_key_base     => 'asecrettoken123456789012',
-      }
+      'gitlab_shell' => {
+        secret_token    => 'asecrettoken1234567890'},
+        'gitlab_rails' => {
+          secret_token => 'asecrettoken123456789010'},
+          'gitlab_ci'  => {
+            secret_token    => null,
+            secret_key_base => 'asecrettoken123456789011',
+            db_key_base     => 'asecrettoken123456789012',
+          }
     },
 
 }
--- roles/nfs_client/manifests/init.pp
+++ roles/nfs_client/manifests/init.pp
@@ -5,9 +5,6 @@
 
 class nfs_client {
   class { '::nfs_sc': 
-    #    nfs_server_enabled => false,
-    #nfs_client_enabled => true,
-
     nfs_server => '10.2.25.1',
     nfs_share  => 'export/owncloud_data',
     nfs_mount  => '/repositorio',
