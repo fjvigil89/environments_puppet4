22910e315794f3c02dc3738133d20b43f970412e
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-17 13:34:21 -0400

    Fgb

--- profiles/gitlabserver/manifests/init.pp
+++ profiles/gitlabserver/manifests/init.pp
@@ -7,7 +7,8 @@ class gitlabserver{
   include git
   class { 'gitlab':
     external_url         => 'http://gitlab.upr.edu.cu',
-    skip_auto_migrations => true,
+    skip_auto_reconfigure => 'present'
+    #skip_auto_migrations => true,
     nginx                => {
       redirect_http_to_https  => true
     },
