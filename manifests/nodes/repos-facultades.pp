87cf7a29b6b2d345389c080ce41d444715535039
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-18 18:48:03 -0400

    used mount

--- manifests/nodes/repos-facultades.pp
+++ manifests/nodes/repos-facultades.pp
@@ -1,5 +1,11 @@
 node 'repofct.upr.edu.cu' { 
-  include nfs_client
+  mount {'/repositorio':
+   device  => '10.2.25.1:/export/repos_facultades',
+   fstype  => 'nfs',
+   ensure  => mounted,
+   options => 'default',
+   atboot  => true,
+  }
   include smb_rclient
 }
 
