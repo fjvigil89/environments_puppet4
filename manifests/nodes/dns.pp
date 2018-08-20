4111518d215fccf5fc7e4b10a60a5f72a5f00601
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-20 16:21:44 -0400

    Add ns1 node conf

--- manifests/nodes/dns.pp
+++ manifests/nodes/dns.pp
@@ -8,3 +8,14 @@ node 'dns.upr.edu.cu'{
   }
 
 }
+node 'ns1.upr.edu.cu'{
+  class { '::basesys':
+    uprinfo_usage  => 'servidor dns externo',
+    application    => 'DNS Bind9',
+    puppet_enabled => false,
+    repos_enabled  => true,
+    mta_enabled    => false,
+  }
+
+}
+
