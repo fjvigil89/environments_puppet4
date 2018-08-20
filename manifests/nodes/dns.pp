a7d0f1a38260ff68e879db559de9de1148605812
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-20 16:36:14 -0400

    Change conf ns1 conf

--- manifests/nodes/dns.pp
+++ manifests/nodes/dns.pp
@@ -11,14 +11,13 @@ node 'dns.upr.edu.cu'{
 node 'ns1.upr.edu.cu'{
   package { 'lsb-release':
           ensure => installed,
-  }#~>
-  #  class { '::basesys':
-  #uprinfo_usage  => 'servidor dns externo',
-  #  application    => 'DNS Bind9',
-  #  puppet_enabled => false,
-  #  repos_enabled  => true,
-  #  mta_enabled    => false,
-  #}
-
+  }~>
+  class { '::basesys':
+    uprinfo_usage  => 'servidor dns externo',
+    application    => 'DNS Bind9',
+    puppet_enabled => false,
+    repos_enabled  => true,
+    mta_enabled    => false,
+  }
 }
 
