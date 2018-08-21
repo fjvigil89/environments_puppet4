2c4879241cdba23b78aa592eb2a111ba9c43e2dd
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-21 14:25:24 -0400

    fix error

--- roles/dns_secundary/manifests/init.pp
+++ roles/dns_secundary/manifests/init.pp
@@ -6,18 +6,17 @@ class dns_secundary {
 include bind
 bind::server::conf { '/etc/named.conf':
   listen_on_addr    => [ 'any' ],
-  listen_on_v6_addr => [ 'any' ],
   forwarders        => [ '10.2.1.8', ],
   allow_query       => [ 'any' ],
   zones             => {
     'upr.edu.cu' => [
       'type slave',
-      'masters { 10.2.1.8;}';
+      'masters { 10.2.1.8 }';
       'file "db.upr.edu.cu"',
     ],
     '1.2.10.in-addr.arpa' => [
       'type slave',
-      'masters {10.2.1.8;}'
+      'masters { 10.2.1.8 }'
       'file "db.1.2.10.in-addr.arpa"',
     ],
   },
