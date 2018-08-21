2aedf6e3a638e6f7147bf70ee296338d8e9508a6
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-21 17:35:19 -0400

    uffff

--- profiles/dns_bind/manifests/init.pp
+++ profiles/dns_bind/manifests/init.pp
@@ -1,14 +1,14 @@
 # Class: dns_bind
 # ===========================
-#
+# 
 # Full description of class dns_bind here.
 #
 class dns_bind (
   Array[String] $zone_name       = $::dns_bind::params::zone_name,
   Array[String] $zone_reverse    = $::dns_bind::params::zone_reverse,
-  String zone_type               = $::dns_bind::params::zone_type,
-  String recursion               = $::dns_bind::params::recursion,
-  String notify                  = $::dns_bind::params::notify,
+  String $zone_type              = $::dns_bind::params::zone_type,
+  String $recursion              = $::dns_bind::params::recursion,
+  String $notify                 = $::dns_bind::params::notify,
   Array[String] $mymasters       = $::dns_bind::params::mymasters,
   Array[String] $mymatch_clients = $::dns_bind::params::mymatch_clients,
 ) inherits ::dns_bind::params {
