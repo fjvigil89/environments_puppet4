32a84db7526660920b3954a6c9239bf5861c049c
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-21 17:08:20 -0400

    test dns_bind module

--- manifests/nodes/puppet_host.pp
+++ manifests/nodes/puppet_host.pp
@@ -23,15 +23,16 @@ node 'puppet-test.upr.edu.cu'{
     repos_enabled  => true,
   }
   #include nfs_client
-  class { 'freeradius':
-  max_requests    => 4096,
-  max_servers     => 4096,
-  mysql_support   => true,
-  perl_support    => true,
-  utils_support   => true,
-  wpa_supplicant  => true,
-  winbind_support => true,
-  syslog          => true,
-  log_auth        => 'yes',
-}
+  # class { 'freeradius':
+  #max_requests    => 4096,
+  #max_servers     => 4096,
+  #mysql_support   => true,
+  #perl_support    => true,
+  #utils_support   => true,
+  #wpa_supplicant  => true,
+  #winbind_support => true,
+  #syslog          => true,
+  #log_auth        => 'yes',
+  #}
+  class {'::dns_bind':}
 }
---
+++ profiles/dns_bind/manifests/init.pp
@@ -0,0 +1,47 @@
+# Class: dns_bind
+# ===========================
+#
+# Full description of class dns_bind here.
+#
+class dns_bind (
+  Array[String] $zone_name       = $::dns_bind::params::zone_name,
+  Array[String] $zone_reverse    = $::dns_bind::params::zone_reverse,
+  String zone_type               = $::dns_bind::params::zone_name,
+  String recursion               = $::dns_bind::params::zone_name::recursion,
+  String notify                  = $::dns_bind::params::zone_name::notify,
+  Array[String] $mymasters       = $::dns_bind::params::zone_name,
+  Array[String] $mymatch_clients = $::dns_bind::params::mymatch_clients,
+) inherits ::dns_bind::params {
+bind::server::conf {
+  $zone_name.each |String $value| {
+  zones => {
+    $value => [
+      $zone_type,
+      "file db.$value",
+    ],
+  },
+  }
+  views => {
+    'trusted' => {
+      'match-clients' => $mymatch_clients,
+      'zones'         => {
+        $zone_name.each |String $value| {
+          $value => [
+            $zone_type,
+            "file db.$zone_name",
+        ],
+        }
+      },
+      $zone_reverse.each |String $value| {
+        $value  => [
+          $zone_type,
+          "file db.$value",
+        ],
+      }
+    },
+    'default' => {
+      'match-clients' => [ 'any' ],
+    },
+  },
+}
+}
