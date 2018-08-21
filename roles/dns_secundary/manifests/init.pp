80135c0b3a2dc29efded612ee86e5bab0b260173
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-21 14:27:45 -0400

    f error

--- roles/dns_secundary/manifests/init.pp
+++ roles/dns_secundary/manifests/init.pp
@@ -11,12 +11,12 @@ bind::server::conf { '/etc/named.conf':
   zones             => {
     'upr.edu.cu' => [
       'type slave',
-      'masters { 10.2.1.8 }';
+      'masters { 10.2.1.8; }',
       'file "db.upr.edu.cu"',
     ],
     '1.2.10.in-addr.arpa' => [
       'type slave',
-      'masters { 10.2.1.8 }'
+      'masters { 10.2.1.8; }',
       'file "db.1.2.10.in-addr.arpa"',
     ],
   },
