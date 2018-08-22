42b206249a1b026ae13cc26dfa37678d8cee912c
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-22 12:38:15 -0400

    test bind zone

--- profiles/dns_bind/manifests/init.pp
+++ profiles/dns_bind/manifests/init.pp
@@ -13,20 +13,26 @@ class dns_bind (
   Array[String] $mymatch_clients = $::dns_bind::params::mymatch_clients,
 ) inherits ::dns_bind::params {
 include bind
-bind::server::conf { '/etc/named.conf':
-  listen_on_addr    => [ 'any' ],
-  listen_on_v6_addr => [ 'any' ],
-  forwarders        => [ '8.8.8.8', '8.8.4.4' ],
-  allow_query       => [ 'localnets' ],
-  zones             => {
-    'myzone.lan' => [
+bind::server::conf {
+  zones => {
+    'example.com' => [
       'type master',
-      'file "myzone.lan"',
+      'file "example.com"',
     ],
-    '1.168.192.in-addr.arpa' => [
-      'type master',
-      'file "1.168.192.in-addr.arpa"',
-    ],
+  },
+  views => {
+    'trusted' => {
+      'match-clients' => [ '192.168.23.0/24' ],
+      'zones' => {
+        'myzone.lan' => [
+          'type master',
+          'file "myzone.lan"',
+        ],
+      },
+    },
+    'default' => {
+      'match-clients' => [ 'any' ],
+    },
   },
 }
 }
