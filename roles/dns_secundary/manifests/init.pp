07a764f9dcb86cbc4d0fff951b8b310213775da8
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-23 14:17:02 -0400

    directory

--- roles/dns_secundary/manifests/init.pp
+++ roles/dns_secundary/manifests/init.pp
@@ -7,6 +7,7 @@ bind::server::conf { '/etc/bind/named.conf.options':
   listen_on_addr => [ '10.2.1.205','localhost' ],
   forwarders     => [ '10.2.1.8' ], 
   allow_query    => [ '10.2.0.0/15' ],
+  directory      => '/var/cache/bind',
 }
 }
 
