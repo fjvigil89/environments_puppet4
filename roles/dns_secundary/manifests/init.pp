46ddbd9587bc67180a0e88b17bddc454b1917291
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-23 13:49:18 -0400

    erer

--- roles/dns_secundary/manifests/init.pp
+++ roles/dns_secundary/manifests/init.pp
@@ -4,9 +4,9 @@
 class dns_secundary {
 include bind
 bind::server::conf { '/etc/bind/named.conf.options':
-  forwarders  => [ '10.2.1.8' ], 
-  allow_query => [ '10.2.0.0/15' ],
-  directory   => '/var/cache/bind/',
+  listen_on_addr => [ '10.2.1.13','localhost' ],
+  forwarders     => [ '10.2.1.8' ], 
+  allow_query    => [ '10.2.0.0/15' ],
 }
 }
 
