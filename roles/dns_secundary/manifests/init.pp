517665c95258cb8fa376206713884f9297466b8a
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-23 12:48:33 -0400

    fg

--- roles/dns_secundary/manifests/init.pp
+++ roles/dns_secundary/manifests/init.pp
@@ -3,10 +3,7 @@
 #   
 class dns_secundary {
 include bind
-bind::server::conf { '/etc/named.conf':
-  listen_on_addr    => [ 'any' ],
-  forwarders        => [ '10.2.1.8' ],
-  allow_query       => [ 'any' ],
+bind::server::conf {
   zones             => {
     'upr.edu.cu' => [
       'type slave',
@@ -40,6 +37,42 @@ bind::server::conf { '/etc/named.conf':
     ],
   },
 }
+views => {
+    'trusted' => {
+      'match-clients' => [ '10.2.0.0/15' ],
+      'zones' => {
+     'upr.edu.cu' => [
+      'type slave',
+      'masters { 10.2.1.8; }',
+      'file "db.upr.edu.cu"',
+    ],
+    'ceces.upr.edu.cu' => [
+      'type slave',
+      'masters { 10.2.1.8; }',
+      'file "db.ceces.upr.edu.cu"',
+    ],
+    'progintec.upr.edu.cu' => [
+      'type slave',
+      'masters { 10.2.24.158; }',
+      'file "db.progintec.upr.edu.cu"',
+    ],
+    '1.2.10.in-addr.arpa' => [
+      'type slave',
+      'masters { 10.2.1.8; }',
+      'file "db.1.2.10.in-addr.arpa"',
+    ],
+    '22.2.10.in-addr.arpa' => [
+      'type slave',
+      'masters { 10.2.1.8; }',
+      'file "db.22.2.10.in-addr.arpa"',
+    ],
+
+      },
+'default' => {
+      'match-clients' => [ 'any' ],
+    },
+    },
+
 bind::server::file { 'db.upr.edu.cu':
   source => 'puppet:///modules/dns_secundary/dns/db.upr.edu.cu',
   mode   => '0770',
