8e80d49ffedd49a06e6487383d0ca01cdc2b7ee1
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-23 18:19:17 -0400

    fg

--- roles/dns_secundary/manifests/init.pp
+++ roles/dns_secundary/manifests/init.pp
@@ -3,24 +3,12 @@
 ####   
 class dns_secundary {
 include bind
-#bind::server::conf { '/etc/named.conf':
-#listen_on_addr => ['10.2.1.13','localhost'],
-#  directory      => '/var/cache/bind',
-#  recursion      => 'yes',
-#  forwarders     => [ '10.2.1.8' ],
-#  allow_query    => [ '10.2.0.0/15' ],
-#}
 bind::server::conf { '/etc/named.conf':
-       allow_query => [ '10.2.0.0/15' ],
-       'zones' => {
-         'tele4.upr.edu.cu' => [
-           'type master',
-           'file "db.tele4.upr.edu.cu"',
-         ],
-      }, 
-}
-bind::server::file { 'db.tele4.upr.edu.cu':
-  source => 'puppet:///modules/dns_secundary/dns/db.tele4.upr.edu.cu',
+listen_on_addr => ['10.2.1.13','localhost'],
+  directory      => '/var/cache/bind',
+  recursion      => 'yes',
+  forwarders     => [ '10.2.1.8' ],
+  allow_query    => [ '10.2.0.0/15' ],
 }
 }
 
