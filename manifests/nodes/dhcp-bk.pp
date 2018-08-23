502a692fc76662f2a0a9113b14d616f7ed0a78ef
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-08-23 00:25:28 -0400

    listo para prueba con server dhcp-bk

---
+++ manifests/nodes/dhcp-bk.pp
@@ -0,0 +1,22 @@
+node 'dhcp-bk.upr.edu.cu' { 
+  #este se llama desde roles
+  include dhcpprodserver
+
+  #este desde profile
+  class {'dhcpserver':
+      interfaces   => ['eth0'],
+      pool_enabled => true,
+      pool         => ['Assets','Trabajadores','Estudiantes'],
+      network      => ['10.2.132.0','10.2.133.0','10.2.134.0'],
+      mask         => ['255.255.255.0','255.255.255.0','255.255.255.0'],
+      range        => ['10.2.132.10 10.2.132.254','10.2.133.2 10.2.133.254','10.2.134.2 10.2.134.254'],
+      gateway      => ['10.2.132.1','10.2.133.1','10.2.134.1'],
+      host_enabled => false,
+      host         => ['assets.upr.edu.cu'],
+      comment      => ['Servidor Assets para Contabilidad'],
+      mac          => ['72:92:c5:24:74:e4'],
+      ip           => ['10.2.132.101']
+    }
+  
+
+}
