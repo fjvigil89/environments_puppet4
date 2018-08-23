928372cea05f05e82f08a519c64d90f018e347b3
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-23 16:58:46 -0400

    test view zone dns

--- profiles/dns_bind/manifests/params.pp
+++ profiles/dns_bind/manifests/params.pp
@@ -4,11 +4,12 @@
 # Full description of class dns_bind::params here.
 #
 class dns_bind::params {
-    $zone_name       = ['upr.edu.cu','ceces.upr.edu.cu']
-    $zone_reverse    = ['1.2.10.in-addr.arpa','22.2.10.in-addr.arpa']
-    $zone_type       = 'type master'
-    #$recursion       = 'yes'
-    #$notify          = 'yes'
-    $mymasters       = [ '192.168.0.1', '192.168.0.2']
-    $mymatch_clients = [ '10.2.0.0/15', '200.14.49.0/24']
+    $zone_name        = []
+    $zone_reverse     = []
+    $zone_type        = 'type master'
+    $recursion        = 'yes'
+    $listen_on_addr   = []
+    $mymasters        = []
+    $mymatch_clients  = [ '10.2.0.0/15' ]
+    $dns_master       = 'true'
 }
--- roles/dns_secundary/manifests/init.pp
+++ roles/dns_secundary/manifests/init.pp
@@ -3,13 +3,34 @@
 ####   
 class dns_secundary {
 include bind
-bind::server::conf { '/etc/named.conf':
-  listen_on_addr => ['10.2.1.13','localhost'],
-  directory      => '/var/cache/bind',
-  recursion      => 'yes',
-  forwarders     => [ '10.2.1.8' ],
-  allow_query    => [ '10.2.0.0/15' ],
-
+#bind::server::conf { '/etc/named.conf':
+#listen_on_addr => ['10.2.1.13','localhost'],
+#  directory      => '/var/cache/bind',
+#  recursion      => 'yes',
+#  forwarders     => [ '10.2.1.8' ],
+#  allow_query    => [ '10.2.0.0/15' ],
+#}
+bind::server::conf {
+  zones => {
+    'example.com' => [
+      'type master',
+      'file "example.com"',
+    ],
+  },
+  views => {
+    'trusted' => {
+      'match-clients' => [ '192.168.23.0/24' ],
+      'zones' => {
+        'myzone.lan' => [
+          'type master',
+          'file "myzone.lan"',
+        ],
+      },
+    },
+    'default' => {
+      'match-clients' => [ 'any' ],
+    },
+  },
 }
 }
 
