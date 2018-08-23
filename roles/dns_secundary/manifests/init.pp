1ea830bd61a2056fe128186498d49d8c661f339f
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-23 16:22:44 -0400

    test

--- roles/dns_secundary/manifests/init.pp
+++ roles/dns_secundary/manifests/init.pp
@@ -4,26 +4,13 @@
 class dns_secundary {
 include bind
 bind::server::conf { '/etc/named.conf':
-  listen_on_addr    => [ 'any' ],
-  listen_on_v6_addr => [ 'any' ],
-  forwarders        => [ '10.2.1.13' ],
-  allow_query       => [ 'localnets' ],
-  zones             => {
-    'upr.edu.cu' => [
-      'type master',
-      'file "db.upr.edu.cu"',
-    ],
-    '1.2.10.in-addr.arpa' => [
-      'type master',
-      'file "db.1.2.10.in-addr.arpa"',
-    ],
-  },
-}
-bind::server::file { 'db.upr.edu.cu':
-  source => 'puppet:///modules/dns_secundary/dns/db.upr.edu.cu',
-}
-bind::server::file { 'db.1.2.10.in-addr.arpa':
-  source => 'puppet:///modules/dns_secundary/dns/db.1.2.10.in-addr.arpa',
+  listen_on_addr => ['10.2.1.13','localhost'],
+  directory      => '/var/cache/bind',
+  recursion      => 'yes',
+  forwarders     => [ '10.2.1.8' ],
+  allow_query    => [ '10.2.0.0/15' ],
+  forward        => 'only',
+
 }
 }
 
