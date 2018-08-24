407d53482a23a0b7bebb538dba053ef9381aad89
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-08-24 16:15:12 -0400

    enable basesys repos-facultades

--- roles/smb_rclient/manifests/init.pp
+++ roles/smb_rclient/manifests/init.pp
@@ -4,13 +4,13 @@
 # Full description of class smb_rclient here.
 
 class smb_rclient {
-  #  class { '::basesys':
-  #  uprinfo_usage  => 'servidor NOC',
-  #  application    => 'Noc Server UPR',
-  #  puppet_enabled => false,
-  #  repos_enabled  => true,
-  #  mta_enabled    => false,
-  #}
+    class { '::basesys':
+    uprinfo_usage  => 'servidor NOC',
+    application    => 'Noc Server UPR',
+    puppet_enabled => false,
+    repos_enabled  => true,
+    mta_enabled    => false,
+  }
   class { '::samba_client':
      shares_name    => 'Informatica',
      shares_comment => 'Repositorio de Informatica',
