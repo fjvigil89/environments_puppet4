1934ffc39234b9aba1eb54ba1c17088b55dcc32f
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-08-24 17:58:55 -0400

    probando nueva config de dns

--- roles/dns_secundary/manifests/cache.pp
+++ roles/dns_secundary/manifests/cache.pp
@@ -2,22 +2,22 @@
 # ===========================
 #### 
 class dns_secundary::cache(){
- bind::server::conf { $::dns_secundary::config_file :
-  listen_on_addr    => $::dns_secundary::listen_on_addr,
-  listen_on_v6_addr => $::dns_secundary::listen_on_v6_addr,
-  forwarders        => $::dns_secundary::forwarders,
-  allow_query       => $::dns_secundary::allow_query,
 
-  zones             => $::dns_secundary::zone_name.each |String $value| {
+  $zones = $::dns_secundary::zone_name.each |String $value| {
     $value = [
         $::dns_secundary::zone_type,
         "file db.$value",
       ],
+  }
 
-  },
-
-  #zones             => {
-  #  'myzone.lan' => [
+ bind::server::conf { $::dns_secundary::config_file :
+  listen_on_addr              => $::dns_secundary::listen_on_addr,
+  listen_on_v6_addr           => $::dns_secundary::listen_on_v6_addr,
+  forwarders                  => $::dns_secundary::forwarders,
+  allow_query                 => $::dns_secundary::allow_query,
+  zones                       => $zones,
+  #zones                      => {
+  #  'myzone.lan'             => [
   #    'type master',
   #    'file "myzone.lan"',
   #  ],
