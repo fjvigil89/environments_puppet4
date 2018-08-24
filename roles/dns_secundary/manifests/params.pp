4bddc24fa42a43500d65721a89400a570b48fb10
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-08-24 18:10:43 -0400

    probando nueva config de dns

--- roles/dns_secundary/manifests/params.pp
+++ roles/dns_secundary/manifests/params.pp
@@ -7,7 +7,7 @@ class dns_secundary::params{
     $config_file        = '/etc/named.conf'
     $listen_on_addr     = [ 'any' ]
     $listen_on_v6_addr  = [ 'any' ]
-    $forwarders      = [undef]
+    $forwarders      = ['10.2.1.8']
     $allow_query     = [ 'any' ]
     $zone_name       = ['upr.edu.cu','ceces.upr.edu.cu']
     $zone_reverse    = ['1.2.10.in-addr.arpa','22.2.10.in-addr.arpa']
