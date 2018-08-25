07a765344482e5d5cf02cac3df0b92ad3023025d
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-08-25 11:26:03 -0400

    dns en prod

--- roles/dns_secundary/manifests/cache.pp
+++ roles/dns_secundary/manifests/cache.pp
@@ -11,20 +11,20 @@ class dns_secundary::cache(){
   forwarders               => $::dns_secundary::forwarders,
   allow_query              => $::dns_secundary::allow_query,
   directory                => $::dns_secundary::directory,
-  #zones                   => {
-  #  'upr.edu.cu'          => [
-  #    'type master',
-  #    'file "db.upr.edu.cu"',
-  #  ],
-  #  '1.2.10.in-addr.arpa' => [
-  #    'type master',
-  #    'file "db.1.2.10.in-addr.arpa"',
-  #  ],
-  #},
+  zones                   => {
+    'upr.edu.cu'          => [
+      'type master',
+      'file "db.upr.edu.cu"',
+    ],
+    '1.2.10.in-addr.arpa' => [
+      'type master',
+      'file "db.1.2.10.in-addr.arpa"',
+    ],
+  },
 }
 
-#bind::server::file { $::dns_secundary::file_zone_name :
-#    source_base =>  'puppet:///modules/dns_secundary/dns/',
-#  }
+bind::server::file { $::dns_secundary::file_zone_name :
+    source_base =>  'puppet:///modules/dns_secundary/dns/',
+  }
 
 }
