ed9af0daba6f67498d0bd455e7324650e33444b8
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-08-25 18:02:51 -0400

    bind new

--- roles/dns_secundary/manifests/init.pp
+++ roles/dns_secundary/manifests/init.pp
@@ -25,7 +25,7 @@ class dns_secundary (
     repos_enabled  => true,
     mta_enabled    => false,
     dns_enabled    => false,
-  }~> class {'::bind_server':;}~> class {'::dns_secundary::cache':;}
+  }~> class {'bind_server':;}~> class {'::dns_secundary::cache':;}
 
 }
 
