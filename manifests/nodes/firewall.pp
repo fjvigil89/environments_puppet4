5be595eee3036073f0a8d11b4ec6a0e6ba6840c9
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-09-01 17:56:56 -0400

    add conf firewall new

--- manifests/nodes/firewall.pp
+++ manifests/nodes/firewall.pp
@@ -8,3 +8,14 @@ node 'firewall.upr.edu.cu' {
   }
 }
 
+node 'firewall1.upr.edu.cu' {
+  class { '::basesys':
+    uprinfo_usage  => 'servidor firewall',
+    application    => 'Firewall IPTABLES Server UPR',
+    puppet_enabled => false,
+    repos_enabled  => true,
+    mta_enabled    => false,
+  }
+}
+
+
