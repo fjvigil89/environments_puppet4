cf6aa8deec9489d2027d6438cfa7b5ae24522e09
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-09-01 10:37:32 -0400

    cambios en puppet

--- manifests/nodes/puppet_host.pp
+++ manifests/nodes/puppet_host.pp
@@ -27,7 +27,7 @@ node 'client-puppet.upr.edu.cu'{
   #    mac          => ['72:92:c5:24:74:e4'],
   #    ip           => ['10.2.202.3']
   #  }
-  #include dns_secundary
+  ##include dns_secundary
 
 }
 
