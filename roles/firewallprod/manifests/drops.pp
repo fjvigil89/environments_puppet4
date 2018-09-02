0cf57f5deaef8846635e1aea6c0c0b82c5723b25
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-09-02 17:30:02 -0400

    test

--- roles/firewallprod/manifests/drops.pp
+++ roles/firewallprod/manifests/drops.pp
@@ -6,14 +6,14 @@
 class firewallprod::drops {
 if($::firewallprod::drop_both){
   $::firewallprod::hosts_todrop.each |String $host|{
-    firewall { 'Source DROP $host':
+    firewall { "Source DROP ${host}":
       chain  => 'INPUT',
       action => 'drop',
       source => $host,
     }
   }
 $::firewallprod::hosts_todrop.each |String $host|{
-firewall { 'Destination DROP $host':
+firewall { "Destination DROP ${host}":
       chain       => 'INPUT',
       action      => 'drop',
       destination => $host,
