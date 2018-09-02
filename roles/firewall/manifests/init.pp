4bb3234d420191552b6a546b5a5e67ce835b979b
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-09-02 15:49:28 -0400

    test

--- roles/firewall/manifests/init.pp
+++ roles/firewall/manifests/init.pp
@@ -5,4 +5,12 @@
 #
 class firewall {
   class { '::server_firewall': }
+  firewall { '100 snat for network foo2':
+  chain    => 'POSTROUTING',
+  jump     => 'MASQUERADE',
+  proto    => 'all',
+  outiface => 'eth0',
+  source   => '10.1.2.0/24',
+  table    => 'nat',
+}
 }
