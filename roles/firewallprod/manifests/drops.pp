b31db77944bdc1460c389b5cf75b8b4888e3f3c4
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-09-02 16:36:51 -0400

    g

--- roles/firewallprod/manifests/drops.pp
+++ roles/firewallprod/manifests/drops.pp
@@ -4,11 +4,11 @@
 # Full description of class firewallprod::drops here.
 #
 class firewallprod::drops {
-    each($::firewallprod::host_todrop) |Integer $index, String $host|{
-      firewall { $::firewallprod::host_todrop[$index] :
-        chain  => 'INPUT',
-        action => 'drop',
-        source => $::firewallprod::host_todrop[$index],
-      }
+  $::firewallprod::host_todrop.each |Integer $index, String $host|{
+    firewall { $::firewallprod::host_todrop[$index]:
+      chain  => 'INPUT',
+      action => 'drop',
+      source => $::firewallprod::host_todrop[$index],
     }
+  }
 }
