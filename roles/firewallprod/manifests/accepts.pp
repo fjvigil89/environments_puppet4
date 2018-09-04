28314d56b32b3b20d76b9d9c49fea1e088934bf6
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-09-04 14:39:09 -0400

    Fg

--- roles/firewallprod/manifests/accepts.pp
+++ roles/firewallprod/manifests/accepts.pp
@@ -4,25 +4,21 @@
 # Full description of class firewallprod::accepts here.
 #
 class firewallprod::accepts {
-  #Line Value
-  $line = 0;
   # FROM Reduniv ACCEPT
-  $::firewallprod::hosts_toaccept.each |String $host|{
-    firewall { "$line $host ACCEPT FROM RedUniv":
+  $::firewallprod::hosts_toaccept.each |String $host, Integer $index|{
+    firewall { "$index $host ACCEPT FROM RedUniv":
       chain  => $::firewallprod::chain_from,
       action => 'accept',
       source => $host,
     }
-    $line = $line + 1
   }
   #To RedUniv ACCEPT
-  $::firewallprod::hosts_toaccept.each |String $host|{
-    firewall { "$line $host ACCEPT to RedUniv":
+  $::firewallprod::hosts_toaccept.each |String $host, Integer $index|{
+    firewall { "$index $host ACCEPT to RedUniv":
       chain       => $::firewallprod::chain_to,
       action      => 'accept',
       destination => $host,
     }
-    $line = $line + 1
   }
 
 
