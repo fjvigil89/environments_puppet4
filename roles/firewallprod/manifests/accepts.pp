71b79f3fc92a7dc5eb30f1a6a2a41b15e2c9e2d8
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-09-04 14:34:41 -0400

    test increment

--- roles/firewallprod/manifests/accepts.pp
+++ roles/firewallprod/manifests/accepts.pp
@@ -4,21 +4,25 @@
 # Full description of class firewallprod::accepts here.
 #
 class firewallprod::accepts {
+  #Line Value
+  $line = 0;
   # FROM Reduniv ACCEPT
   $::firewallprod::hosts_toaccept.each |String $host|{
-    firewall { "$host ACCEPT FROM RedUni":
+    firewall { "$line $host ACCEPT FROM RedUniv":
       chain  => $::firewallprod::chain_from,
       action => 'accept',
       source => $host,
     }
+    $line = $line + 1
   }
   #To RedUniv ACCEPT
   $::firewallprod::hosts_toaccept.each |String $host|{
-    firewall { "$host ACCEPT to RedUniv":
+    firewall { "$line $host ACCEPT to RedUniv":
       chain       => $::firewallprod::chain_to,
       action      => 'accept',
       destination => $host,
     }
+    $line = $line + 1
   }
 
 
