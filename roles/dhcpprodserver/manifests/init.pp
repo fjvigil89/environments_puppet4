2f38a55321a2dd007aee92e868a1089b83eddb5f
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-09-04 18:54:46 -0400

    Add dhcp server upr conf

--- roles/dhcpprodserver/manifests/init.pp
+++ roles/dhcpprodserver/manifests/init.pp
@@ -16,10 +16,10 @@ class dhcpprodserver(){
      ensure =>  installed,
   }~>
   class { '::basesys':
-  uprinfo_usage  => 'servidor dhcp',
-  application    => 'production',
-  puppet_enabled => false,
-  mta_enabled    => false,  
+    uprinfo_usage  => 'servidor dhcp',
+    application    => 'production',
+    puppet_enabled => true,
+    mta_enabled    => false,  
   }
 
 }
