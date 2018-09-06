9a5cc699ede1079408b99b50313881aeab7b7325
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-09-06 10:50:30 -0400

    install roundcube 3

--- manifests/nodes/puppet_host.pp
+++ manifests/nodes/puppet_host.pp
@@ -36,18 +36,7 @@ node 'client-puppet.upr.edu.cu'{
   user     => 'roundcube',
   password => postgresql_password('roundcube', 'secret'),
  }
- class { 'roundcube':
-    db_type     => 'pgsql',
-    db_name     => 'roundcube',
-    db_host     => 'localhost',
-    db_username => 'roundcube',
-    db_password => 'secret',
-    plugins => [  
-     'emoticons',
-      'markasjunk',
-      'password',
-    ],
-  }
+ class { 'roundcube':}
 
 }
 
