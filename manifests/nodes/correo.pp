69260dff57eda4a912767263fdfed6d408493f20
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-09-06 17:15:36 -0400

    change dns-cacehe0 /22 to /24

--- manifests/nodes/correo.pp
+++ manifests/nodes/correo.pp
@@ -2,7 +2,7 @@ node 'correo-profe.upr.edu.cu' {
   class { '::basesys':
     uprinfo_usage  => 'servidor correo fisico',
     application    => 'Zimbra',
-    puppet_enabled => true,
+    puppet_enabled => false,
     repos_enabled  => true,
     mta_enabled    => false,
   }
