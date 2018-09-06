1d6922aded92c18e2b4df46a98890871712ed0ee
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-09-06 13:52:00 -0400

    creando repos2

--- manifests/nodes/repos-facultades.pp
+++ manifests/nodes/repos-facultades.pp
@@ -1,46 +1,59 @@
 node 'repofct.upr.edu.cu' {
   include repoFacult_server
  
+  file { "/repositorio" :
+ 		ensure => 'directory',
+		owner  => 'root',
+		mode   => '0777',
+  }
+  mount {'/repositorio':
+  	device  => '10.2.25.1:/export/repos_facultades',
+  	fstype  => 'nfs4',
+ 		ensure  => 'mounted',
+ 		options => 'default',
+ 		atboot  => true,
+ 	}  
+ 
   class { '::samba_client':
-  ¦shares_name    => 'Telecomunicaciones',
-  ¦shares_comment => 'Repositorio de Telecomunicaciones',
-  ¦shares_path    => '/repositorio/Telecomunicaciones',
-  ¦valid_users    => ['tele',],
-  ¦writable       => 'yes',
-  ¦browseable     => 'yes',
+  	shares_name    => 'Telecomunicaciones',
+  	shares_comment => 'Repositorio de Telecomunicaciones',
+  	shares_path    => '/repositorio/Telecomunicaciones',
+  	valid_users    => ['tele',],
+  	writable       => 'yes',
+  	browseable     => 'yes',
   }
   
   class { '::apache':
-  ¦ default_vhost => false,
-  ¦ mpm_module    => 'prefork',
+  	default_vhost => false,
+  	mpm_module    => 'prefork',
   }
 
   apache::vhost { 'repotele.upr.edu.cu non-ssl':
-    servername    => 'repotele.upr.edu.cu',
-    serveraliases => ['www.repotele.upr.edu.cu'], 
-    port          => '80',
-    docroot           => '/repositorio/Telecomunicaciones',
-    directories   => [ {
-      'path'           => '/repositorio/Telecomunicaciones',
-      'options'        => ['Indexes','FollowSymLinks','MultiViews'],
-      'allow_override' => 'All',
-      'directoryindex' => 'index.php',
-    },],
-      redirect_status  => 'permanent',
-      redirect_dest    => 'https://repotele.upr.edu.cu/',
+  	servername    => 'repotele.upr.edu.cu',
+  	serveraliases => ['www.repotele.upr.edu.cu'], 
+  	port          => '80',
+  	docroot           => '/repositorio/Telecomunicaciones',
+  	directories   => [ {
+  		'path'           => '/repositorio/Telecomunicaciones',
+  		'options'        => ['Indexes','FollowSymLinks','MultiViews'],
+  		'allow_override' => 'All',
+  		'directoryindex' => 'index.php',
+  	},],
+  		redirect_status  => 'permanent',
+  		redirect_dest    => 'https://repotele.upr.edu.cu/',
   }~>
   apache::vhost { 'repotele.upr.edu.cu ssl':
-    servername    => 'repotele.upr.edu.cu',
-    serveraliases =>  ['www.repotele.upr.edu.cu'],
-    port          => '443',
-    docroot       => '/repositorio/Telecomunicaciones',
-    ssl           => true,
-    directories =>  [ {
-      'path'           => '/repositorio/Telecomunicaciones',
-      'options'        => ['Indexes','FollowSymLinks','MultiViews'],
-      'allow_override' => 'All',    
-      'directoryindex' => 'index.php',
-    },],
+  	servername    => 'repotele.upr.edu.cu',
+  	serveraliases =>  ['www.repotele.upr.edu.cu'],
+  	port          => '443',
+  	docroot       => '/repositorio/Telecomunicaciones',
+  	ssl           => true,
+  	directories =>  [ {
+  		'path'           => '/repositorio/Telecomunicaciones',
+  		'options'        => ['Indexes','FollowSymLinks','MultiViews'],
+  		'allow_override' => 'All',    
+  		'directoryindex' => 'index.php',
+  	},],
   } 
 
 }
--- roles/repoFacult_server/manifests/init.pp
+++ roles/repoFacult_server/manifests/init.pp
@@ -11,17 +11,18 @@ class repoFacult_server {
     mta_enabled    => false,
   }
 
-  file { '/repositorio':
-  ¦ ensure => 'directory',
-  ¦ owner  => 'root',
-  ¦ mode   => '0777',
-  }
-  mount {'/repositorio':
-    ¦device  => '10.2.25.1:/export/repos_facultades',
-    ¦fstype  => 'nfs4',
-    ¦ensure  => 'mounted',
-    ¦options => 'default',
-    ¦atboot  => true,
-  }
+ 
+  #file { "/repositorio" :
+  #¦ ensure => 'directory',
+  #¦ owner  => 'root',
+  #¦ mode   => '0777',
+  #}
+  #mount {'/repositorio':
+  #  ¦device  => '10.2.25.1:/export/repos_facultades',
+  #  ¦fstype  => 'nfs4',
+  #  ¦ensure  => 'mounted',
+  #  ¦options => 'default',
+  #  ¦atboot  => true,
+  #}
    
 }
