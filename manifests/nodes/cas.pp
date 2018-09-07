5172e5e69277b88f2ac1ba76823b1b77ca2be230
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-09-07 09:13:23 -0400

    Servicio cas

---
+++ manifests/nodes/cas.pp
@@ -0,0 +1,10 @@
+node 'cas.upr.edu.cu' {  
+  package { 'lsb-release':
+          ensure => installed,
+  }~>
+  class { '::basesys':
+    uprinfo_usage  => 'Servidor CAS'
+    application    => 'Servidor CAS para authentication',
+    repos_enabled  => true,
+  }
+}
