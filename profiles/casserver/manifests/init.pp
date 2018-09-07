82efefdadf55cd87e78b240f4ca52525d1dfa51d
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-09-07 12:58:50 -0400

    Servicio CAS

--- profiles/casserver/manifests/init.pp
+++ profiles/casserver/manifests/init.pp
@@ -5,6 +5,9 @@
 class casserver {
 
  class { 'cas': 
-   server_name => 'https://cas.upr.edu.cu'
+   server_name        => 'http://cas.upr.edu.cu',
+   ldap_url           => 'ldap://10.2.24.35',
+   ldap_base_dn       => 'DC=upr,DC=edu,DC=cu',
+   ldap_search_filter => 'cn={sAMAccountName}'
  }
 }
