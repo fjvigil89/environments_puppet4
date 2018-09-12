4a334e27f7bec3039d9968fea40eb61e64703d5b
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-09-12 12:46:53 -0400

    add correo estudiantes node conf

--- manifests/nodes/correo.pp
+++ manifests/nodes/correo.pp
@@ -1,9 +1,19 @@
 node 'correo-profe.upr.edu.cu' {  
   class { '::basesys':
-    uprinfo_usage  => 'servidor correo fisico',
+    uprinfo_usage  => 'servidor correo profesores fisico',
     application    => 'Zimbra',
     puppet_enabled => false,
     repos_enabled  => true,
     mta_enabled    => false,
   }
 }
+
+ node 'correo-est.upr.edu.cu' {
+  class { '::basesys':
+    uprinfo_usage  => 'servidor correo estudiantes KVM',
+    application    => 'Zimbra',
+    puppet_enabled => false,
+    repos_enabled  => true,
+  ¦ mta_enabled    => false,
+  }
+ }
