8ccc4238a3a6a4a77ca27feac0d3a88f8c4293ca
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-09-14 16:06:54 -0400

    repos 33

--- profiles/samba_client/manifests/init.pp
+++ profiles/samba_client/manifests/init.pp
@@ -13,19 +13,30 @@ class samba_client (
   String $browseable         = $::samba_client::params::browseable,
   String $path_nfs           = $::samba_client::params::path_nfs,
 ) inherits samba_client::params {
-
-  $share =  each($::samba_client::shares_name) |Integer $index, String $value|{
-    $value = { 
-      'comment'     => "$shares_comment $shares_name[$index]",
-      'path'        => "$path_nfs $shares_path[$index]",
-      'valid users' => $valid_users[$index],
-      'writable'    => $writable,
-      'browseable'  => $browseable,
-    }
-  }
+  
+  #  $share =  each($::samba_client::shares_name) |Integer $index, String $value|{
+  #  $value = { 
+  #    'comment'     => "$shares_comment $shares_name[$index]",
+  #    'path'        => "$path_nfs $shares_path[$index]",
+  #    'valid users' => $valid_users[$index],
+  #    'writable'    => $writable,
+  #    'browseable'  => $browseable,
+  #  }
+  #}
+  
+  # }
   
 
   class { 'samba':
-    shares_definitions => $share,
+    shares_definitions => {
+      'share' => {
+        'comment'     => "$shares_comment $shares_name[0]",
+        'path'        => "$path_nfs $shares_path[0]",
+        'valid users' => $valid_users[0],
+        'writable'    => $writable,
+        'browseable'  => $browseable,
+
+      }
+    }
   }
 }
