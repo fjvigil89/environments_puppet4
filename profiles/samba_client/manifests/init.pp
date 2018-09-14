7b3e5968ffe761ee82e8adee1f15bec44e109220
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-09-14 16:42:48 -0400

    repos 43

--- profiles/samba_client/manifests/init.pp
+++ profiles/samba_client/manifests/init.pp
@@ -5,38 +5,41 @@
 #
 
 class samba_client (
-  String $shares_comment     = $::samba_client::params::shares_comment,
+  String $shares_comment            = $::samba_client::params::shares_comment,
   Array[String] $shares_name        = $::samba_client::params::shares_name,
   Array[String] $shares_path        = $::samba_client::params::shares_path,
-  Array[String] $valid_users = $::samba_client::params::valid_users,
-  String $writable           = $::samba_client::params::writable,
-  String $browseable         = $::samba_client::params::browseable,
-  String $path_nfs           = $::samba_client::params::path_nfs,
+  Array[String] $valid_users        = $::samba_client::params::valid_users,
+  String $writable                  = $::samba_client::params::writable,
+  String $browseable                = $::samba_client::params::browseable,
+  String $path_nfs                  = $::samba_client::params::path_nfs,
 ) inherits samba_client::params {
   
-  #  $share =  each($::samba_client::shares_name) |Integer $index, String $value|{
-  #  $value = { 
+  #    $share =  each($::samba_client::shares_name) |Integer $index, String $value|{
+  #  shares_definitions {  
+  #  $value :   
   #    'comment'     => "$shares_comment $shares_name[$index]",
   #    'path'        => "$path_nfs $shares_path[$index]",
   #    'valid users' => $valid_users[$index],
   #    'writable'    => $writable,
   #    'browseable'  => $browseable,
   #  }
-  #}
-  
+    
   # }
+  
+   
   
 
   class { 'samba':
-    shares_definitions => {
-      'share' => {
-        'comment'     => "$shares_comment $shares_name[0]",
-        'path'        => "$path_nfs $shares_path[0]",
+    shares_definitions => { #$::samba_client::shares_name.each |Integer $index, String $value|{
+      $shares_name[0] => { 
+        'comment'     => "$shares_comment+$shares_name[0]",
+        'path'        => "$path_nfs+$shares_path[0]",
         'valid users' => $valid_users[0],
         'writable'    => $writable,
         'browseable'  => $browseable,
-
       }
+      
     }
   }
 }
+
