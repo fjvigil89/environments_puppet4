777be180aced0b1ba800d705bc26342e7e009e9e
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-09-14 11:34:04 -0400

    test lets encypt

--- manifests/nodes/letsencrypt.pp
+++ manifests/nodes/letsencrypt.pp
@@ -4,4 +4,18 @@ node 'letscert.upr.edu.cu' {
     application    => 'lets encrypt',
     mta_enabled    => false,
   }
+include ::apt
+include ::apt::backports
+apt::pin { 'stretch-backports-letsencrypt':
+  release  => 'stretch-backports',
+  packages => prefix(['acme', 'cryptography', 'openssl', 'psutil', 'setuptools', 'pyasn1', 'pkg-resources'], 'python-'),
+  priority => 700,
+}
+class { ::letsencrypt:
+  email => 'lets.encryptupr@gmail.com',
+}
+letsencrypt::certonly { 'upr':
+ domains => ['contable.upr.edu.cu','sync.upr.edu.cu'],
+ plugin  => 'apache',
+}
 }
