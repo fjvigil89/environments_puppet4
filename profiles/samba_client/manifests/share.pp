b44ac58cd92e4ff425934d5b6d2bb8c61f6f7a57
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-09-15 09:03:31 -0400

    repos 01

---
+++ profiles/samba_client/manifests/share.pp
@@ -0,0 +1,24 @@
+# == Define: samba_client::share
+####
+define samba_client::share (
+    $shares_name = undef,
+    $valid_users = undef,
+    $path_nfs    =  undef,
+){
+ 
+  #include ::samba_client::params
+  class { 'samba':
+
+    shares_definitions => { 
+     $shares_name => {
+       'comment'     => "'Repositorio de '${shares_name}",
+       'path'        => "${path_nfs}${shares_name}",
+       'valid users' => $valid_users,
+       'writable'    => 'yes',
+       'browseable'  => 'yes',
+     },
+    }
+  }
+}
+
+
