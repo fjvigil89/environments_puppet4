08c481f04549f6c59f1aa4aa280c58dbcb078e05
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-09-19 13:58:34 -0400

    samba 1

--- profiles/samba_client/manifests/init.pp
+++ profiles/samba_client/manifests/init.pp
@@ -13,42 +13,20 @@ class samba_client (
   String $browseable                = $::samba_client::params::browseable,
   String $path_nfs                  = $::samba_client::params::path_nfs,
 ) inherits samba_client::params {
-  
-  #    $share =  each($::samba_client::shares_name) | DataType $index, Datatype $value|{
-  #   type ${value}:   
-  #    'comment'     => "$shares_comment $shares_name[$index]",
-  #    'path'        => "$path_nfs $shares_path[$index]",
-  #    'valid users' => $valid_users[$index],
-  #    'writable'    => $writable,
-  #    'browseable'  => $browseable,
-     
-  #  }
-  
-  # $shares = $::samba_client::shares_name.each |Integer $index, String $value|
-  #  {
-  #    samba_client::share{$shares_name[$index]:
-  #      shares_name => $shares_name[$index],
-  #      valid_users => $valid_users[$index],
-  #      path_nfs    => $path_nfs,
-  #    }
-      #  }
-  
-   
-#class { 'samba': 
-#   shares_definitions =>  $shares 
-
-#  }
-
-  class { 'samba':
-    share_definitions => { $::samba_client::shares_name.each |Integer $index, String $value|{
-      $value          =>{
+ 
+  $share =  $::samba_client::shares_name.each |Integer $index, String $value|{
+      $value = {  
         'comment'     => "$shares_comment $shares_name[$index]",
         'path'        => "$path_nfs $shares_path[$index]",
         'valid users' => $valid_users[$index],
         'writable'    => $writable,
         'browseable'  => $browseable,
-        } 
+        }
     }
-    }
+    
+
+  class { 'samba':
+    share_definitions => $share, 
+    
   }
 }
