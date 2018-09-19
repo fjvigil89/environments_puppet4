700274fe72b9fbb21838efdd7f361d0889e6c0d7
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-09-19 17:58:13 -0400

    samba 16

--- profiles/samba_client/manifests/init.pp
+++ profiles/samba_client/manifests/init.pp
@@ -14,21 +14,8 @@ class samba_client (
   String $path_nfs                  = $::samba_client::params::path_nfs,
 ) inherits samba_client::params {
  
-  #$val = $::samba_client::shares_name.each |Integer $index, String $value|{
-  #    samba_client::share{$value :
-  #      path_nfs    => $path_nfs,
-  #      valid_users => $valid_users[$index],
-  #    }
-  #  }
-  
-  
-
-    #  class { 'samba':
-    #share_definitions => $val       
-    #}
-    #samba_client::share {$shares_name:}
-    #$var = $::samba_client::shares_name.each |Integer $index, String $value|{
-    $var               = {
+    $::samba_client::shares_name.each |Integer $index, String $value|{
+      $var               = {
       'shares' => {
         'comment'     => "'Repositorio de '${shares_name}",
         'path'        => '/mnt/stuff',
@@ -42,7 +29,7 @@ class samba_client (
       },
     }
     
-    #}
+   }
   
   class { 'samba':
    shares_definitions => $var, 
