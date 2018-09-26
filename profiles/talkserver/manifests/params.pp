f3000ab4bff28a474591b39fab0f287c1c1c78ba
Henry <henry.fleitas@upr.edu.cu>
2018-09-26 10:22:21 -0400

    Talk

--- profiles/talkserver/manifests/params.pp
+++ profiles/talkserver/manifests/params.pp
@@ -6,8 +6,8 @@ class talkserver::params{
   $user  = 'prosody'
   $group = 'prosody'
   $authentication    = 'ldap'
-  $ldap_base         = '"DC=upr,DC=edu,DC=cu"'
+  $ldap_base         = '"OU=_Servicios,DC=upr,DC=edu,DC=cu"'
   $ldap_server       = '"ad.upr.edu.cu:636"'
-  $ldap_rootdn       = '"DN=talk,OU=_Servicios,DC=upr,DC=edu,DC=cu"'
+  $ldap_rootdn       = '"DN=prosody,OU=_Servicios,DC=upr,DC=edu,DC=cu"'
   $ldap_password     = '"40a*talk.2k12"'
 }
