3442c44348dc045495f612ad65046c89926073e3
Henry <henry.fleitas@upr.edu.cu>
2018-10-02 09:09:03 -0400

    Talk

--- profiles/talkserver/manifests/init.pp
+++ profiles/talkserver/manifests/init.pp
@@ -30,7 +30,7 @@ class { 'prosody':
 }
 
 prosody::virtualhost {
-  'chat.upr.edu.cu' :
+  'upr.edu.cu' :
     ensure   => present,
 #ssl_key  => '/etc/ssl/key/sync.upr.edu.cu.key',
 #ssl_cert => '/etc/ssl/crt/sync.upr.edu.cu.crt',
