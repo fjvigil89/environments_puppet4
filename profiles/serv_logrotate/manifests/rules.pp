286422c12c2bb88bbbac5142b83f3bed4127ba2a
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-10-05 10:29:19 -0400

    test

--- profiles/serv_logrotate/manifests/rules.pp
+++ profiles/serv_logrotate/manifests/rules.pp
@@ -7,7 +7,7 @@ class serv_logrotate::rules {
   each($::serv_logrotate::rule_list) |Integer $index, String $value| {
     logrotate::rule { $::serv_logrotate::rule_list[$index]:
       path         => $::serv_logrotate::log_path[$index],
-      rotate       => $::serv_logrotate::filelog_numbers[$index].scanf("%i"),
+      rotate       => $::serv_logrotate::filelog_numbers[$index],
       rotate_every => $::serv_logrotate::rotate_frecuency[$index],
     }
   }
