816b71921abafc977ed17d6e86701c9dcbb15ce5
Henry <henry.fleitas@upr.edu.cu>
2018-10-05 10:27:09 -0400

    Nodo Talk

--- profiles/talkserver/manifests/virtualhost.pp
+++
@@ -1,9 +0,0 @@
-#Clase virtualhost
-
-class talkserver::virtualhost {
-    'upr.edu.cu' :
-      ensure   => present,
-      ssl_key  => '/etc/ssl/key/mydomain.com.key',
-      ssl_cert => '/etc/ssl/crt/mydomain.com.crt',
-  }
-}
