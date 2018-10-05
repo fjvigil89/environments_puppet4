c37dfc5bc6039fe49537f4fe063668724eac7701
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-10-05 14:49:10 -0400

    test frd 2

--- manifests/nodes/puppet_host.pp
+++ manifests/nodes/puppet_host.pp
@@ -38,10 +38,7 @@ node 'puppet-test.upr.edu.cu'{
   max_requests    => 4096,
   max_servers     => 4096,
   mysql_support   => true,
-  perl_support    => true,
   utils_support   => true,
-  wpa_supplicant  => true,
-  winbind_support => true,
   syslog          => true,
   log_auth        => 'yes',
   }
