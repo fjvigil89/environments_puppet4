1a21281549b0f9c24ef2a14bfa1b26255553a18d
Henry <henry.fleitas@upr.edu.cu>
2018-10-11 14:48:59 -0400

    Talk

--- manifests/nodes/puppet_host.pp
+++ manifests/nodes/puppet_host.pp
@@ -65,9 +65,9 @@ node 'puppet-henry.upr.edu.cu'{
     community_modules => ['mod_auth_ldap'],
     authentication    => 'ldap',
     custom_options    => {
-      'ldap_base'     => "dc=upr,dc=edu,dc=cu",
-      'ldap_server'   => '10.2.24.35:389'
-      'ldap_rootdn'   => 'CN=talk,OU=_Servicios,DC=upr,DC=edu,DC=cu',
+      'ldap_base'     => "DC=upr,DC=edu,DC=cu",
+      'ldap_server'   => '10.2.24.35:389',
+      'ldap_rootdn'   => "CN=talk,OU=_Servicios,DC=upr,DC=edu,DC=cu",
       'ldap_password' => '40a*talk.2k12',
       'ldap_scope'    => 'subtree',
       'ldap_tls'      => 'true',
