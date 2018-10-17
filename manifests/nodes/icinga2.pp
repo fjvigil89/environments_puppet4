ef0e9eb0ba994aca613d05cbdb554d66daefaf85
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-10-17 16:36:25 -0400

    add master-icinga1 node conf

--- manifests/nodes/icinga2.pp
+++ manifests/nodes/icinga2.pp
@@ -1,5 +1,13 @@
 node 'master-icinga0.upr.edu.cu'{
   include icinga2_server
 }
-
+node 'master-icinga1.upr.edu.cu'{
+  class { '::basesys':
+    uprinfo_usage      => 'icinga_server1',
+    application        => 'icinga2',
+    puppet_enabled     => true,
+    mta_enabled        => false,
+    monitoring_enabled => false;
+  }
+}
 
