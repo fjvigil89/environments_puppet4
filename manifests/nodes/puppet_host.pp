a6f7b1900cd214b5822a8124d76dfb5070f0714c
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-10-23 11:55:24 -0400

    new agent

--- manifests/nodes/puppet_host.pp
+++ manifests/nodes/puppet_host.pp
@@ -1,11 +1,10 @@
 node 'client-puppet.upr.edu.cu'{
   #class {'::talkserver':;}
-  #class { '::basesys':  
-  #uprinfo_usage => 'servidor test',
-  # application   => 'puppet',  
-  #  repos_enabled => false, 
-     
-  #}
+  class { '::basesys':  
+    uprinfo_usage => 'servidor test',
+    application   => 'puppet',  
+    # repos_enabled => false,      
+  }
   #class { '::letsencrypt_host':
   #email => 'fjvigil@hispavista.com',
   #webroot_enable => true,
