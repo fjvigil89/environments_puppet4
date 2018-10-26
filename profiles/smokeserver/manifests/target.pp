6ecb1092c2bf3ade9bac339c7a2f6759642e76b2
Henry <henry.fleitas@upr.edu.cu>
2018-10-26 16:43:14 -0400

    Smokeping

--- profiles/smokeserver/manifests/target.pp
+++ profiles/smokeserver/manifests/target.pp
@@ -3,8 +3,8 @@
 #
 class smokeserver::target {
   each($::smokeserver::target) |Integer $index, String $value|{
-    smokeping::target{ $value:
-      menu             => $value,
+    smokeping::target{$::smokeserver::target[$index]:
+      menu             => $::smokeserver::params::target[$index],
       pagetitle        => $::smokeserver::params::pagetitle[$index],
       hierarchy_level  => $::smokeserver::params::hierarchy_level[$index],
       hierarchy_parent => $::smokeserver::params::hierarchy_parent[$index],
