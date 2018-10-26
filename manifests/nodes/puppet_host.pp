2b971ac25453f7ed3fc3d48078127cf3f5d78744
Henry <henry.fleitas@upr.edu.cu>
2018-10-26 14:35:27 -0400

    Smokeping

--- manifests/nodes/puppet_host.pp
+++ manifests/nodes/puppet_host.pp
@@ -54,10 +54,9 @@ node 'puppet-henry.upr.edu.cu'{
   include smokeprodserver
   class { '::smokeserver':
     target           => ['Router','Cisco','primero','segundo','TP'],
-    #menu             => ['Router','Cisco','primero','segundo','TP'],
     pagetitle        => ['Conexión de la UPR','Cisco','primero','segundo','TP'],
-    hierarchy_parent => ['Router','Router','Cisco','Cisco','Router'],
-    hierarchy_level  => ['1','2','3','3','2'],
+    hierarchy_parent => ['','Router','Cisco','Cisco','Router'],
+    hierarchy_level  => [1,2,3,3,2],
     host             => ['10.2.1.1','10.2.1.8','10.2.1.13','10.2.1.140','10.2.1.14'],
   }
 }
