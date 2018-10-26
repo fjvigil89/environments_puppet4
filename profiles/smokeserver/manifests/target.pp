c7fe43c983d01ce9a1311eb01205b4b4961465c5
Henry <henry.fleitas@upr.edu.cu>
2018-10-26 13:41:13 -0400

    Smoke

--- profiles/smokeserver/manifests/target.pp
+++ profiles/smokeserver/manifests/target.pp
@@ -4,12 +4,12 @@
 class smokeserver::target(){
   each($::smokeserver::target) |Integer $index, String $value|{
     smokeping::target{ $value:
-      menu             => $::smokeserver::menu[$index],
-      pagetitle        => $::smokeserver::pagetitle[$index],
-      hierarchy_parent => $::smokeserver::hierarchy_parent[$index],
-      hierarchy_level  => $::smokeserver::hierarchy_level[$index],
-      host             => $::smokeserver::host[$index],
-      probe            => $::smokeserver::probes
+      menu             => $::smokeserver::params::menu[$index],
+      pagetitle        => $::smokeserver::params::pagetitle[$index],
+      hierarchy_parent => $::smokeserver::params::hierarchy_parent[$index],
+      hierarchy_level  => $::smokeserver::params::hierarchy_level[$index],
+      host             => $::smokeserver::params::host[$index],
+      probe            => $::smokeserver::params::probes,
     }
   }
 }
