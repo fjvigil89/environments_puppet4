8837ebe95aa61f962803f01bb88fa5449dcfb9ff
Henry <henry.fleitas@upr.edu.cu>
2018-10-26 12:45:21 -0400

    Smoke

--- roles/smokeprodserver/manifests/init.pp
+++ roles/smokeprodserver/manifests/init.pp
@@ -12,12 +12,3 @@ class smokeprodserver(){
       proxmox_enabled => false,
     }
 }
-
-
-class smokeprodserver(){
-  menu             => ['Router','Cisco','primero','segundo','TP'],
-  pagetitle        => ['Conexión de la UPR','Cisco','primero','segundo','TP'],
-  hierarchy_parent => ['','Router','Cisco','Cisco','Router'],
-  hierarchy_level  => ['1','2','3','3','2'],
-  host             => ['10.2.1.1','10.2.1.8','10.2.1.13','10.2.1.140','10.2.1.14'],
-}
