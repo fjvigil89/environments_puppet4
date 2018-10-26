7e1e9837c051cbdb0fcfdda9fdab6ce80341f561
Henry <henry.fleitas@upr.edu.cu>
2018-10-26 16:18:44 -0400

    Smokeping

--- profiles/smokeserver/manifests/target.pp
+++ profiles/smokeserver/manifests/target.pp
@@ -7,9 +7,6 @@ class smokeserver::target {
       menu             => $value,
       pagetitle        => $::smokeserver::params::pagetitle[$index],
       hierarchy_level  => $::smokeserver::params::hierarchy_level[$index],
-     if($::smokeserver::params::hierarchy_level==2){
-       hierarchy_parent => 'World',
-     }
       hierarchy_parent => $::smokeserver::params::hierarchy_parent[$index],
       host             => $::smokeserver::params::host[$index],
     }
