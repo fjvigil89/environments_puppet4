0785dd0c1c1901df4b93a60439c15092b4d5fd2a
Henry <henry.fleitas@upr.edu.cu>
2018-10-29 09:26:28 -0400

    Smokeping rol

--- manifests/nodes/puppet_host.pp
+++ manifests/nodes/puppet_host.pp
@@ -52,14 +52,25 @@ node 'test.upr.edu.cu'{
   }
 node 'puppet-henry.upr.edu.cu'{
   include smokeprodserver
-  class { 'smokeserver':
-    target           => ['Router','Cisco','primero','segundo','TP'],
-    pagetitle        => ['Conexión de la UPR','Cisco','primero','segundo','TP'],
-    hierarchy_level  => [2,2,3,3,2],
-    hierarchy_parent => ['Router','Router','Cisco','Cisco','Router'],
-    host             => ['10.2.1.1','10.2.1.8','10.2.1.13','10.2.1.140','10.2.1.14'],
-  }
+  package { 'lsb-release':
+    ensure => installed,
+    }~> 
+    class { '::basesys':
+      uprinfo_usage => 'Servidor test',
+      application   => 'puppet',
+      proxmox_enabled => false,
+    }
 }
+  
+  
+# class { 'smokeserver':
+#    target           => ['Router','Cisco','primero','segundo','TP'],
+#    pagetitle        => ['Conexión de la UPR','Cisco','primero','segundo','TP'],
+#    hierarchy_level  => [2,2,3,3,2],
+#    hierarchy_parent => ['Router','Router','Cisco','Cisco','Router'],
+#    host             => ['10.2.1.1','10.2.1.8','10.2.1.13','10.2.1.140','10.2.1.14'],
+#  }
+#}
   #  class { '::smokeping':
   #   mode   => 'standalone',
   #   probes => [
--- roles/smokeprodserver/manifests/init.pp
+++ roles/smokeprodserver/manifests/init.pp
@@ -3,12 +3,24 @@
 #================================
 #
 class smokeprodserver(){
-  package { 'lsb-release':
-    ensure => installed,
-    }~> 
-    class { '::basesys':
-      uprinfo_usage => 'Servidor test',
-      application   => 'puppet',
-      proxmox_enabled => false,
-    }
+  class { 'smokeserver':
+    target           => ['Router','Cisco','primero','segundo','TP'],
+    pagetitle        => ['Conexión de la UPR','Cisco','primero','segundo','TP'],
+    hierarchy_level  => [2,2,3,3,2],
+    hierarchy_parent => ['Router','Router','Cisco','Cisco','Router'],
+    host             => ['10.2.1.1','10.2.1.8','10.2.1.13','10.2.1.140','10.2.1.14'],
+  }
 }
+
+
+
+
+  #  package { 'lsb-release':
+  #  ensure => installed,
+  #    }~> 
+  #   class { '::basesys':
+  #    uprinfo_usage => 'Servidor test',
+  #    application   => 'puppet',
+  #    proxmox_enabled => false,
+  #  }
+  #}
