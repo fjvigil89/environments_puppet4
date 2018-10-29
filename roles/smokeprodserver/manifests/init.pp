c4322b6e502c8c0e2cd6a833fcc24a1547d494b7
Henry <henry.fleitas@upr.edu.cu>
2018-10-29 15:42:47 -0400

    Smoke agragar L3

--- roles/smokeprodserver/manifests/init.pp
+++ roles/smokeprodserver/manifests/init.pp
@@ -9,40 +9,41 @@ class smokeprodserver(){
     pagetitle => 'Routers de la UPR',
     alerts    => [ 'bigloss', 'noloss','startloss' ],
     }
-    smokeping::target { 'Cisco':
+  smokeping::target { 'Cisco':
     hierarchy_parent => 'Routers',
     hierarchy_level  => 2,
     menu             => 'Cisco',
     pagetitle        => 'Cisco',
     host             => '10.2.1.7',
     }
-    smokeping::target { 'TP':
+  smokeping::target { 'TP':
     hierarchy_parent => 'Routers',
     hierarchy_level  => 2,
     menu             => 'TP',
     pagetitle        => 'TP',
     host             => '10.2.1.14',
     }
-    smokeping::target { 'Primero':
+  smokeping::target { 'Primero':
     hierarchy_parent => 'Cisco',
     hierarchy_level  => 3,
     menu             => 'Primero',
     host             => '10.2.1.8',
     probe            => 'FPing',
     }
-    smokeping::target { 'Segundo':
+  smokeping::target { 'Segundo':
     hierarchy_parent => 'TP',
     hierarchy_level  => 3,
     menu             => 'Segundo',
     host             => '10.2.1.13',
     probe            => 'FPing',
     }
-    smokeping::target { 'Switch L3 Nodo Central':
+  smokeping::target { 'Switch L3 Nodo Central':
     menu             => 'Switch L3 Nodo Central',
     hierarchy_parent => 'Routers',
     hierarchy_level  => '2',
     pagetitle        => 'Switch L3 Nodo Central',
     host             => '10.2.1.1',
+    probe            => 'FPing',
     }
 }
 
