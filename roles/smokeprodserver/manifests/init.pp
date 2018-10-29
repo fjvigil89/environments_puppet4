c271816bdb6d8ec6f454e94019011585f35081cd
Henry <henry.fleitas@upr.edu.cu>
2018-10-29 09:30:28 -0400

    Smokeping rol

--- roles/smokeprodserver/manifests/init.pp
+++ roles/smokeprodserver/manifests/init.pp
@@ -6,7 +6,7 @@ class smokeprodserver(){
   class { 'smokeserver':
     target           => ['Router','Cisco','primero','segundo','TP'],
     pagetitle        => ['Conexión de la UPR','Cisco','primero','segundo','TP'],
-    hierarchy_level  => [2,2,3,3,2],
+    hierarchy_level  => [1,2,3,3,2],
     hierarchy_parent => ['Router','Router','Cisco','Cisco','Router'],
     host             => ['10.2.1.1','10.2.1.8','10.2.1.13','10.2.1.140','10.2.1.14'],
   }
