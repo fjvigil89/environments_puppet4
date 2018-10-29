712e9d01ad10f45f081598272fc34625c827ef75
Henry <henry.fleitas@upr.edu.cu>
2018-10-29 10:03:16 -0400

    Smokeping

--- roles/smokeprodserver/manifests/init.pp
+++ roles/smokeprodserver/manifests/init.pp
@@ -3,15 +3,39 @@
 #================================
 #
 class smokeprodserver(){
-  class { 'smokeserver':
-    target           => ['Router','Cisco','primero','segundo','TP'],
-    pagetitle        => ['Conexión de la UPR','Cisco','primero','segundo','TP'],
-    hierarchy_level  => [1,2,3,3,2],
-    hierarchy_parent => ['Router','Router','Cisco','Cisco','Router'],
-    host             => ['10.2.1.1','10.2.1.8','10.2.1.13','10.2.1.140','10.2.1.14'],
-  }
+  include smokeserver
+    smokeping::target { 'Routers':
+      menu      => 'Routers',
+      pagetitle => 'Routers de la UPR',
+      alerts    => [ 'bigloss', 'noloss','startloss' ],
+    }
+    smokeping::target { 'Cisco':
+      hierarchy_parent => 'Routers',
+      hierarchy_level  => 2,
+      menu             => 'Cisco',
+      pagetitle        => 'Cisco',
+    }
+    smokeping::target { 'TP':
+      hierarchy_parent => 'Routers',
+      hierarchy_level  => 2,
+      menu             => 'TP',
+      host             => '10.2.1.1',
+    }
+    smokeping::target { 'Primero':
+      hierarchy_parent => 'Cisco',
+      hierarchy_level  => 3,
+      menu             => 'Primero',
+      host             => '10.2.1.8',
+      probe            => 'FPing',
+    }
+    smokeping::target { 'Segundo':
+      hierarchy_parent => 'TP',
+      hierarchy_level  => 3,
+      menu             => 'Segundo',
+      host             => '10.2.1.13',
+      probe            => 'FPing',
+    }
 }
-
 
 
 
