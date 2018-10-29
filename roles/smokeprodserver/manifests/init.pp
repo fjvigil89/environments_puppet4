182c03ebbc3ae26b8f3c0093e5c14536f0488b16
Henry <henry.fleitas@upr.edu.cu>
2018-10-29 15:38:23 -0400

    Smoke agragar L3

--- roles/smokeprodserver/manifests/init.pp
+++ roles/smokeprodserver/manifests/init.pp
@@ -4,41 +4,47 @@
 #
 class smokeprodserver(){
   include smokeserver
-    smokeping::target { 'Routers':
-      menu      => 'Routers',
-      pagetitle => 'Routers de la UPR',
-      alerts    => [ 'bigloss', 'noloss','startloss' ],
+  smokeping::target { 'Routers':
+    menu      => 'Routers',
+    pagetitle => 'Routers de la UPR',
+    alerts    => [ 'bigloss', 'noloss','startloss' ],
     }
     smokeping::target { 'Cisco':
-      hierarchy_parent => 'Routers',
-      hierarchy_level  => 2,
-      menu             => 'Cisco',
-      pagetitle        => 'Cisco',
-      host             => '10.2.1.7',
+    hierarchy_parent => 'Routers',
+    hierarchy_level  => 2,
+    menu             => 'Cisco',
+    pagetitle        => 'Cisco',
+    host             => '10.2.1.7',
     }
     smokeping::target { 'TP':
-      hierarchy_parent => 'Routers',
-      hierarchy_level  => 2,
-      menu             => 'TP',
-      pagetitle        => 'TP',
-      host             => '10.2.1.1',
+    hierarchy_parent => 'Routers',
+    hierarchy_level  => 2,
+    menu             => 'TP',
+    pagetitle        => 'TP',
+    host             => '10.2.1.14',
     }
     smokeping::target { 'Primero':
-      hierarchy_parent => 'Cisco',
-      hierarchy_level  => 3,
-      menu             => 'Primero',
-      host             => '10.2.1.8',
-      probe            => 'FPing',
+    hierarchy_parent => 'Cisco',
+    hierarchy_level  => 3,
+    menu             => 'Primero',
+    host             => '10.2.1.8',
+    probe            => 'FPing',
     }
     smokeping::target { 'Segundo':
-      hierarchy_parent => 'TP',
-      hierarchy_level  => 3,
-      menu             => 'Segundo',
-      host             => '10.2.1.13',
-      probe            => 'FPing',
+    hierarchy_parent => 'TP',
+    hierarchy_level  => 3,
+    menu             => 'Segundo',
+    host             => '10.2.1.13',
+    probe            => 'FPing',
+    }
+    smokeping::target { 'Switch L3 Nodo Central':
+    menu             => 'Switch L3 Nodo Central',
+    hierarchy_parent => 'Routers',
+    hierarchy_level  => '2',
+    pagetitle        => 'Switch L3 Nodo Central',
+    host             => '10.2.1.1',
     }
 }
-
 
 
   #  package { 'lsb-release':
