06dcd36581564e170f30dcee185cb1edab81be6a
Henry <henry.fleitas@upr.edu.cu>
2018-11-01 15:39:05 -0400

    Smokeping add vh2

--- roles/smokeprodserver/manifests/init.pp
+++ roles/smokeprodserver/manifests/init.pp
@@ -5,20 +5,21 @@
 class smokeprodserver(){
   package { 'lsb-release':
     ensure => installed,
+  }
     #    }~>
     #    class { '::basesys':
     #     uprinfo_usage => 'Servidor test',
     #      application   => 'puppet',
     #      proxmox_enabled => false,
     #    }
-}
+    #   }
     #  smokeserver::target { ['Switch_L3_Nodo_Central','Router_PAP','Router_FCP','Router_FCF']:
     #pagetitle        => ["Switch L3 Nodo Central","Router PAP","Router FCP","Router FCF"],
     #  hierarchy_level  => 2,
     #hierarchy_parent => 'Routers',
     #host             => ['10.2.1.1','10.2.1.8','10.2.1.13','10.2.1.140','10.2.1.14'],
     # }
-  #}
+    }
 
   # smokeping::target { ['Switch_L3_Nodo_Central','Router_PAP','Router_FCP','Router_FCF']:
   #  hierarchy_parent => ['Routers','Routers',
