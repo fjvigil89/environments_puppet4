6fd156dee96bf4e4af562f7c624807ff9b6ff2d2
Henry <henry.fleitas@upr.edu.cu>
2018-11-05 11:33:36 -0500

    Agregar router docente2

--- manifests/nodes/smokeping.pp
+++ manifests/nodes/smokeping.pp
@@ -3,7 +3,7 @@ node 'smokeping.upr.edu.cu'{
   include smokeprodserver
   class {'smokeserver':
     target           => ['Routers','L3','PAP','FCP','FCF','Docente','CUM','Vinales','Palacios','San_Luis','Minas','Guane','Mantua','La_Palma','Sandino','Consolacion'],
-    menu             => ['Routers','Switch L3 Nodo Central','Router PAP','Router FCP','Router FCF','Docente','CUM','Viñales','Los Palacios','San Luis','Minas','Guane','Mantua','La Palma','Sandino','Consolacion'],
+    menu             => ['Routers','Switch L3 Nodo Central','Router PAP','Router FCP','Router FCF','Router Docente','CUM','Viñales','Los Palacios','San Luis','Minas','Guane','Mantua','La Palma','Sandino','Consolacion'],
     hierarchy_level  => [1,2,2,2,2,2,1,2,2,2,2,2,2,2,2,2],
     hierarchy_parent => ['Routers','Routers','Routers','Routers','Routers','Routers','CUM','CUM','CUM','CUM','CUM','CUM','CUM','CUM','CUM','CUM'],
     host             => ['','10.2.1.1','10.2.1.5','10.2.0.10','10.2.8.200','10.2.0.6','','10.2.20.49','10.2.20.119','10.2.20.177','10.2.20.33','10.2.20.209','10.2.20.17','10.2.20.165','10.2.20.1','10.2.20.145'],
