7e94e6076f2b2b82ae6170c1c3d0dadc090ac160
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-11-14 11:16:11 -0500

    change icinga node conf

--- manifests/nodes/icinga2.pp
+++ manifests/nodes/icinga2.pp
@@ -1,5 +1,13 @@
 node 'master-icinga0.upr.edu.cu'{
-  include icinga2_server
+  class { '::basesys': 
+    uprinfo_usage      => 'icinga_server',
+    application        => 'icinga2',
+    puppet_enabled     => true,
+    mta_enabled        => false,
+    monitoring_enabled => false;
+  }
+  class { '::monitoring':;}
+  #include icinga2_server
 }
 node 'master-icinga1.upr.edu.cu'{
   class { '::basesys':
