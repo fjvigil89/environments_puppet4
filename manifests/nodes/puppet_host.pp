907298b73b858c190bede9210420b9c558093e0b
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-11-23 14:56:51 -0500

    dejando squid para el futuro

--- manifests/nodes/puppet_host.pp
+++ manifests/nodes/puppet_host.pp
@@ -29,73 +29,7 @@ node 'client-puppet.upr.edu.cu'{
   #  ensure => present,
   #}
 
-
-  class { 'squidserver':;}
-
-  squidserver::acl { 'CONNECT':
-    type    => method,
-    entries => ['CONNECT'],
-  }
-  
-  squidserver::acl{ 'SSL_ports' :
-    type    => port,
-    entries => ['443']
-  }
-  squidserver::acl { 'Safe_ports':
-    type    => port,
-    entries => ['80','443','21','70','210','1025-65535','280','488','591','777','901'],
-  }
-  squidserver::acl { 'red_pap':
-    type    => src,
-    entries => ['10.71.46.0/24 20.0.0.0/24 10.2.9.0/24 10.2.75.0/25'],
-
-  }
-  
-  squidserver::tcp_outgoing_address{'OutGoing':
-    value    => ['200.14.49.29'],
-    acl_name => ['red_pap']
-  }
-
-  squidserver::cache_peer{'cache_peer':
-    pattern    => ["${::ip}"],
-    type       => ['sibling'],
-    proxy_port => ['8080'],
-    icp_port   => ['0'],
-    #options    => ['no-query'],
-  }
-
-  squid::auth_param { 'basic auth_param':
-    order   => '400',
-    scheme  => 'basic',
-    entries => [
-      'program /usr/lib/squid3/basic_ldap_auth -R -v 3 -b "dc=upr,dc=edu,dc=cu" -D internet@upr.edu.cu -w P@ssword -f (|(userPrincipalName=%s)(sAMAccountName=%s)) -h 10.2.24.35',
-      'children 10',
-      'realm Servidor Proxy de la UPR',
-      'credentialsttl 2 hours',
-    ],
-  }
-  squid::extra_config_section { 'external_acl_type':
-    order          => '400',
-    config_entries => [{
-      'external_acl_type' => [
-        'ADGroup    %LOGIN /usr/lib/squid3/ext_ldap_group_acl -R -v 3 -b "dc=upr,dc=edu,dc=cu" -D internet@upr.edu.cu -w P@ssword -f "(&(objectclass=person)(sAMAccountName=%v)(memberof=cn=%a,ou=_Gestion,dc=upr,dc=edu,dc=cu))" -h 10.2.24.35',
-      ],
-    }],
-  }
-  squidserver::acl { 'ldapauth':
-    type    => proxy_auth,
-    order   => '4',
-    entries => ['REQUIRED'],
-  }
-  squidserver::acl { 'AccesoAlGrupo':
-    type    => external,
-    order   => '4',
-    entries => ['ADGroup UPR-Internet-Profes UPR-Internet-NoDocente UPR-Internet-Est'],
-  }
-
-
-
-
+  #class { 'squidserver':;}
 
 }
 
--- roles/squidserver/manifests/init.pp
+++ roles/squidserver/manifests/init.pp
@@ -6,15 +6,6 @@ class squidserver(
 )inherits ::squidserver::params {
   
   class { 'squid_server':
-    http_access                    => {
-      'authorized_networks' => { action => 'allow', value =>'authorized_networks', },
-      'purge_localhost' => { action => 'allow', value =>'purge localhost', },
-      'purge' => { action => 'deny', value =>'purge', },
-      '!Safe_ports' => { action => 'deny', value =>'!Safe_ports', },
-      'CONNECT !SSL_ports' => { action => 'deny', value =>'CONNECT !SSL_ports', },
-      'localhost' => { action => 'allow', value =>'localhost', },
-      'all' => { action => 'deny', value =>'all', },
-    },
     http_ports                     => { 'http' => { port => '8080', }},
     https_ports                    => { 'https' => { port => '8443',}},
     icp_access                     => { 'icp_access'=>{ action => 'deny', value =>'all',}},
@@ -28,5 +19,73 @@ class squidserver(
     via                            => false,
 
   }
+
+  squidserver::acl { 'CONNECT':
+    type    => method,
+    entries => ['CONNECT'],
+  }
+
+  squidserver::acl{ 'SSL_ports' :
+    type    => port,
+    entries => ['443']
+  }
+  squidserver::acl { 'Safe_ports':
+    type    => port,
+    entries => ['80','443','21','70','210','1025-65535','280','488','591','777','901'],
+  }
+  squidserver::acl { 'red_pap':
+    type    => src,
+    entries => ['10.71.46.0/24 20.0.0.0/24 10.2.9.0/24 10.2.75.0/25'],
+
+  }
+  squidserver::acl { 'ldapauth':
+    type    => proxy_auth,
+    order   => '4',
+    entries => ['REQUIRED'],
+  }
+  squidserver::acl { 'AccesoAlGrupo':
+    type    => external,
+    order   => '4',
+    entries => ['ADGroup UPR-Internet-Profes UPR-Internet-NoDocente UPR-Internet-Est'],
+  }
+
+
+  squidserver::tcp_outgoing_address{'OutGoing':
+    value    => ['200.14.49.29'],
+    acl_name => ['red_pap']
+  }
+
+  squidserver::cache_peer{'cache_peer':
+    pattern    => ["${::ip}"],
+    type       => ['sibling'],
+    proxy_port => ['8080'],
+    icp_port   => ['0'],
+    #options    => ['no-query'],
+  }
+
+  squid::auth_param { 'basic auth_param':
+    order   => '400',
+    scheme  => 'basic',
+    entries => [
+      'program /usr/lib/squid3/basic_ldap_auth -R -v 3 -b "dc=upr,dc=edu,dc=cu" -D internet@upr.edu.cu -w P@ssword -f (|(userPrincipalName=%s)(sAMAccountName=%s)) -h 10.2.24.35',
+      'children 10',
+      'realm Servidor Proxy de la UPR',
+      'credentialsttl 2 hours',
+    ],
+  }
+  squid::extra_config_section { 'external_acl_type':
+    order          => '400',
+    config_entries => [{
+      'external_acl_type' => [
+        'ADGroup    %LOGIN /usr/lib/squid3/ext_ldap_group_acl -R -v 3 -b "dc=upr,dc=edu,dc=cu" -D internet@upr.edu.cu -w P@ssword -f "(&(objectclass=person)(sAMAccountName=%v)(memberof=cn=%a,ou=_Gestion,dc=upr,dc=edu,dc=cu))" -h 10.2.24.35',
+      ],
+    }],
+  }
+
+
+
+
+
+
 
 }
