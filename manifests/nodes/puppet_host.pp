b49ecad6a29b6a8171a66730978413afb26cdfe7
Henry <henry.fleitas@upr.edu.cu>
2018-11-27 16:51:25 -0500

    Empezando a instalar jabber 3

--- manifests/nodes/puppet_host.pp
+++ manifests/nodes/puppet_host.pp
@@ -222,12 +222,12 @@ node 'puppet-henry.upr.edu.cu' {
     community_modules => ['mod_auth_ldap'],
     authentication    => 'ldap',
     custom_options    => {
-      'ldap_base'     => "OU=Servicios,DC=upr,DC=edu,DC=cu",
+      'ldap_base'     => "OU='Servicios',DC='upr',DC='edu',DC='cu'",
       'ldap_server'   => "10.2.24.35:636",
-      'ldap_rootdn'   => "CN=talk,OU=_Servicios,DC=upr,DC=edu,DC=cu",
+      'ldap_rootdn'   => "CN='talk',OU='_Servicios',DC='upr',DC='edu',DC='cu'",
       'ldap_password' => "40a*talk.2k12",
-      'ldap_scope'    => "subtree",
-      'ldap_tls'      => "true",
+      'ldap_scope'    => 'subtree',
+      'ldap_tls'      => 'true',
     },
   }
   prosody::virtualhost {
