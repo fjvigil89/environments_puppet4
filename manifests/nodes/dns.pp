66c880fee8d9f8715b8bc521006398a109e7b350
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-12-03 17:02:28 -0500

    test ns slave

--- manifests/nodes/dns.pp
+++ manifests/nodes/dns.pp
@@ -70,7 +70,7 @@ node 'ns2.upr.edu.cu', 'ns3.upr.edu.cu'{
     recursion          => 'yes',
     zone_name          => [ 'upr.edu.cu'],
     zone_type          => 'type slave',
-    mymasters          => '200.14.49.2',
+    mymaster          => '200.14.49.2',
     file_zone_name     => [ 'db.49.14.200.in-addr.arpa', 'db.143.55.200.in-addr.arpa', 'db.173.207.152.in-addr.arpa'],
     zone_reverse       => [ '49.14.200.in-addr.arpa', '143.55.200.in-addr.arpa', '173.207.152.in-addr.arpa'],
     
