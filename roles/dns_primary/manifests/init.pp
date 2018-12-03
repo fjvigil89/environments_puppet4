d69df872418bcbd3664c573e9995332e530a1aa2
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-12-03 16:36:12 -0500

    fix syntax problem

--- roles/dns_primary/manifests/init.pp
+++ roles/dns_primary/manifests/init.pp
@@ -8,7 +8,7 @@ class dns_primary (
  Array[String] $listen_on_addr  	  = $::dns_primary::params::listen_on_addr,
  Array[String] $listen_on_v6_addr 	= $::dns_primary::params::listen_on_v6_addr,
  Array[String] $forwarders      	  = $::dns_primary::params::forwarders,
- Optional [Boolean] $forward        = $::dns_primary::params::forward,
+ Optional[Boolean] $forward        = $::dns_primary::params::forward,
  Optional[String] $recursion        = $::dns_primary::params::recursion,
  Array[String] $allow_query     	  = $::dns_primary::params::allow_query,
  String $dump_file              	  = $::dns_primary::params::dump_file,
