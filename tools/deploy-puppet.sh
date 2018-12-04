98c2658592d14050a1c9ecb1427f1ce2939bdb3e
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-12-04 17:37:40 -0500

    script deploy only environment1

--- tools/deploy-puppet.sh
+++ tools/deploy-puppet.sh
@@ -2,4 +2,4 @@
 
 ENV=${1:-production}
 echo Deploying $ENV
-ssh -i /root/.ssh/id_rsa root@puppet-master.upr.edu.cu '/opt/puppetlabs/puppet/bin/r10k deploy environment ${ENV} -p -v'
+ssh -i /root/.ssh/id_rsa root@puppet-master.upr.edu.cu '/opt/puppetlabs/puppet/bin/r10k deploy environment $ENV -p -v'
