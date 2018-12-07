6f9b2b35bd399745ccdff9725cae6cd99196bbff
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-12-07 11:57:59 -0500

    fix error

--- manifests/nodes/ns.pp
+++ manifests/nodes/ns.pp
@@ -1,9 +1,74 @@
-node /^ns\d+$/ {  
+node 'ns1.upr.edu.cu'{
+  package { 'lsb-release':
+          ensure => installed,
+  }~>
   class { '::basesys':
-    uprinfo_usage  => 'servidor ns externo',
-    application    => 'NS Externo',
+    uprinfo_usage  => 'servidor dns externo',
+    application    => 'DNS Bind9',
     puppet_enabled => false,
-    repos_enabled  => false,
+    repos_enabled  => true,
     mta_enabled    => false,
+    dns_enabled    => false,
   }
 }
+node 'ns2.upr.edu.cu', 'ns3.upr.edu.cu'{
+  package { 'lsb-release':
+    ensure => installed,
+    }~>
+    class { '::basesys':
+      uprinfo_usage  => 'servidor dns externo secundario',
+      application    => 'DNS Bind9',
+      puppet_enabled => false,
+      repos_enabled  => true,
+      mta_enabled    => false,
+      dns_enabled    => false,
+    }
+  $zone    = 'type slave'
+  $allow   = "{ any; }"
+  $direct  = "/etc/bind"
+  $masters = "{ 200.14.49.2; }"
+  
+  class {'::dns_primary':
+    config_file        => '/etc/bind/named.conf',
+    directory          => '/etc/bind',
+    dump_file          => 'cache_dump.db',
+    statistics_file    => 'named_stats.txt',
+    memstatistics_file => 'named_mem_stats.txt',
+    slave              => true,
+    allow_query        => $allow,
+    recursion          => 'yes',
+    zone_name          => [ 'upr.edu.cu'],
+    zone_type          => $zone,
+    mymaster           => $masters,
+    file_zone_name     => [ 'db.upr.edu.cu', 'db.49.14.200', 'db.143.55.200', 'db.173.207.152'],
+    zone_reverse       => [ '49.14.200.in-addr.arpa', '143.55.200.in-addr.arpa', '173.207.152.in-addr.arpa'],
+    zones              => {
+      'upr.edu.cu' => [
+        $zone,
+        "allow-query $allow",
+        "masters $masters",
+        "file ${direct}'/zones/db.upr.edu.cu",
+      ],
+      '27/0.49.14.200.in-addr.arpa' => [
+        $zone,
+        "allow-query $allow",
+        "masters $masters",
+        "file ${direct}/zones/db.49.14.200",
+      ],
+      '29/8.143.55.200.in-addr.arpa' => [
+        $zone,
+        "allow-query $allow",
+        "masters $masters",
+        "file ${direct}/zones/db.143.55.200",
+        ],
+        '29/40.173.207.152.in-addr.arpa' => [
+          $zone,
+          "allow-query $allow",
+          "masters $masters",
+          "file ${direct}/zones/db.173.207.152",
+          ],
+    },
+  }
+}
+
+
