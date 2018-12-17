652f621ddce176f528b35327b9580083ec3e0d59
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-12-17 15:07:36 -0500

    Create server reverse proxy for production 1

--- manifests/nodes/reverse-proxy.pp
+++ manifests/nodes/reverse-proxy.pp
@@ -15,27 +15,18 @@ node 'reverse-proxy.upr.edu.cu'{
   class {'::reverseProxy_Server':
     server_name => [
       'correo.upr.edu.cu',
-      'bower.upr.edu.cu',
-      'cifam.upr.edu.cu',
-      'coodes.upr.edu.cu',
       'cvforestal.upr.edu.cu',
       'intranet.upr.edu.cu',
       'proxy-login.upr.edu.cu',
-      'catalogo.upr.edu.cu ',
-      'composer.upr.edu.cu',
-      'eventos.upr.edu.cu',
       'nexus.upr.edu.cu',
-      'cfores.upr.edu.cu',
-      'cooder.upr.edu.cu',
-      'crai.upr.edu.cu',
       'harbor.upr.edu.cu',
       'proxy-go.upr.edu.cu'
     ],
     listen_port => [
-      '443','80','80','80','80','80','443','80','80','80','80','80','80','80','80','443'
+      '443','80','80','443','80','80','443'
     ],
     ssl_port    => [
-      '443','80','80','80','443','443','443','80','80','80','443','80','80','80','443','443'
+      '443','443','443','443','443','443','443'
     ],
 
   }
--- profiles/reverseProxy/manifests/init.pp
+++ profiles/reverseProxy/manifests/init.pp
@@ -3,10 +3,10 @@
 # Copyright 2018 Your name here, unless otherwise noted.
 #
 class reverseProxy (
-  Boolean $manage_repo                  => $::reverseProxy::params::manage_repo, 
-  Optional[Array[Integer]] $listen_port => $::reverseProxy::params::listen_port,
-  Optional[Array[Integer]] $ssl_port    => $::reverseProxy::params::ssl_port,
-  Optional[Array[String]] $server_name  => $::reverseProxy::params::server_name,
+  Boolean $manage_repo                  = $::reverseProxy::params::manage_repo, 
+  Optional[Array[Integer]] $listen_port = $::reverseProxy::params::listen_port,
+  Optional[Array[Integer]] $ssl_port    = $::reverseProxy::params::ssl_port,
+  Optional[Array[String]] $server_name  = $::reverseProxy::params::server_name,
 )inherits ::reverseProxy::params{
   
   class {'::reverseProxy::common':;}
