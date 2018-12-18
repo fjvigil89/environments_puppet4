c8daa022abda06fc1881361d5c37775b6adbff93
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-12-18 10:13:36 -0500

    reverse-proxy 10

--- profiles/reverseproxy_server/manifests/server.pp
+++ profiles/reverseproxy_server/manifests/server.pp
@@ -2,51 +2,88 @@
    if($::reverseproxy_server::server_name)
    {
      each ($::reverseproxy_server::server_name) |Integer $index, String $value|{
-       if($::reverseproxy_server::location_allow[$index] == '*')
+       $red_univ= false
+       if($::reverseproxy_server::location_allow[$index] == 'red_univ')
        {
-         $allow = ['any']
-         $deny  = ['all']
-       }
-       else{
          $allow = ['200.14.49.64/27','200.55.143.160/29','10.0.0.0/8']
          $deny  = ['all']
+         $red_univ= true
        }
        if($::reverseproxy_server::ssl_port[$index] == 443) and ($::reverseproxy_server::listen_port[$index]== 443)
        {
-         nginx::resource::server { $::reverseproxy_server::server_name[$index]:
-           listen_port    => $::reverseproxy_server::listen_port[$index],
-           ssl_port       => $::reverseproxy_server::ssl_port[$index],
-           ssl            => true,
-           ssl_cert       => "/etc/letsencrypt/live/${value}/fullchain.pem",
-           ssl_key        => "/etc/letsencrypt/live/${value}/privkey.pem",
-           proxy          => "https://${value}",
-           server_name    => ["${value}"],
-           location_allow => $allow,
-           location_deny  => $deny,
-         }
-       }
-       else{
-         if($::reverseproxy_server::ssl_port[$index] == 443) and ($::reverseproxy_server::listen_port[$index]== 80){
+         if ($red_univ){
            nginx::resource::server { $::reverseproxy_server::server_name[$index]:
-             listen_port => $::reverseproxy_server::listen_port[$index],
-             ssl_port    => $::reverseproxy_server::ssl_port[$index],
-             ssl         => true,
-             ssl_cert    => "/etc/letsencrypt/live/${value}/fullchain.pem",
-             ssl_key     => "/etc/letsencrypt/live/${value}/privkey.pem",
-             proxy       => "https://${value}",
-             server_name => ["${value}"],
+             listen_port    => $::reverseproxy_server::listen_port[$index],
+             ssl_port       => $::reverseproxy_server::ssl_port[$index],
+             ssl            => true,
+             ssl_cert       => "/etc/letsencrypt/live/${value}/fullchain.pem",
+             ssl_key        => "/etc/letsencrypt/live/${value}/privkey.pem",
+             proxy          => "https://${value}",
+             server_name    => ["${value}"],
              location_allow => $allow,
              location_deny  => $deny,
            }
          }
          else{
            nginx::resource::server { $::reverseproxy_server::server_name[$index]:
-             listen_port => $::reverseproxy_server::listen_port[$index],
-             ssl_port    => $::reverseproxy_server::ssl_port[$index],
-             proxy       => "http://${value}",
-             server_name => ["${value}"],
-             location_allow => $allow,
-             location_deny  => $deny,
+             listen_port    => $::reverseproxy_server::listen_port[$index],
+             ssl_port       => $::reverseproxy_server::ssl_port[$index],
+             ssl            => true,
+             ssl_cert       => "/etc/letsencrypt/live/${value}/fullchain.pem",
+             ssl_key        => "/etc/letsencrypt/live/${value}/privkey.pem",
+             proxy          => "https://${value}",
+             server_name    => ["${value}"],
+           }
+
+         }
+       }
+       else{
+         if($::reverseproxy_server::ssl_port[$index] == 443) and ($::reverseproxy_server::listen_port[$index]== 80){
+           if($red_univ){
+             nginx::resource::server { $::reverseproxy_server::server_name[$index]:
+               listen_port => $::reverseproxy_server::listen_port[$index],
+               ssl_port    => $::reverseproxy_server::ssl_port[$index],
+               ssl         => true,
+               ssl_cert    => "/etc/letsencrypt/live/${value}/fullchain.pem",
+               ssl_key     => "/etc/letsencrypt/live/${value}/privkey.pem",
+               proxy       => "https://${value}",
+               server_name => ["${value}"],
+               location_allow => $allow,
+               location_deny  => $deny,
+             }
+           }
+           else{
+             nginx::resource::server { $::reverseproxy_server::server_name[$index]:
+               listen_port => $::reverseproxy_server::listen_port[$index],
+               ssl_port    => $::reverseproxy_server::ssl_port[$index],
+               ssl         => true,
+               ssl_cert    => "/etc/letsencrypt/live/${value}/fullchain.pem",
+               ssl_key     => "/etc/letsencrypt/live/${value}/privkey.pem",
+               proxy       => "https://${value}",
+               server_name => ["${value}"],
+             }
+
+           }
+         }
+         else{
+           if($red_univ){
+             nginx::resource::server { $::reverseproxy_server::server_name[$index]:
+               listen_port => $::reverseproxy_server::listen_port[$index],
+               ssl_port    => $::reverseproxy_server::ssl_port[$index],
+               proxy       => "http://${value}",
+               server_name => ["${value}"],
+               location_allow => $allow,
+               location_deny  => $deny,
+             }
+           }
+           else{
+             nginx::resource::server { $::reverseproxy_server::server_name[$index]:
+               listen_port => $::reverseproxy_server::listen_port[$index],
+               ssl_port    => $::reverseproxy_server::ssl_port[$index],
+               proxy       => "http://${value}",
+               server_name => ["${value}"],
+             }
+
            }
          }
        }
