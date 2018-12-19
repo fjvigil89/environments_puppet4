9eb2785b8d914ffbe6daa1bd620ffd9827825bca
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-12-19 10:15:33 -0500

    reverse-proxy add gitlab...1

--- manifests/nodes/reverse-proxy.pp
+++ manifests/nodes/reverse-proxy.pp
@@ -16,7 +16,7 @@ node 'reverse-proxy.upr.edu.cu'{
   class { 'reverse_proxy_server':
     server_name    => ['correo.upr.edu.cu','cvforestal.upr.edu.cu','intranet.upr.edu.cu','proxy-login.upr.edu.cu','nexus.upr.edu.cu','harbor.upr.edu.cu','proxy-go.upr.edu.cu','gitlab.upr.edu.cu'],
     listen_port    => [80,80,80,443,80,80,443,80],
-    ssl_port       => [443,443,443,443,443,443,443,80],
+    ssl_port       => [443,443,443,443,443,443,443,443],
     location_allow => ['*','*','red_univ','*','*','*','*','*'],
 
   }
