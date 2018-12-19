9f5c4e4b65ff5ee3ae1b03926306c96657c15057
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-12-19 15:15:26 -0500

    reverse-proxy add cron letsencript 1

--- profiles/letsencrypt_host/manifests/install.pp
+++ profiles/letsencrypt_host/manifests/install.pp
@@ -17,11 +17,11 @@ class letsencrypt_host::install {
   if($::letsencrypt_host::webroot_enable) {
     each($::letsencrypt_host::dominios)|Integer $index, String $value|{
       letsencrypt::certonly { "${value}":
-        domains       => [$value],
-        plugin        => $::letsencrypt_host::plugin,
-        webroot_paths => $::letsencrypt_host::webroot_paths
-        manage_cron => true,
-        cron_before_command => 'service nginx stop',
+        domains              => [$value],
+        plugin               => $::letsencrypt_host::plugin,
+        webroot_paths        => $::letsencrypt_host::webroot_paths,
+        manage_cron          => true,
+        cron_before_command  => 'service nginx stop',
         cron_success_command => '/bin/systemctl reload nginx.service',
         suppress_cron_output => true,
       }
@@ -30,10 +30,10 @@ class letsencrypt_host::install {
   else {
     each($::letsencrypt_host::dominios)|Integer $index, String $value|{
       letsencrypt::certonly { "${value}":
-        domains => [$value],
-        plugin  => $::letsencrypt_host::plugin,
-        manage_cron => true,
-        cron_before_command => 'service nginx stop',
+        domains              => [$value],
+        plugin               => $::letsencrypt_host::plugin,
+        manage_cron          => true,
+        cron_before_command  => 'service nginx stop',
         cron_success_command => '/bin/systemctl reload nginx.service',
         suppress_cron_output => true,
       }
