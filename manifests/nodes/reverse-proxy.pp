6ae9dbc15aaa495b80f652e1a808729e7471f705
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-12-20 09:54:12 -0500

    HA reverse-proxy 01

--- manifests/nodes/reverse-proxy.pp
+++ manifests/nodes/reverse-proxy.pp
@@ -15,8 +15,25 @@ node 'nginx-inverso.upr.edu.cu','proxy-inverso.upr.edu.cu','toran-proxy.upr.edu.
 node /^reverse-proxy\d+$/{
 
   $hosts = '
-  10.2.1.30 puppet-master.upr.edu.cu puppetboard
+  10.2.1.30 puppet-master.upr.edu.cu puppetboard.upr.edu.cu
   10.2.24.85 repos.upr.edu.cu
+  10.2.25.12 correo.upr.edu.cu
+  10.2.24.142 cvforestal.upr.edu.cu
+  10.2.24.48 intranet.upr.edu.cu
+  10.2.24.145 proxy-login.upr.edu.cu
+  200.14.49.8 nexus.upr.edu.cu harbor.upr.edu.cu
+  200.55.143.12 proxy-go.upr.edu.cu
+  10.2.1.210 gitlab.upr.edu.cu
+  10.2.1.49 icingaweb.upr.edu.cu
+  10.2.24.82 composer.upr.edu.cu
+  10.2.24.133 bower.upr.edu.cu
+  10.2.24.69 cooder.upr.edu.cu
+  10.2.24.128 npm.upr.edu.cu
+  10.2.24.116 mendive.upr.edu.cu podium.upr.edu.cu cfores.upr.edu.cu cifam.upr.edu.cu coodes.upr.edu.cu revistaecovida.upr.edu.cu 
+  10.2.24.120 rc.upr.edu.cu
+  10.2.24.119 crai.upr.edu.cu blogcrai.upr.edu.cu
+  10.2.24.163 tocororo.upr.edu.cu
+  10.2.24.230 tocororo.upr.edu.cu
   '
   file_line { 'add_ip_reverse':
     ensure => present,
