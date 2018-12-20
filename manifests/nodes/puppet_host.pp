61079bf8bb25d590de5e08351e45283ed2b50abe
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-12-20 11:10:26 -0500

    test haproxy 1

--- manifests/nodes/puppet_host.pp
+++ manifests/nodes/puppet_host.pp
@@ -29,58 +29,7 @@ node 'client-puppet.upr.edu.cu'{
   #}
 
   #class { 'squidserver':;}
-  
-  $zone = 'type slave'
-  $allow = "{any;}"
-  $direct = "/etc/bind/zone"
-  $masters = "{200.14.49.2; }"
-  class {'::dns_primary':
-    config_file        => '/etc/bind/named.conf',
-    directory          => $direct,
-    dump_file          => 'cache_dump.db',
-    statistics_file    => 'named_stats.txt',
-    memstatistics_file => 'named_mem_stats.txt',
-    allow_query        => $allow,
-    recursion          => 'yes',
-    zone_name          => [ 'upr.edu.cu'],
-    zone_type          => $zone,
-    slave              => false,
-    mymaster           => $masters,
-    file_zone_name     => [ 'db.upr.edu.cu', 'db.49.14.200.in-addr.arpa', 'db.143.55.200.in-addr.arpa', 'db.173.207.152.in-addr.arpa'],
-    zone_reverse       => [ '49.14.200.in-addr.arpa', '143.55.200.in-addr.arpa', '173.207.152.in-addr.arpa'],
-    zones              => {
-      'upr.edu.cu' => [
-        $zone,
-        "file  ${direct}/db.upr.edu.cu",
-        "allow-query $allow",
-        'notify yes',
-        "masters $masters",
-      ],
-      '27/0.49.14.200.in-addr.arpa' => [
-        $zone,
-       "allow-query $allow", 
-        "file ${direct}/db.49.14.200.in-addr.arpa",
-        "masters $masters",
-      ],
-      '29/8.143.55.200.in-addr.arpa' => [
-        $zone,
-        "allow-query $allow",
-        "file ${direct}/db.143.55.200.in-addr.arpa",
-        "masters $masters",
-      ],
-      '29/40.173.207.152.in-addr.arpa' => [
-        $zone,
-        "allow-query $allow",
-        'file "db.173.207.152.in-addr.arpa"',
-        "masters $masters",
-      ],
-    },
-  }
-
-
 }
-
-
 node 'puppet-test.upr.edu.cu'{
   class { '::basesys':
     uprinfo_usage => 'servidor test',
@@ -101,7 +50,7 @@ node 'puppet-test1.upr.edu.cu'{
   include ::haproxy
   haproxy::listen { 'test':
     collect_exported => false,
-    ipaddress        => $::ipaddress,
+    ipaddress        => '10.2.1.155',
     ports            => '80',
   }
   haproxy::balancermember { 'test00':
