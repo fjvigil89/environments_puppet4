10892c03178b81799a1526b9531579ee72e4d7dd
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-12-21 15:23:22 -0500

    try again 2

--- profiles/haproxy_serv/manifests/balancemember.pp
+++ profiles/haproxy_serv/manifests/balancemember.pp
@@ -8,7 +8,8 @@ class haproxy_serv::balancemember (){
   each($::haproxy_serv::params::balancer_member) |Integer $index, String $value|{
     haproxy::balancermember { $::haproxy_serv::params::balancer_member[$index]:
     listening_service => $::haproxy_serv::params::listening_service,
-    server_names      => $::haproxy::params::server_names[$index],
+    #server_names     => $::haproxy::params::server_names[$index],
+    server_names      => ['puppet-test1.upr.edu.cu','puppet-test2.upr.edu.cu'],
     ipaddresses       => $::haproxy::params::ipaddresses[$index],
     ports             => $::haproxy::params::ports[$index],
     options           => $::haproxy::params::options,
