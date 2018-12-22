b560772ac586380397a14b99b9907c432878df69
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-12-22 09:44:43 -0500

    test haproxy 1

--- profiles/haproxy_serv/manifests/balancemember.pp
+++ profiles/haproxy_serv/manifests/balancemember.pp
@@ -9,13 +9,13 @@ class haproxy_serv::balancemember (){
   each($::haproxy_serv::params::balancer_member) |Integer $index, String $value|{
     haproxy::balancermember { $::haproxy_serv::params::balancer_member[$index]:
     listening_service => $::haproxy_serv::params::listening_service,
-    server_names     => $::haproxy::params::server_names[$index],
+    server_names     => $::haproxy::server_names[$index],
     #server_names      => ['nginx01.upr.edu.cu','nginx02.upr.edu.cu'],
-    ipaddresses      => $::haproxy::params::ipaddresses[$index],
+    ipaddresses      => $::haproxy::ipaddresses[$index],
     #ipaddresses       => ['10.2.1.77','10.2.1.79'],
-    ports            => $::haproxy::params::ports[$index],
+    ports            => $::haproxy::ports[$index],
     #ports             => ['80'],
-    options           => $::haproxy::params::options,
+    options           => $::haproxy::options,
   }
   }
 }
