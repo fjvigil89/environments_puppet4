e0656b267f5c16535efaf05a073d308f47e38265
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-12-22 10:01:14 -0500

    test

--- profiles/haproxy_serv/manifests/balancemember.pp
+++ profiles/haproxy_serv/manifests/balancemember.pp
@@ -6,15 +6,17 @@
 #
 class haproxy_serv::balancemember (){
   include ::haproxy_serv::params
+  $serv_names = $::haproxy::server_names,
   each($::haproxy_serv::params::balancer_member) |Integer $index, String $value|{
     haproxy::balancermember { $::haproxy_serv::params::balancer_member[$index]:
     listening_service => $::haproxy_serv::params::listening_service,
-    server_names     => $::haproxy::server_names[$index],
-    #server_names      => ['nginx01.upr.edu.cu','nginx02.upr.edu.cu'],
-    ipaddresses      => $::haproxy::ipaddresses[$index],
-    #ipaddresses       => ['10.2.1.77','10.2.1.79'],
-    ports            => $::haproxy::ports[$index],
-    #ports             => ['80'],
+    #server_names     => $::haproxy::server_names[$index],
+    server_names      => $serv_names[$index],
+    #server_names     => ['nginx01.upr.edu.cu','nginx02.upr.edu.cu'],
+    ipaddresses       => $::haproxy::ipaddresses[$index],
+    #ipaddresses      => ['10.2.1.77','10.2.1.79'],
+    ports             => $::haproxy::ports[$index],
+    #ports            => ['80'],
     options           => $::haproxy::options,
   }
   }
