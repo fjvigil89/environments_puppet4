68e714449b8c92f07cbc6fba7c358c9168943468
Arian Trujillo Díaz <arian@upr.edu.cu>
2018-12-22 09:26:09 -0500

    test haproxy

--- profiles/haproxy_serv/manifests/balancemember.pp
+++ profiles/haproxy_serv/manifests/balancemember.pp
@@ -9,11 +9,11 @@ class haproxy_serv::balancemember (){
     haproxy::balancermember { $::haproxy_serv::params::balancer_member[$index]:
     listening_service => $::haproxy_serv::params::listening_service,
     #server_names     => $::haproxy::params::server_names[$index],
-    server_names      => $::haproxy::params::server_names,
+    server_names      => ['nginx01.upr.edu.cu','nginx02.upr.edu.cu'],
     #ipaddresses      => $::haproxy::params::ipaddresses[$index],
-    ipaddresses       => $::haproxy::params::ipaddresses,
+    ipaddresses       => ['10.2.1.77','10.2.1.79'],
     #ports            => $::haproxy::params::ports[$index],
-    ports             => $::haproxy::params::ports,
+    ports             => ['80'],
     options           => $::haproxy::params::options,
   }
   }
