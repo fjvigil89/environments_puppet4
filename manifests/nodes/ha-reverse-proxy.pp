21090ce08e295a46b20d9ab373f95a38326f6239
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-12-23 12:07:46 -0500

    HA reverse-proxy

---
+++ manifests/nodes/ha-reverse-proxy.pp
@@ -0,0 +1,22 @@
+node 'ha-reverse-proxy.upr.edu.cu' {  
+  package { 'lsb-release':
+          ensure => installed,
+  }~>
+  class { '::basesys':
+    uprinfo_usage   => 'servidor gestion',
+    application     => 'Proxmox Gestion',
+    proxmox_enabled => true,
+    repos_enabled   => true,
+    mta_enabled     => false,
+  }
+  class {'::haproxy_serv':
+    ipaddress         => $ipaddress,
+    listening_service => 'nginx',
+    mode              => 'http',
+    balancer_member   => ['reverse-proxy0', 'reverse-proxy1'],
+    server_names      => ['reverse-proxy0.upr.edu.cu', 'reverse-proxy1.upr.edu.cu'],
+    ipaddresses       => ['200.14.49.6','200.14.49.8'],
+    ports             => ['80','443'],
+    #options           => 'check',
+  }
+}
