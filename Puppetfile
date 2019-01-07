f71a77a6bee222edd4a51a364b53aec71745ff02
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-01-07 09:17:12 -0500

    elastic

--- Puppetfile
+++ Puppetfile
@@ -78,12 +78,12 @@ mod 'meltwater-cpan', '2.0.0'
 mod 'puppetlabs-haproxy', '2.2.0'
 
 #ELK
-#mod 'elastic-logstash', '6.1.5'
-#mod 'elastic-kibana', '6.3.1'
-#mod 'elastic-elasticsearch', '6.3.3'
-#mod 'richardc-datacat', '0.6.2'
-#mod 'elastic-elastic_stack', '6.2.4'
-#mod 'pcfens-filebeat', '3.3.3'
+mod 'elastic-logstash', '6.1.5'
+mod 'elastic-kibana', '6.3.1'
+mod 'elastic-elasticsearch', '6.3.3'
+mod 'richardc-datacat', '0.6.2'
+mod 'elastic-elastic_stack', '6.2.4'
+mod 'pcfens-filebeat', '3.3.3'
 #ldap
 mod 'camptocamp-openldap', '1.17.0'
 #Proxy
--- manifests/nodes/puppet_host.pp
+++ manifests/nodes/puppet_host.pp
@@ -8,6 +8,72 @@ node 'client-puppet.upr.edu.cu'{
 
   }
   #class { 'squidserver':;}
+  $myconfig =  @("MYCONFIG"/L)
+  input {
+    beats {
+      port => 5043
+    }
+  }
+  output {
+    elasticsearch {
+      hosts => "10.2.1.205:9200"
+      manage_template => false
+      index => "%{[@metadata][beat]}-%{+YYYY.MM.dd}"
+      document_type => "%{[@metadata][type]}"
+    }
+    stdout { codec => rubydebug }
+  }
+  | MYCONFIG
+
+  include ::java
+  class { 'elasticsearch':
+    #elasticsearch_user => 'elasticsearch',
+    manage_repo        => true,
+    restart_on_change  => true,
+    autoupgrade        => true,
+  }
+  elasticsearch::instance { 'es-01':
+     config => {
+      'network.host' => '10.2.1.205',
+    },
+  }
+  class { 'logstash':
+    settings => {
+      'http.host' => '10.2.1.205',
+    }
+  }
+
+  logstash::configfile { '02-beats-input.conf':
+    content => $myconfig,
+  }
+  logstash::plugin { 'logstash-input-beats': }
+
+  class { 'kibana' :
+    config => {
+      'server.host'       =>  '10.2.1.205',
+      'elasticsearch.url' => 'http://10.2.1.205:9200',
+      'server.port'       => '5601',
+    }
+  }
+
+  class { 'filebeat':
+    outputs => {
+      'logstash' => {
+        'hosts' => [
+          '10.2.1.205:5043',
+        ],
+        'index' => 'filebeat',
+      },
+    },
+  }
+
+  filebeat::prospector { 'syslogs':
+  paths    => [
+      '/var/log/auth.log',
+      '/var/log/syslog',
+    ],
+    doc_type => 'syslog-beat',
+  }
 }
 node 'puppet-test1.upr.edu.cu' {
   class { '::basesys':
