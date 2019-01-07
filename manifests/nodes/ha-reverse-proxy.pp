3b096c1a0035dcb266bf3423456dd97aadcb1b90
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-01-07 13:32:44 -0500

    HA1

--- manifests/nodes/ha-reverse-proxy.pp
+++ manifests/nodes/ha-reverse-proxy.pp
@@ -13,9 +13,9 @@ node 'ha-reverse-proxy.upr.edu.cu' {
     ipaddress         => $ipaddress,
     listening_service => 'nginx',
     mode              => 'http',
-    balancer_member   => ['reverse-proxy0'],
-    server_names      => ['reverse-proxy0.upr.edu.cu'],
-    ipaddresses       => ['200.14.49.6'],
+    balancer_member   => ['reverse-proxy0','reverse-proxy1'],
+    server_names      => ['reverse-proxy0.upr.edu.cu','reverse-proxy1.upr.edu.cu'],
+    ipaddresses       => ['200.14.49.6','200.14.49.5'],
     ports             => ['80','443'],
     #options           => 'check',
   }
