4b17ef1513054570640f497fd2b35c8c0f56017a
Arian Trujillo Díaz <arian@upr.edu.cu>
2019-01-10 13:04:38 -0500

    test haproxy 3 ports balancermembers

--- profiles/haproxy_serv/manifests/balancemember.pp
+++ profiles/haproxy_serv/manifests/balancemember.pp
@@ -10,7 +10,7 @@ class haproxy_serv::balancemember (){
     listening_service => $::haproxy_serv::listening_service,
     server_names      => $::haproxy_serv::server_names[$index],
     ipaddresses       => $::haproxy_serv::ipaddresses[$index],
-    ports             => $::haproxy_serv::ports[$index],
+    ports             => $::haproxy_serv::ports,
     options           => $::haproxy_serv::options,
   }
   }
