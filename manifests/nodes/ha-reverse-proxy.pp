fb5d36ee7801b07eec3f66bd48fd8f032ab6c2b2
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-01-10 16:23:22 -0500

    ssl a los proxys

--- manifests/nodes/ha-reverse-proxy.pp
+++ manifests/nodes/ha-reverse-proxy.pp
@@ -9,14 +9,35 @@ node 'ha-reverse-proxy.upr.edu.cu' {
     repos_enabled   => true,
     mta_enabled     => false,
   }
+
   class {'::haproxy_serv':
+    enable_ssl        => true,
     ipaddress         => $ipaddress,
     listening_service => 'nginx',
     mode              => 'http',
     balancer_member   => ['reverse-proxy0','reverse-proxy1'],
     server_names      => ['reverse-proxy0.upr.edu.cu','reverse-proxy1.upr.edu.cu'],
     ipaddresses       => ['200.14.49.6','200.14.49.5'],
-    ports             => ['80','80'],
-    #options           => 'check',
+    ports             => ['80','443'],
+    frontend_name     => 'nginx_server',
+    frontend_options  => {
+      'default_backend' => 'nginx_backend' ,
+      'timeout client'  => '30s' ,
+      'option'          => [
+        'tcplog',
+      ],
+      },
+    backend_names     => ['nginx_backend'],
+    backend_options   => {
+      'option'  => [
+        'tcplog',
+      ],
+      'balance' => 'roundrobin',
+    },
+    bind              => {
+      '0.0.0.0:80'  => [],
+      '0.0.0.0:443' => [],
+    },
   }
+
 }
