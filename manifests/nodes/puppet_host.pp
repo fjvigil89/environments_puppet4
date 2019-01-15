5f5b23c3bf300f317adc69bd6a9abfbf2aa02175
Arian Trujillo Díaz <arian@upr.edu.cu>
2019-01-15 11:32:25 -0500

    try again

--- manifests/nodes/puppet_host.pp
+++ manifests/nodes/puppet_host.pp
@@ -111,7 +111,6 @@ node 'puppet-test1.upr.edu.cu' {
     #cache_dirs   => { '/data/' => { type => 'ufs', options => '15000 32 256 min-size=32769', process_number => 2 }},
     #}
 }
-}
 node 'puppet-test.upr.edu.cu' {
   class { '::basesys':
     uprinfo_usage => 'servidor test',
