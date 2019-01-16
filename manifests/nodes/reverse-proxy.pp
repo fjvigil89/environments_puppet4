beaba7783833cc59c45952394f14652e0b3a011f
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-01-16 16:24:31 -0500

    quitando trabajo reverse proxy con hosts

--- manifests/nodes/reverse-proxy.pp
+++ manifests/nodes/reverse-proxy.pp
@@ -14,57 +14,6 @@ node 'toran-proxy.upr.edu.cu' {
 ##Para hacer los proxy inversos
 node /^reverse-proxy\d+$/{
 
-  $hosts = "
-  es127.0.0.1     localhost
-  ::1             localhost ip6-localhost ip6-loopback
-  ff02::1         ip6-allnodes
-  ff02::2         ip6-allrouters
-  $::ipaddress $::fqdn
-  10.2.1.30 puppet-master.upr.edu.cu puppetboard.upr.edu.cu
-  10.2.24.85 repos.upr.edu.cu
-  10.2.25.12 correo.upr.edu.cu
-  10.2.24.142 cvforestal.upr.edu.cu
-  10.2.24.48 intranet.upr.edu.cu
-  10.2.24.145 proxy-login.upr.edu.cu
-  200.14.49.8 nexus.upr.edu.cu harbor.upr.edu.cu
-  200.55.143.12 proxy-go.upr.edu.cu
-  10.2.1.210 gitlab.upr.edu.cu
-  10.2.1.49 icingaweb.upr.edu.cu
-  10.2.24.82 composer.upr.edu.cu
-  10.2.24.133 bower.upr.edu.cu
-  10.2.24.69 cooder.upr.edu.cu www.upr.edu.cu
-  10.2.24.128 npm.upr.edu.cu
-  10.2.24.116 mendive.upr.edu.cu podium.upr.edu.cu cfores.upr.edu.cu cifam.upr.edu.cu coodes.upr.edu.cu revistaecovida.upr.edu.cu 
-  10.2.24.120 rc.upr.edu.cu
-  10.2.24.119 crai.upr.edu.cu blogcrai.upr.edu.cu
-  10.2.24.163 tocororo.upr.edu.cu
-  10.2.24.230 eventos.upr.edu.cu
-  10.2.24.126 moodle.upr.edu.cu
-  10.2.24.220 telefonos.upr.edu.cu
-  10.2.1.10 mrtg.upr.edu.cu
-  10.2.24.70 ftp.upr.edu.cu
-  10.2.24.33 media.upr.edu.cu
-  10.2.24.232 abcd3.upr.edu.cu
-  10.2.24.32 irc.upr.edu.cu
-
-  " 
-  file {'add_hosts':
-    ensure  => present,
-    path    => '/etc/hosts',
-    content => "",
-    before  => File_line["add_ip_reverse"],
-  }
-  file_line { 'add_ip_reverse':
-    ensure                                => present,
-    path                                  => '/etc/hosts',
-    line                                  => $hosts,
-    replace                               => true,
-    #match                                => '^# --- END PVE ---',
-    after                                 => '^# --- END PVE ---',
-    match_for_absence                     => true,
-    replace_all_matches_not_matching_line => true,
-
-  }
 
   class { 'reverse_proxy_server':
     server_name    => [
