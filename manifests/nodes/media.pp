26c45d2602003e1663eb5691a15f1ebd4ba68328
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-01-23 14:43:43 -0500

    ha-media

--- manifests/nodes/media.pp
+++ manifests/nodes/media.pp
@@ -19,46 +19,11 @@ node 'ha-media.upr.edu.cu' {
     server_names      => ['media0.upr.edu.cu','media1.upr.edu.cu','media2.upr.edu.cu'],
     ipaddresses       => ['10.2.24.3','10.2.24.4','10.2.24.5'],
     ports             => ['80'],
-    frontend_name     => ['media_server'],
-    frontend_options  => {
-      'default_backend' => 'media_backend' ,
-      'timeout client'  => '30s' ,
-      'option'          => [
-        'tcplog',
-      ],
-      },
-    backend_names     => ['media_backend'],
-    backend_options   => {
-      'option'  => [
-        'tcplog',
-      ],
-      'balance' => 'roundrobin',
-    },
-    bind              => {
-      '0.0.0.0:80'  => [],
-    },
   }
-  haproxy::frontend { 'media_http':
-    mode    => $frontend_mode,
-    options => {
-      'default_backend' => 'media_bhttp' ,
-      'timeout client'  => '30s' ,
-      'option'          => [
-        'tcplog',
-      ],
-    },
-    bind    => {
-      '0.0.0.0:80'  => [],
-    },
-  }
-  haproxy::backend { 'media_bhttp':
-    mode    => 'http',
-    options => {
-      'option'  => [
-        'tcplog',
-      ],
-      'balance' => 'roundrobin',
-    },
+  haproxy::listen { 'media_bhttp':
+    collect_exported => false,
+    ipaddress        => $::ipaddress,
+    ports            => '80',
   }
   haproxy::balancermember { 'media0.upr.edu.cu':
     listening_service => 'media_bhttp',
