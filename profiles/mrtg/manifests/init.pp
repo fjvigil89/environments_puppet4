9ced781a9e9f9df672ba6aca5e6b3f8989b81ffc
Henry <henry.fleitas@upr.edu.cu>
2019-01-24 16:49:51 -0500

    Modulos mrtg

--- profiles/mrtg/manifests/init.pp
+++ profiles/mrtg/manifests/init.pp
@@ -1,37 +1,15 @@
-#Clase MRTG
+#Clase mrtgserver
 
-class mrtg (
-  $owner = $::mrtg::params::owner,
-  $group = $::mrtg::params::group,
-  $mode = $::mrtg::params::mode,
-  $community = $::mrtg::params::community,
-  Array[String] $ip = $::mrtg::params::ip,
-  Array[String] $names = $::mrtg::params::name,
-)inherits ::mrtg::params {
-  include ::mrtg::apache
-  include ::mrtg::device
-  package { 'mrtg':
-    name   => $operatingsystem ? {
-      default => "mrtg",
-    },
-    ensure => installed,
-  }
-
-  file { "mrtg.cfg":
-    owner   => $owner,
-    group   => $group,
-    mode    => $mode,
-    require => Package["mrtg"],
-    ensure  => present,
-    path    => $operatingsystem ? {
-    default => "/etc/mrtg/mrtg.cfg"
-    },
-  }
-
-  file { '/var/www/mrtg':
-    ensure => directory,
-    owner  => $owner,
-    group  => $group,
-    mode   => $mode,
-  }
+class mrtgserver (
+  $owner = $::mrtgserver::params::owner,
+  $group = $::mrtgserver::params::group,
+  $mode = $::mrtgserver::params::mode,
+  $community = $::mrtgserver::params::community,
+  Array[String] $ip = $::mrtgserver::params::ip,
+  Array[String] $names = $::mrtgserver::params::name,
+)inherits ::mrtgserver::params {
+  class {'::mrtgserver::apache':;}
+  class {'::mrtgserver::device':;}
+  class {'::mrtgserver::mrtg':;}
 }
+
