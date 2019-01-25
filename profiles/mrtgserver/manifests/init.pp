3c02e6a162dfc6508ec7cd501a4fa5e6de389ab2
Henry <henry.fleitas@upr.edu.cu>
2019-01-25 15:17:05 -0500

    edit init

--- profiles/mrtgserver/manifests/init.pp
+++ profiles/mrtgserver/manifests/init.pp
@@ -1,15 +1,19 @@
 #Clase mrtgserver
 
 class mrtgserver (
+  
   $owner = $::mrtgserver::params::owner,
   $group = $::mrtgserver::params::group,
   $mode = $::mrtgserver::params::mode,
   $community = $::mrtgserver::params::community,
+  
   Array[String] $ip = $::mrtgserver::params::ip,
   Array[String] $names = $::mrtgserver::params::name,
+
 )inherits ::mrtgserver::params {
+  
   class {'::mrtgserver::apache':;}
-  #  class {'::mrtgserver::device':;}
   class {'::mrtgserver::mrtg':;}
+  #  class {'::mrtgserver::device':;}
 }
 
