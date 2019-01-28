10ed3c1583394038b8ec54ddf1bebb5574f5638e
Henry <henry.fleitas@upr.edu.cu>
2019-01-28 13:45:21 -0500

    ADD cron

--- profiles/mrtgserver/manifests/mrtg.pp
+++ profiles/mrtgserver/manifests/mrtg.pp
@@ -22,14 +22,19 @@ class mrtgserver::mrtg(){
   #    default => "/etc/mrtg/mrtg.cfg"
   #  },
   #}
-  exec {'cfgmaker':
-    creates => "/etc/mrtg/10.2.1.1.cfg",
-    command => "/usr/bin/cfgmaker network4core@dminUPR@10.2.1.1 > /etc/mrtg/10.2.1.1.cfg"
-  }
   file { '/var/www/mrtg':
     ensure => directory,
     owner  => $owner,
     group  => $group,
     mode   => $mode,
+  }
+  exec {'cfgmaker':
+    creates => "/etc/mrtg/10.2.1.1.cfg",
+    command => "/usr/bin/cfgmaker network4core@dminUPR@10.2.1.1 > /etc/mrtg/10.2.1.1.cfg"
+  }
+  cron {'indexmaker':
+    user    => $owner,
+    command => '/usr/bin/indexmaker --columns=2 --addhead="<H1 align= "center" > Multi Router Traffic Grapher <H1>" --title="Tr&aacute;fico de Enlaces UPR" /etc/mrtg/10.2.1.1.cfg > /var/www/mrtg/index.html',
+    minute  => '*/1'
   }
 }
