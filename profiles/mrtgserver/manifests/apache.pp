6f3d2bc46edf98f0cdc5d9ebbff1df0add86aa31
Henry <henry.fleitas@upr.edu.cu>
2019-01-29 16:27:54 -0500

    Vhost ok

--- profiles/mrtgserver/manifests/apache.pp
+++ profiles/mrtgserver/manifests/apache.pp
@@ -1,14 +1,13 @@
 ## Manage Apache config 
 
 class mrtgserver::apache(){
-  
   include apache2
 
   apache::vhost { 'mrtg':
-    port          => $port,
-    docroot       => $docroot,
-    servername    => $servername,
-    aliases       => $alias,
+    port       => $port,
+    docroot    => $docroot,
+    servername => $servername,
+    aliases    => $alias,
   }
 
   apache::vhost { 'whois':
