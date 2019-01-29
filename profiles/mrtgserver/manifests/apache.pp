4f43635692a0c0ac29bc5ff1e89cfe08406cd083
Henry <henry.fleitas@upr.edu.cu>
2019-01-29 13:23:19 -0500

    Vhost

--- profiles/mrtgserver/manifests/apache.pp
+++ profiles/mrtgserver/manifests/apache.pp
@@ -1,58 +1,13 @@
 ## Manage Apache config 
 
 class mrtgserver::apache(){
-  case $facts['operatingsystem'] {
-    'CentOS', 'RedHat': {
-      if $facts['architecture'] == 'x86_64' {
-        package { 'httpd':
-          name   => 'httpd.x86_64',
-          ensure => installed,
-        }
-      }
-      else {
-        package { 'httpd':
-          name   => 'httpd.i386',
-          ensure => installed,
-        }
-      }
-      service { 'httpd':
-        ensure => running,
-        enable => true,
-      }
-      file { 'http.conf':
-        path   => '/etc/httpd/conf/httpd.conf',
-        owner  => $owner,
-        group  => $group,
-        mode   => $mode,
-        ensure => present,
-        }
-      }
-    'Debian', 'Ubuntu': {
-      package { 'apache2':
-        name   => 'apache2',
-        ensure => installed,
-      }
-      service {'apache2':
-        ensure => running,
-        enable => true,
-      }
-      file { 'apache2.conf':
-        path   => '/etc/apache2/apache2.conf',
-        owner  => $owner,
-        group  => $group,
-        mode   => $mode,
-        ensure => present,
-      }
-    }
-    default : {}
-  }
+  
+  include apache2
+
   apache::vhost { 'mrtg':
-    port          => '80',
-    suphp_engine  => 'on',
-    docroot       => '/var/www/mrtgpup',
-    servername    => 'mrtg-pup.upr.edu.cu',
-    aliases       => 'mrtg',
-    docroot_owner => 'www-data',
-    docroot_group => 'www-data',
+    port          => $port,
+    docroot       => $docroot,
+    servername    => $servername,
+    aliases       => $alias,
   }
 }
