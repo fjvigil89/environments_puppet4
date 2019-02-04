94461d46021a29d31d48597a13e5e8f3fabfd668
Henry <henry.fleitas@upr.edu.cu>
2019-02-04 15:25:40 -0500

    parametro ip y names

--- profiles/mrtgserver/manifests/params.pp
+++ profiles/mrtgserver/manifests/params.pp
@@ -9,6 +9,6 @@ class mrtgserver::params {
   #$docroot = '/var/www/mrtgpup'
   #$servername = 'mrtg-pup.upr.edu.cu'
   #$alias = 'mrtg'
-  #$ip = ['10.2.1.1','10.2.8.6']
-  #$names = ['L3','2960']
+  $ip = ['10.2.1.1','10.2.8.6']
+  $names = ['L3','2960']
   }
