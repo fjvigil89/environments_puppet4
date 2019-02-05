f046478c76719d9680c96b36515aff9bb82951df
Henry <henry.fleitas@upr.edu.cu>
2019-02-05 13:53:02 -0500

    Test dos exec

--- profiles/mrtgserver/manifests/mrtg.pp
+++ profiles/mrtgserver/manifests/mrtg.pp
@@ -10,13 +10,24 @@ class mrtgserver::mrtg(){
     group  => $group,
     mode   => $mode,
   }
-  exec {'cfgmaker':
+  exec {'primero':
     creates => "/etc/mrtg/10.2.1.1.cfg",
     command => "/usr/bin/cfgmaker network4core@dminUPR@10.2.1.1 > /etc/mrtg/10.2.1.1.cfg"
   }
+  exec {'segundo':
+    creates => "/etc/mrtg/segundo.cfg",
+    command => "/usr/bin/cfgmaker network4core@dminUPR@10.2.1.1 > /etc/mrtg/segundo.cfg"
+  }
+
   cron {'indexmaker':
     user    => $owner,
     command => '/usr/bin/indexmaker --columns=2 --addhead="<H1 align= "center" > Multi Router Traffic Grapher <H1>" --title="Tr&aacute;fico de Enlaces UPR" /etc/mrtg/10.2.1.1.cfg > /var/www/mrtg/index.html',
     minute  => '*/1'
   }
 }
+
+#each($::mrtgserver::ip) |Integer $index|{
+#  exec {$::mrtgserver::name[$value]:
+#  creates => "/etc/mrtg/$::mrtgserver::name[$value]",
+#  command => "/usr/bin/cfgmaker network4core@dminUPR@$::mrtgserver::ip[$value] > /etc/mrtg/$::mrtgserver::name[$value].cfg"
+#}}
