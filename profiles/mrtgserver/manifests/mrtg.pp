6ee3f2d3cae08836e4db30d011890c4170c7c9a7
Henry <henry.fleitas@upr.edu.cu>
2019-02-05 15:50:40 -0500

    Test exec con names2

--- profiles/mrtgserver/manifests/mrtg.pp
+++ profiles/mrtgserver/manifests/mrtg.pp
@@ -13,7 +13,7 @@ class mrtgserver::mrtg(){
   each($::mrtgserver::names) |Integer $index, String $value|{
     exec { $value :
       creates => "/etc/mrtg/$value.cfg",
-      command => "/usr/bin/cfgmaker network4core@dminUPR@$::mrtgserver::ip[$index] > /etc/mrtg/$value.cfg"
+      command => "'/usr/bin/cfgmaker network4core@dminUPR@'$::mrtgserver::ip[$index] > '/etc/mrtg/'$value.cfg"
     }
   }
   # exec {'cfgmaker':
