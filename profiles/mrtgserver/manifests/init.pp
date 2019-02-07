8adf3a44e2ace3c8029c5c7f95b775f98359285a
Henry <henry.fleitas@upr.edu.cu>
2019-02-07 09:39:08 -0500

    Test comunidad4

--- profiles/mrtgserver/manifests/init.pp
+++ profiles/mrtgserver/manifests/init.pp
@@ -5,7 +5,7 @@ class mrtgserver (
   $owner = $::mrtgserver::params::owner,
   $group = $::mrtgserver::params::group,
   $mode = $::mrtgserver::params::mode,
-  $comunidad = $::mrtgserver::params::community,
+  $comunidad = $::mrtgserver::params::comunidad,
   
   Array[String] $ip = $::mrtgserver::params::ip,
   Array[String] $names = $::mrtgserver::params::names,
