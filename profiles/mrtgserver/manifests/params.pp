777275496a6be1c7ad7172d6b5aab698f2d0e994
Henry <henry.fleitas@upr.edu.cu>
2019-02-07 09:40:07 -0500

    Test comunidad4

--- profiles/mrtgserver/manifests/params.pp
+++ profiles/mrtgserver/manifests/params.pp
@@ -5,6 +5,6 @@ class mrtgserver::params {
   $group = 'root'
   $mode  = '0775'
   $comunidad = 'network4core@dminUPR'
-  $ip = ['10.2.1.1','10.2.8.6','192.168.200.1','10.2.8.2']
+  $ip = ['10.2.1.1','192.168.200.1','10.2.8.2']
   $names = ['L3','2960']
   }
