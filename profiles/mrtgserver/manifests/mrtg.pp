9f186912fb8eac9ba90558f598bbf3d5922c8eda
Henry <henry.fleitas@upr.edu.cu>
2019-02-08 09:19:55 -0500

    Test create file

--- profiles/mrtgserver/manifests/mrtg.pp
+++ profiles/mrtgserver/manifests/mrtg.pp
@@ -21,6 +21,9 @@ class mrtgserver::mrtg(){
     command => '/usr/bin/indexmaker --columns=2 --addhead="<H1 align= "center" > Multi Router Traffic Grapher <H1>" --title="Tr&aacute;fico de Enlaces UPR" /etc/mrtg/10.2.1.1.cfg /etc/mrtg/192.168.200.1.cfg /etc/mrtg/10.2.8.2.cfg > /var/www/mrtg/index.html',
     minute  => '*/1'
   }
+  file { 'prueba':
+    source => "/etc/puppetlabs/code/environments/henry/profiles/mrtgserver/manifests/mrtg.pp"
+  }
 }
   #Agregar en /etc/cron.d/mrtg:
   #  */1 * * * *   root   env LANG=C /usr/bin/mrtg /etc/mrtg/10.2.1.1.cfg 2> /dev/null&
