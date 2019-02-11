736dd9242f30af47a1da914d276d99567fc2c77c
Henry <henry.fleitas@upr.edu.cu>
2019-02-11 09:28:54 -0500

    Ordenar indexmaker

--- profiles/mrtgserver/manifests/mrtg.pp
+++ profiles/mrtgserver/manifests/mrtg.pp
@@ -18,11 +18,8 @@ class mrtgserver::mrtg(){
   }
   cron {'indexmaker':
     user    => $owner,
-    command => '/usr/bin/indexmaker --columns=2 --addhead="<H1 align= "center" > Multi Router Traffic Grapher <H1>" --title="Tr&aacute;fico de Enlaces UPR" /etc/mrtg/10.2.1.1.cfg /etc/mrtg/192.168.200.1.cfg /etc/mrtg/10.2.8.2.cfg > /var/www/mrtg/index.html',
+    command => '/usr/bin/indexmaker --columns=2 --addhead="<H1 align= "center" > Multi Router Traffic Grapher <H1>" --title="Tr&aacute;fico de Enlaces UPR" /etc/mrtg/192.168.200.1.cfg /etc/mrtg/10.2.1.1.cfg /etc/mrtg/10.2.8.2.cfg > /var/www/mrtg/index.html',
     minute  => '*/1'
-  }
-  file { '/etc/prueba':
-    source => "/etc/puppetlabs/code/environments/henry/profiles/mrtgserver/manifests/mrtg.pp"
   }
 }
   #Agregar en /etc/cron.d/mrtg:
