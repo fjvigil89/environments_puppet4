bb44e29960b5ace54a3ca0fab83ef46c29e18ba0
Arian Trujillo Díaz <arian@upr.edu.cu>
2019-03-02 16:13:05 -0500

    test repos 1

--- manifests/nodes/repos_os.pp
+++ manifests/nodes/repos_os.pp
@@ -9,17 +9,10 @@ node 'repos.upr.edu.cu' {
     repos_enabled   => false,
     mta_enabled     => false,
   }
-  class { 'debmirror':
-    ensure        => 'present',
-    allowed_hosts => ['10.2.0.0/15', '200.14.49.0/27', '200.55.143.8/29'],
-    datadir       => '/srv/repos',
-  }
-  debmirror::repository { 'debian':
-    ensure => 'present',
-    mirror => 'repos.uclv.edu.cu',
-    arch   => 'amd64',
-    hour   => '5',
-    minute => '05',
-    cron   => 'yes',
+  aptly::mirror { 'debian_stable':
+    location      => 'http://repos.uclv.edu.cu',
+    distribution  => 'stable',
+    components    => [ 'main' ],
+    architectures => ['amd64'],
   }
 }
