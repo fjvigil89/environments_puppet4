3e36a42c301c8e4c9e87716874e5326bc9d29912
Arian Trujillo Díaz <arian@upr.edu.cu>
2019-03-02 15:21:26 -0500

    delete old repo manifest node

--- manifests/nodes/repos.pp
+++
@@ -1,9 +0,0 @@
-node 'repos.upr.edu.cu' {  
-    class { '::basesys':
-    uprinfo_usage  => 'Servidor Repositorio de Paquetes Linux',
-    application    => 'Repositorios debmirro,lftp.rsync',
-    #puppet_enabled => false,
-    repos_enabled  => true,
-    mta_enabled    => false,
-  }
-}
