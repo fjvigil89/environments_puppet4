6443e9c1d81f670134c211caf98c6451bc61bcb7
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-03-06 11:17:57 -0500

    phpipam desde la upr

--- manifests/nodes/phpipam.pp
+++ manifests/nodes/phpipam.pp
@@ -4,7 +4,17 @@ node 'phpipam.upr.edu.cu'{
       packages     => ['php7.2-mbstring','r10k','php7.2','php7.2-cli','php7.2-curl','php7.2-intl','php7.2-ldap','php7.2-mysql','php7.2-sybase','libapache2-mod-php7.2','php7.2-mcrypt','phpmyadmin','freetds-bin','freetds-common', 'php7.2-gd','php7.2-gmp'],
       manage_repos => true, 
 
-    } 
+    }
+    vcsrepo { '/var/www/phpipam/':
+      ensure     => latest,
+      provider   => 'git',
+      remote     => 'origin',
+      source     => {
+        'origin' => 'git@gitlab.upr.edu.cu:dcenter/phpipam.git',
+      },
+      revision   => 'master',
+      submodules => true,
+    }~>
   apache::vhost { 'phpipam.upr.edu.cu non-ssl':
     servername    => 'phpipam.upr.edu.cu',
     serveraliases => ['www.phpipam.upr.edu.cu'], 
