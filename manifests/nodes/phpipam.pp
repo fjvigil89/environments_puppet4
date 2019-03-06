ed15c708bdb341dfdafb364308a6213fa0d4ec6e
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-03-06 12:54:47 -0500

    quitar redireccionaiento para https

--- manifests/nodes/phpipam.pp
+++ manifests/nodes/phpipam.pp
@@ -26,16 +26,16 @@ node 'phpipam.upr.edu.cu'{
       'allow_override' => 'All',
       'directoryindex' => 'index.php',
       },],
-      redirect_status  => 'permanent',
-      redirect_dest    => 'https://phpipam.upr.edu.cu/',
+      #redirect_status  => 'permanent',
+      #redirect_dest    => 'https://phpipam.upr.edu.cu/',
   }
 
   exec{"a2enmod_php7":
     command => '/usr/bin/sudo a2enmod php7.0',
-  }
+  }~>
   exec{"a2enmod_rewrite":
     command => '/usr/bin/sudo a2enmod rewrite',
-  }
+  }~>
   exec{"service_apache2_restart":
     command     => '/usr/bin/sudo service apache2 restart',
     refreshonly => true;
