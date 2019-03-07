6b0b69351c19f5ce7965a656b24b3f7161188613
Arian Trujillo Díaz <arian@upr.edu.cu>
2019-03-07 10:42:26 -0500

    test ci

--- roles/reverse_proxy_server/manifests/init.pp
+++ roles/reverse_proxy_server/manifests/init.pp
@@ -22,7 +22,7 @@ class reverse_proxy_server (
   class { '::letsencrypt_host':
     dominios => $server_name,
     }->
-  class {'::reverseproxy_server':
+  class { '::reverseproxy_server':
     manage_repo    => false,
     server_name    => $server_name,
     listen_port    => $listen_port,
