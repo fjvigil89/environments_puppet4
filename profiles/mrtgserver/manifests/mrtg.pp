83e378afd7d62bbb0a5caf4e4954c2591a901229
Henry <henry.fleitas@upr.edu.cu>
2019-03-11 14:29:10 -0400

    Copiando archivos a mrtgserver

--- profiles/mrtgserver/manifests/mrtg.pp
+++ profiles/mrtgserver/manifests/mrtg.pp
@@ -16,12 +16,37 @@ class mrtgserver::mrtg(){
     group  => $group,
     mode   => $mode,
   }
-  each($::mrtgserver::ip) |Integer $index, String $value|{
-    exec { $value :
-      creates => "/etc/mrtg/${value}.cfg",
-      command => "/usr/bin/cfgmaker network4core@dminUPR@$value > /etc/mrtg/$value.cfg"
-    }
+  #  each($::mrtgserver::ip) |Integer $index, String $value|{
+  #  exec { $value :
+  #    creates => "/etc/mrtg/${value}.cfg",
+  #    command => "/usr/bin/cfgmaker network4core@dminUPR@$value > /etc/mrtg/$value.cfg"
+  #  }
+  #}
+  
+  file { '/etc/mrtg/10.2.1.1.cfg':
+    ensure => 'file',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/mrtgserver/10.2.1.1.cfg',
   }
+
+  file { '/etc/mrtg/10.2.8.2.cfg':
+    ensure => 'file',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/mrtgserver/10.2.8.2.cfg',
+  }
+
+  file { '/etc/mrtg/192.168.200.1.cfg':
+    ensure => 'file',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/mrtgserver/192.168.200.1.cfg',
+  }
+
   cron {'indexmaker':
     user    => $owner,
     command => '/usr/bin/indexmaker --columns=2 --addhead="<H1 align= "center" > Multi Router Traffic Grapher <H1>" --title="Tr&aacute;fico de Enlaces UPR" /etc/mrtg/192.168.200.1.cfg /etc/mrtg/10.2.1.1.cfg /etc/mrtg/10.2.8.2.cfg > /var/www/mrtg/index.html',
