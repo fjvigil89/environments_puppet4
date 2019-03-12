d3d45f9e7aa4d8f60776968d5efb157c1e13089b
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-03-12 11:38:08 -0400

    virtual host kibana 1

--- profiles/kibanaserver/manifests/nginx.pp
+++ profiles/kibanaserver/manifests/nginx.pp
@@ -4,9 +4,6 @@
 #
 class kibanaserver::nginx {
 
- exec{"htpasswd_kibana":
-   command => '/usr/bin/sudo echo "admin:$(openssl passwd -apr1 $*uprP@ssword*$)" | sudo tee -a /etc/nginx/htpasswd.kibana',
-  }~>
  class {'nginx':
    manage_repo => false,
  }
@@ -16,8 +13,10 @@ class kibanaserver::nginx {
    auth_basic           => 'Restricted Access',
    auth_basic_user_file => '/etc/nginx/htpasswd.kibana',
    #proxy_set_header    => ['Host $host','X-Real-IP $remote_addr'],
-   }
-
+  }~>
+ exec{"htpasswd_kibana":
+   command => '/usr/bin/sudo echo "admin:$(openssl passwd -apr1 $*uprP@ssword*$)" | sudo tee -a /etc/nginx/htpasswd.kibana',
+  }
 }
 
 
