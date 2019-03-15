34b77dd9ce45eeb4b0c4b4ced26d77660acfa3a2
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-03-15 15:00:41 -0400

    filebeat propio2

--- manifests/nodes/elk.pp
+++ manifests/nodes/elk.pp
@@ -2,13 +2,6 @@ node 'elk.upr.edu.cu' {
   
   class {'::elkserver':;}->
   class {'::filebeatserver':;}
-  filebeatserver::prospector{'logs':
-    paths    => [
-    '/var/log/*',
-    ],
-    doc_type => 'syslogs',
-  }
-    
 
   class {'metricbeatserver':;}
 
