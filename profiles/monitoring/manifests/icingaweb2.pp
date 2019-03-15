4077fdc7d5ac71dc6922da87bff7a4cabb76e299
Arian Trujillo Díaz <arian@upr.edu.cu>
2019-03-15 16:10:56 -0400

    test elasticsearch module in to icingaweb

--- profiles/monitoring/manifests/icingaweb2.pp
+++ profiles/monitoring/manifests/icingaweb2.pp
@@ -20,17 +20,17 @@ class {'::icingaweb2':
 }
 
 #Resourse Authentication
-class {'::monitoring::icingaweb_auth':;}
+class { '::monitoring::icingaweb_auth':;}
 #Configure Doc Module
 include ::icingaweb2::module::doc
 #Director Module
-class {'::monitoring::module_director':;}
+class { '::monitoring::module_director':;}
 # Configure Monitoring Module
-class {'::monitoring::module_monitoring':;}
+class { '::monitoring::module_monitoring':;}
 #PuppetDB Icingaweb2 Module
-class {'::monitoring::module_puppetdb':;}
+class { '::monitoring::module_puppetdb':;}
 #Installing apache or httpd
-class {'::monitoring::icingaweb_web':;}
+class { '::monitoring::icingaweb_web':;}
 #Graphite Module
 class { 'icingaweb2::module::graphite':
   git_revision => 'v1.1.0',
@@ -41,8 +41,9 @@ class { 'icingaweb2::module::cube':
   git_revision => 'v1.0.0'
 }
 #File Shipper conf
-class {'::monitoring::module_fileshipper':;}
-
+class { '::monitoring::module_fileshipper':;}
+#ElasticSearch module
+class { '::icingaweb_elasticsearch':;}
 }
 
 
