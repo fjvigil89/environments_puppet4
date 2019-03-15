9b21111d8deebc4dd4d8a07da35027d6c82efbc4
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-03-15 16:11:16 -0400

    filebeat templates 2

--- roles/filebeatserver/manifests/service.pp
+++ roles/filebeatserver/manifests/service.pp
@@ -5,6 +5,14 @@
 # Copyright 2019 Your name here, unless otherwise noted.
 #
 class filebeatserver::service(
+  Optional[String] $type              = $::filebeatserver::type,
+  Optional[String] $paths             = $::filebeatserver::paths,
+  Optional[String] $log_type          = $::filebeatserver::log_type,
+  Optional[String] $kibana_host       = $::filebeatserver::kibana_host,
+  Optional[String] $kibana_username   = $::filebeatserver::kibana_username,
+  Optional[String] $kibana_password   = $::filebeatserver::kibana_password,
+  Optional[String] $logstash_host     = $::filebeatserver::logstash_host,
+
 ) {
   #class { 'filebeat':
   #  manage_repo         => false,
