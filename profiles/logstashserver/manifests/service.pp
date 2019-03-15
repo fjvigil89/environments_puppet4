a2d1563f5cc83f0f766267f287ec4f786fa7c21f
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-03-15 17:48:26 -0400

    filebeat templates ok10

--- profiles/logstashserver/manifests/service.pp
+++ profiles/logstashserver/manifests/service.pp
@@ -6,18 +6,6 @@
 class logstashserver::service {
 
 
-    vcsrepo { '/home/root/logstash/':
-      ensure     => latest,
-      provider   => 'git',
-      remote     => 'origin',
-      source     => {
-        'origin' => 'git@gitlab.upr.edu.cu:dcenter/logstash.git',
-      },
-      revision   => 'master',
-    }~>
-  exec{"instalar_logstash":
-      command => '/usr/bin/sudo dpkg -i /home/root/logstash/logstash-6.6.0.deb',
-    }~>   
     service{'logstash':
       ensure => running,
       enable => true,
