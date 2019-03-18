1e297dd9b5f8fcb36dcc977b0bd7eb755ac54ebe
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-03-18 14:27:40 -0400

    filtros de logstash

--- profiles/logstashserver/manifests/init.pp
+++ profiles/logstashserver/manifests/init.pp
@@ -7,8 +7,7 @@ class logstashserver
 (
   Array[String] $filtros = [
     '02-beats-input.conf',
-    '10-syslog-filter.conf',
-    '11-proxy-filter.conf',
+    '10-syslog-filter.conf',   
     '30-elasticsearch-output.conf',
   ],
 ){
