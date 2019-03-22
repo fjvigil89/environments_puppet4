661c6260958e13de85091d0f7652f61eed58ccba
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-03-22 11:26:30 -0400

    reverse proxy vs letsencript 1

--- profiles/letsencrypt_host/manifests/define.pp
+++ profiles/letsencrypt_host/manifests/define.pp
@@ -11,7 +11,8 @@ define letsencrypt_host2 (
   Optional[String] $plugin                 = $::letsencrypt_host::params::plugin,
   Optional[String] $email                  = undef,
   Optional[String] $config_dir             = $::letsencrypt_host::params::config_dir,
- )inherits ::letsencrypt_host::params{
+ ){
+   include letsencrypt_host::params
    class {'::letsencrypt_host::package':;}
    class {'::letsencrypt_host::install':;}
 
