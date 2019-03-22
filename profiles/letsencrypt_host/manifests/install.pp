abbdd9ca4c8fde5e95bf702d69ae1fbb2f543684
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-03-22 12:37:33 -0400

    reverse proxy vs letsencript 10

--- profiles/letsencrypt_host/manifests/install.pp
+++ profiles/letsencrypt_host/manifests/install.pp
@@ -15,18 +15,18 @@ class letsencrypt_host::install {
     }
     #}
   if($::letsencrypt_host::host::webroot_enable) {
-    each($::letsencrypt_host::host::dominios)|Integer $index, String $value|{
-      letsencrypt::certonly { "${value}":
-        domains       => [$value],
+    #each($::letsencrypt_host::host::dominios)|Integer $index, String $value|{
+      letsencrypt::certonly { "${::letsencrypt_host::host::dominios}":
+        domains       => [$::letsencrypt_host::host::dominios],
         plugin        => $::letsencrypt_host::host::plugin,
         webroot_paths => $::letsencrypt_host::host::webroot_paths,
       }
     }
   }
   else {
-    each($::letsencrypt_host::host::dominios)|Integer $index, String $value|{
-      letsencrypt::certonly { "${value}":
-        domains    => [$value],
+    #each($::letsencrypt_host::host::dominios)|Integer $index, String $value|{
+      letsencrypt::certonly { "${::letsencrypt_host::host::dominios}":
+        domains    => [$::letsencrypt_host::host::dominios],
         plugin     => $::letsencrypt_host::host::plugin,
       }
     }
