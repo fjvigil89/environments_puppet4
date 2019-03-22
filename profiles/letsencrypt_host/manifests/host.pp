fa0b98aa23a7e35b6de7ebae9d41e10a385a4c15
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-03-22 14:28:45 -0400

    reverse proxy

--- profiles/letsencrypt_host/manifests/host.pp
+++ profiles/letsencrypt_host/manifests/host.pp
@@ -5,7 +5,7 @@
 #
 define letsencrypt_host::host (
 # lint:ignore:140chars
-  Optional[Array[String]] $dominios       = $::letsencrypt_host::params::dominios,
+  Optional[String] $dominios                = 'upr.edu.cu',
   Optional[Boolean] $webroot_enable        = false,
   Optional[Array[String]] $webroot_paths   = $::letsencrypt_host::params::webroot_paths,
   Optional[String] $plugin                 = $::letsencrypt_host::params::plugin,
