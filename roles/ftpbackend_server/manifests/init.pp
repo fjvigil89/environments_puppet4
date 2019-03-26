be13a9f6caa81bc9582e87dbeccbb331fb81e807
Arian Trujillo Díaz <arian@upr.edu.cu>
2019-03-26 14:52:59 -0400

    configure mpm apache

--- roles/ftpbackend_server/manifests/init.pp
+++ roles/ftpbackend_server/manifests/init.pp
@@ -74,6 +74,7 @@ class { '::php':
 ensure_packages(['php7.0-dev','php7.0-apcu','php7.0-mbstring','php7.0','php7.0-cli','php7.0-curl','php7.0-intl','php7.0-ldap','php7.0-sybase','php7.0-mcrypt','php7.0-xml','php7.0-mysql','php7.0-common','php-fpm','php7.0-gd','ffmpeg','graphicsmagick'])
 class { 'apache':
   default_vhost => false,
+  mpm_module    => 'prefork',
 }
 apache::vhost { $fqdn:
   servername    => $fqdn,
