f65da7e8da08fd64a07327b27b10da9d5e1924a3
Henry <henry.fleitas@upr.edu.cu>
2019-03-26 09:09:01 -0400

    edit rol whois

--- roles/whois/manifests/init.pp
+++ roles/whois/manifests/init.pp
@@ -6,40 +6,40 @@ class whois {
     group  => 'root',
     owner  => 'root',
     mode   => '0775',
+    }~>
+     vcsrepo { '/var/www/whois':
+       ensure   => latest,
+       provider => 'git',
+       remote   => 'origin',
+       source   => {
+         'origin' => 'git@gitlab.upr.edu.cu:dcenter/whois.git',
+       },
+       revision => 'master',
     }
 
 #Copy SSH Key
 
-file { '/root/.ssh/id_rsa':
-  ensure => file,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0600',
-  source => 'puppet:///modules/whois/keys/id_rsa',
-}
+  file { '/root/.ssh/id_rsa':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0600',
+    source => 'puppet:///modules/whois/keys/id_rsa',
+    }
 
-file { '/root/.ssh/id_rsa.pub':
-  ensure => file,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0644',
-  source => 'puppet:///modules/whois/keys/id_rsa.pub',
-}
+  file { '/root/.ssh/id_rsa.pub':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/whois/keys/id_rsa.pub',
+    }
 
-file { '/root/.ssh/config':
-  ensure => file,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0644',
-  source => 'puppet:///modules/whois/keys/config',
-}~>
-  vcsrepo { '/var/www/whois':
-    ensure   => latest,
-    provider => 'git',
-    remote   => 'origin',
-    source   => {
-      'origin' => 'git@gitlab.upr.edu.cu:dcenter/whois.git',
-      },
-    revision => 'master',
+  file { '/root/.ssh/config':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/whois/keys/config',
     }
 }
