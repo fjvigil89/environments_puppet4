69b28cae1d7859e588d79297367760a81eec15b0
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-04-08 16:59:31 -0400

    edit whois

--- roles/whois/manifests/init.pp
+++ roles/whois/manifests/init.pp
@@ -1,12 +1,12 @@
 # Class: whois
 #
 class whois {
-#  file { '/var/www/whois':
-#    ensure => directory,
-#    group  => 'root',
-#    owner  => 'root',
-#    mode   => '0775',
-#    }~>
+  file { '/var/www/whois':
+    ensure => directory,
+    group  => 'root',
+    owner  => 'root',
+    mode   => '0775',
+    }~>
      vcsrepo { '/var/www/whois':
        ensure   => latest,
        provider => 'git',
@@ -19,27 +19,27 @@ class whois {
 
 #Copy SSH Key
 
-  file { '/root/.ssh/id_rsa':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0600',
-    source => 'puppet:///modules/whois/keys/id_rsa',
+#  file { '/root/.ssh/id_rsa':
+#    ensure => file,
+#    owner  => 'root',
+#    group  => 'root',
+#    mode   => '0600',
+#    source => 'puppet:///modules/whois/keys/id_rsa',
+#    }
+#
+#  file { '/root/.ssh/id_rsa.pub':
+#    ensure => file,
+#    owner  => 'root',
+#    group  => 'root',
+#    mode   => '0644',
+#    source => 'puppet:///modules/whois/keys/id_rsa.pub',
+#    }
+#
+#  file { '/root/.ssh/config':
+#    ensure => file,
+#    owner  => 'root',
+#    group  => 'root',
+#    mode   => '0644',
+#    source => 'puppet:///modules/whois/keys/config',
     }
-
-  file { '/root/.ssh/id_rsa.pub':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/whois/keys/id_rsa.pub',
-    }
-
-  file { '/root/.ssh/config':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/whois/keys/config',
-    }
-}
+#}
