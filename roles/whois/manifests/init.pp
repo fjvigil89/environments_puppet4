7e06b530d5389b865fede2f685678d8156d8508d
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-04-09 08:59:04 -0400

    whois ensure present

--- roles/whois/manifests/init.pp
+++ roles/whois/manifests/init.pp
@@ -8,7 +8,7 @@ class whois {
     mode   => '0775',
     }~>
      vcsrepo { '/var/www/whois':
-       ensure   => latest,
+       ensure   => present,
        provider => 'git',
        remote   => 'origin',
        source   => {
