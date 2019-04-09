5dc8d77223f3fd91bcfd3336c1376fdad5c0c50c
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-04-09 10:25:55 -0400

    edit whois node

--- roles/whois/manifests/init.pp
+++ roles/whois/manifests/init.pp
@@ -1,21 +1,21 @@
 # Class: whois
 #
 class whois {
-  file { '/var/www/whois':
-    ensure => directory,
-    group  => 'root',
-    owner  => 'root',
-    mode   => '0775',
-    }~>
-     vcsrepo { '/var/www/whois/':
-       ensure   => latest,
-       provider => 'git',
-       remote   => 'origin',
-       source   => {
-         'origin' => 'git@gitlab.upr.edu.cu:dcenter/whois.git',
-       },
-       revision => 'master',
-    }
+ # file { '/var/www/whois':
+ #   ensure => directory,
+ #   group  => 'root',
+ #   owner  => 'root',
+ #   mode   => '0775',
+ #   }~>
+ #    vcsrepo { '/var/www/whois/':
+ #      ensure   => latest,
+ #      provider => 'git',
+ #      remote   => 'origin',
+ #      source   => {
+ #        'origin' => 'git@gitlab.upr.edu.cu:dcenter/whois.git',
+ #      },
+ #      revision => 'master',
+ #   }
 
 #Copy SSH Key
 
