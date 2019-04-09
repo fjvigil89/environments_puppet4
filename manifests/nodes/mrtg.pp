b16137ed9c1c1db6aff6aeb27d4a3a6d2ff233ff
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-04-09 10:31:04 -0400

    edit whois rol

--- manifests/nodes/mrtg.pp
+++ manifests/nodes/mrtg.pp
@@ -12,19 +12,4 @@ node 'mrtg.upr.edu.cu' {
       application     => 'mrtg',
       proxmox_enabled => false,
     }
-  file { '/var/www/whois':
-    ensure => directory,
-    group  => 'root',
-    owner  => 'root',
-    mode   => '0775',
-    }~>
-    vcsrepo { '/var/www/whois/':
-     ensure   => latest,
-     provider => 'git',
-     remote   => 'origin',
-     source   => {
-      'origin' => 'git@gitlab.upr.edu.cu:dcenter/whois.git',
-        },
-     revision => 'master',
-     }
 }
