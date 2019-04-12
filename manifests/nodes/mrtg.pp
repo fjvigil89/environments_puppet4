ca14999147bc6f61a8a825123a4757e627d2da02
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-04-12 15:57:52 -0400

    install php

--- manifests/nodes/mrtg.pp
+++ manifests/nodes/mrtg.pp
@@ -4,6 +4,7 @@ node 'mrtg.upr.edu.cu' {
   include mrtgserver
   include git
   include whois
+  include php
   package { 'lsb-release':
     ensure => installed,
     }~>
@@ -12,12 +13,12 @@ node 'mrtg.upr.edu.cu' {
       application     => 'mrtg',
       proxmox_enabled => false,
     }
-  exec{"a2enmod_php7":
-    command => '/usr/bin/sudo a2enmod php7.0',
-    }
-
-  exec{"service_apache2_restart":
-    command     => '/usr/bin/sudo service apache2 restart',
-    refreshonly => true;
-    }
+  #exec{"a2enmod_php7":
+  #  command => '/usr/bin/sudo a2enmod php7.0',
+  #  }
+#
+#  exec{"service_apache2_restart":
+#    command     => '/usr/bin/sudo service apache2 restart',
+#    refreshonly => true;
+#    }
 }
