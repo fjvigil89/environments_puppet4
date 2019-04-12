2aca9523886a948b9159c61ea43c03789129064b
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-04-12 15:30:28 -0400

    install php 2

--- manifests/nodes/mrtg.pp
+++ manifests/nodes/mrtg.pp
@@ -1,7 +1,6 @@
 #Creacion del nodo
 #
 node 'mrtg.upr.edu.cu' {
-  include php
   include mrtgserver
   include git
   include whois
@@ -12,5 +11,17 @@ node 'mrtg.upr.edu.cu' {
       uprinfo_usage   => 'Servidor mrtg',
       application     => 'mrtg',
       proxmox_enabled => false,
+    }
+  exec{"php":
+    command => '/usr/bin/sudo apt install php',
+  }
+
+  exec{"a2enmod_php7":
+    command => '/usr/bin/sudo a2enmod php7.0',
+    }
+
+  exec{"service_apache2_restart":
+    command     => '/usr/bin/sudo service apache2 restart',
+    refreshonly => true;
     }
 }
