20e4b0cb13f2c4d90e5d93911fb607387b65c8d2
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-05-03 13:48:53 -0400

    add node telefonos

--- manifests/nodes/telefonos-pup.pp
+++ manifests/nodes/telefonos-pup.pp
@@ -1,12 +1,3 @@
 node 'telefonos-pup.upr.edu.cu' {  
-  package { 'lsb-release':
-          ensure => installed,
-  }~>
-  class { '::basesys':
-    uprinfo_usage  => 'servidor guía telenfónica',
-    application    => 'Guía Telefónica UPR',
-    puppet_enabled => false,
-    repos_enabled  => true,
-    mta_enabled    => false,
-  }
+include telefonos
 }
