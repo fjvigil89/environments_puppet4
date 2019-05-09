52046c0f1ac69676651bd61141a539fa712b5f14
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-05-09 16:18:12 -0400

    copiar llaves a telefonos-pup4

--- manifests/nodes/telefonos-pup.pp
+++ manifests/nodes/telefonos-pup.pp
@@ -1,26 +1,10 @@
 node 'telefonos-pup.upr.edu.cu'{
 
 include telefonos
-
-file { '/home/telefonos':
-  ensure => directory,
-  group  => 'root',
-  owner  => 'root',
-  mode   => '0775',
-  }~>
-  vcsrepo { '/home/telefonos':
-      ensure     => latest,
-      provider   => 'git',
-      remote     => 'origin',
-      source     => {
-        'origin' => 'git@gitlab.upr.edu.cu:dcenter/telefonos.git',
-      },
-      revision   => 'master',
-    }
-apache::vhost { 'telefonos':
-	port       => '80',
-	docroot    => '/home/telefonos/',
-	servername => 'telefonos-pup.upr.edu.cu',
-	aliases    => 'telefonos',
-	}
-}
+class { '::basesys':
+  uprinfo_usage => 'servidor telefonos',
+  application   => 'guia de telefonos UPR',
+  mta_enabled   => false,
+  repos_enabled => false,
+  }
+  }
--- roles/telefonos/manifests/init.pp
+++ roles/telefonos/manifests/init.pp
@@ -33,6 +33,29 @@
     packages       => $packages,
     }
 
+file { '/home/telefonos':
+  ensure => directory,
+  group  => 'root',
+  owner  => 'root',
+  mode   => '0775',
+  }~>
+  vcsrepo { '/home/telefonos':
+    ensure     => latest,
+	provider   => 'git',
+	remote     => 'origin',
+	source     => {
+	    'origin' => 'git@gitlab.upr.edu.cu:dcenter/telefonos.git',
+		},
+	revision   => 'master',
+	}
+	
+ apache::vhost { 'telefonos':
+   port       => '80',
+   docroot    => '/home/telefonos/',
+   servername => 'telefonos-pup.upr.edu.cu',
+   aliases    => 'telefonos',
+   }
+
  #Copy SSH Key
  #
  file { '/root/.ssh/id_rsa':
