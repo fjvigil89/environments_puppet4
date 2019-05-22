62f3cf6e1eb5e629a05b7397adac711f46efef83
Arian Trujillo Díaz <arian@upr.edu.cu>
2019-05-22 16:17:23 -0400

    add ssh ssl to ns1 p

--- manifests/nodes/nsp.pp
+++ manifests/nodes/nsp.pp
@@ -3,6 +3,21 @@ node 'ns1p.upr.edu.cu' {
     uprinfo_usage => 'servidor test',
     application   => 'puppet',
   }
+  #Copy SSH SSL
+  file { '/root/.ssh/id_rsa':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0774',
+    source => 'puppet:///modules/dns_primary/ssh_keys/id_rsa',
+  }
+  file { '/root/.ssh/id_rsa.pub':
+  ¦ ensure => file,
+  ¦ owner  => 'root',
+  ¦ group  => 'root',
+  ¦ mode   => '0774',
+  ¦ source => 'puppet:///modules/dns_primary/ssh_keys/id_rsa.pub',
+  }
   $zone    = 'type master'
   $allow   = "{ 10.2.0.0/15; 10.71.46.0/24; 20.0.0.0/24; 172.30.146.0/27; 192.168.25.0/24; 200.14.49.0/27; 200.55.143.8/29;}"
   $notify_internal  = "{ 10.2.4.14; 10.2.4.15; }"
