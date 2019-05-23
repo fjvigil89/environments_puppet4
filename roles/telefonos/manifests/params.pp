d7401d4535c9986f9beaa384d4d4e85accca790c
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-05-23 09:43:34 -0400

    edit modules php fix error2

--- roles/telefonos/manifests/params.pp
+++ roles/telefonos/manifests/params.pp
@@ -4,5 +4,5 @@
 
   class telefonos::params {
     $version    = '7.0'
-    $pachages  = ['php7.0-mbstring','r10k','php7.0','php7.0-cli','php7.0-curl','php7.0-intl','php7.0-ldap','php7.0-mysql','php7.0-sybase','libapach    e2-mod-php7.0','php7.0-mcrypt','phpmyadmin','freetds-bin','freetds-common']
+    $pachages  = ['php7.0-mbstring','r10k','php7.0','php7.0-cli','php7.0-curl','php7.0-intl','php7.0-ldap','php7.0-mysql','php7.0-sybase','libapache2-mod-php7.0','php7.0-mcrypt','phpmyadmin','freetds-bin','freetds-common']
     }
