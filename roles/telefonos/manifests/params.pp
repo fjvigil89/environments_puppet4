adb0036b514cc2e03430b8eccfa9c351695a8036
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-05-23 09:30:49 -0400

    edit modules php fix error

--- roles/telefonos/manifests/params.pp
+++ roles/telefonos/manifests/params.pp
@@ -3,6 +3,6 @@
 # It set variable according to platform
 
   class telefonos::params {
-    $version    = '7.0',
-    $pachages  = ['php7.0-mbstring','r10k','php7.0','php7.0-cli','php7.0-curl','php7.0-intl','php7.0-ldap','php7.0-mysql','php7.0-sybase','libapach    e2-mod-php7.0','php7.0-mcrypt','phpmyadmin','freetds-bin','freetds-common'],
+    $version    = '7.0'
+    $pachages  = ['php7.0-mbstring','r10k','php7.0','php7.0-cli','php7.0-curl','php7.0-intl','php7.0-ldap','php7.0-mysql','php7.0-sybase','libapach    e2-mod-php7.0','php7.0-mcrypt','phpmyadmin','freetds-bin','freetds-common']
     }
