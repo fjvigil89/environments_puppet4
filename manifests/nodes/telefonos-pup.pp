aeaafff461091aa6a615101d23b5196713574e30
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-05-23 11:44:24 -0400

    edit node telefonos-pup3

--- manifests/nodes/telefonos-pup.pp
+++ manifests/nodes/telefonos-pup.pp
@@ -26,5 +26,15 @@ node 'telefonos-pup.upr.edu.cu'{
          'allow'          => 'from All',
          'directoryindex' => 'app.php',
        },],
-     }
+     }~>
+	 exec{"a2enmod_php7":
+      command => '/usr/bin/sudo a2enmod php7.0',
+	  }~>
+	 exec{"a2enmod_rewrite":
+      command => '/usr/bin/sudo a2enmod rewrite',
+	  }~>
+	 exec{"service_apache2_restart":
+      command     => '/usr/bin/sudo service apache2 restart',
+      refreshonly => true;
+	  }
 	} 
