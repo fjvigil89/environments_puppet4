6c0ae0ec6ba8e9355281cc990a3bf1a5b3bc32bc
Arian Trujillo Díaz <arian@upr.edu.cu>
2019-05-23 13:34:39 -0400

    test purge repository

--- manifests/nodes/ntp.pp
+++ manifests/nodes/ntp.pp
@@ -7,4 +7,13 @@ node /^ntp\d+$/{
     mta_enabled    => false,
     time_enabled   => false,
   }
+  file { '/etc/icinga2/conf.d':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    purge   => true,
+    recurse => true,
+  }
+
 }
