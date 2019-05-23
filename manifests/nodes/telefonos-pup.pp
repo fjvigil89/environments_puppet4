b563ac8a38780b45935320e1ad311815b0c7d449
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-05-23 11:06:18 -0400

    edit node telefonos-pup

--- manifests/nodes/telefonos-pup.pp
+++ manifests/nodes/telefonos-pup.pp
@@ -1,11 +1,30 @@
 node 'telefonos-pup.upr.edu.cu'{
-
-include telefonos
-
-class { '::basesys':
-  uprinfo_usage => 'servidor telefonos',
-  application   => 'guia de telefonos UPR',
-  mta_enabled   => false,
-  repos_enabled => false,
-  }
-  }
+ class{'::php_server':
+   version      => '7.0',
+   packages     => ['php7.0-mbstring','r10k','php7.0','php7.0-cli','php7.0-curl','php7.0-intl','php7.0-ldap','php7.0-mysql','php7.0-sybase','libapache2    -mod-php7.0','php7.0-mcrypt','phpmyadmin','freetds-bin','freetds-common', 'php7.0-gd','php7.0-gmp','php7.0-snmp'],
+   manage_repos => true,
+   }~>
+   vcsrepo { '/var/www/telefonos':
+     ensure     => latest,
+	 provider   => 'git',
+	 remote     => 'origin',
+	 source     => {
+	    'origin' => 'git@gitlab.upr.edu.cu:dcenter/telefonos.git',
+	 },
+	 revision   => 'master',
+	 }
+	 apache::vhost { 'telefonos':
+	   serveraliases => 'www.telefonos-pup.upr.edu.cu',
+	   port          => '80',
+       docroot       => '/var/www/telefonos/web/',
+       servername    => 'telefonos-pup.upr.edu.cu',
+       aliases       => 'telefonos',
+       directories   => [{
+         'path' => '/var/www/telefonos/web/',
+         'options'   => ['Indexes','FollowSymLinks','MultiViews'],
+         'allow_override' => 'All',
+         'allow'          => 'from All',
+         'directoryindex' => 'app.php',
+       },],
+     }
+	} 
