023fba97f8d2f411be5c50c30977d5310767eee3
Arian Trujillo Díaz <arian@upr.edu.cu>
2019-05-28 17:38:45 -0400

    add Vcs to network devices in to icingaweb fileshipper module

--- profiles/monitoring/manifests/module_fileshipper.pp
+++ profiles/monitoring/manifests/module_fileshipper.pp
@@ -5,11 +5,27 @@
 # Configure monitoring::module_fileshipper
 
 class monitoring::module_fileshipper {
+
+  file { '/var/cache/icinga2/icinga2-networkconf':
+  ensure  => directory,
+  group   => 'bind',
+  owner   => 'bind',
+  mode    => '0775',
+  }~>
+  vcsrepo { '/var/cache/icinga2/icinga2-networkconf':
+    ensure   => latest,
+    provider => 'git',
+    remote   => 'origin',
+    source   => {
+      'origin' => 'git@gitlab.upr.edu.cu:dcenter/icinga2-networkconf.git',
+    },
+    revision => 'master',
+  }
 #Fileshippet Module
 class { 'icingaweb2::module::fileshipper':
   git_revision     => 'master',
   base_directories => {
-    basedir => '/var/cache/icinga2',
+    basedir => '/var/cache/icinga2/netdevices_conf',
   },
   directories      => {
     'test' => {
