b0ed3f9ec6438b47918e7e998b4f93bde1396ac4
Arian Trujillo Díaz <arian@upr.edu.cu>
2019-05-28 17:43:43 -0400

    tray again git fileshipper module

--- profiles/monitoring/manifests/module_fileshipper.pp
+++ profiles/monitoring/manifests/module_fileshipper.pp
@@ -5,13 +5,6 @@
 # Configure monitoring::module_fileshipper
 
 class monitoring::module_fileshipper {
-
-  file { '/var/cache/icinga2/icinga2-networkconf':
-  ensure  => directory,
-  group   => 'bind',
-  owner   => 'bind',
-  mode    => '0775',
-  }~>
   vcsrepo { '/var/cache/icinga2/icinga2-networkconf':
     ensure   => latest,
     provider => 'git',
@@ -25,7 +18,7 @@ class monitoring::module_fileshipper {
 class { 'icingaweb2::module::fileshipper':
   git_revision     => 'master',
   base_directories => {
-    basedir => '/var/cache/icinga2/netdevices_conf',
+    basedir => '/var/cache/icinga2/icinga2-networkconf',
   },
   directories      => {
     'test' => {
