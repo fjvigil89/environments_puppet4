53c59341ad73a32f8b1c78f5529e978ad4dc85bc
Arian Trujillo Díaz <arian@upr.edu.cu>
2019-05-29 11:39:58 -0400

    fix error

--- manifests/nodes/predes.pp
+++ manifests/nodes/predes.pp
@@ -3,11 +3,11 @@ node 'profesor.upr.edu.cu' {
           ensure => installed,
   }~>
   class { '::basesys':
-  ¦ uprinfo_usage      => 'icinga_server',
-  ¦ application        => 'icinga2',
-  ¦ puppet_enabled     => true,
-  ¦ mta_enabled        => false,
-  ¦ monitoring_enabled => false;
+    uprinfo_usage      => 'icinga_server',
+    application        => 'icinga2',
+    puppet_enabled     => true,
+    mta_enabled        => false,
+    monitoring_enabled => false;
   }
   class { '::monitoring':;}
 }
