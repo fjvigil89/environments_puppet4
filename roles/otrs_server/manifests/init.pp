5052e462ff7d00583af1175185842b6ee9b97679
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-07-04 17:19:10 -0400

    agregando otrs 8

--- roles/otrs_server/manifests/init.pp
+++ roles/otrs_server/manifests/init.pp
@@ -12,5 +12,13 @@ class otrs_server {
     uprinfo_usage => 'OTRS',
   }
 
-  include ::otrs_instance
+  class {'otrs':
+    installation_type  => 'web',
+    database_connector => 'mysql',
+    db_host            => $::fqdn,
+    db_name            => 'otrsprod',
+    db_user            => 'otrs',
+    db_password        => 'plzCr3at3Atick3t',
+  }
+  #include ::otrs_instance
 }
