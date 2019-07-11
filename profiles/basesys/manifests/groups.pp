34cc60882b27b00df7256d082eee104852750fc5
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-07-11 16:38:05 -0400

    agregando grupos

--- profiles/basesys/manifests/groups.pp
+++ profiles/basesys/manifests/groups.pp
@@ -4,4 +4,10 @@ class basesys::groups (
   # Systeem user uit hiera, zie basesys/data/groups.yml
   $system_groups = lookup('basesys::system_groups', {merge => hash, default_value => {}})
   create_resources('@group', $system_groups)
+
+  ## Todos en los sistemas ver accounts.yaml
+   Group::System_groups<|ensure=='present'|> {
+     #purge_sshkeys => true,
+   }
+
 }
