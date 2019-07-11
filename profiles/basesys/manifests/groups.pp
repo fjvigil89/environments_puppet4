c26e1da337ef2fccf7ae11e28658092399fee6d8
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-07-11 17:13:29 -0400

    agregando grupos 6

--- profiles/basesys/manifests/groups.pp
+++ profiles/basesys/manifests/groups.pp
@@ -3,7 +3,10 @@ class basesys::groups (
 ) {
   # Systeem user uit hiera, zie basesys/data/groups.yml
   $groups = lookup('basesys::system_groups', {merge => hash, default_value => {}})
-  create_resources('@groups', $groups)
+  create_resources('@group', $groups)
+ # $system_groups = lookup('basesys::system_groups', {merge => hash, default_value => {}})
+ # create_resources('@group', $system_groups)
+
 
 
 }
