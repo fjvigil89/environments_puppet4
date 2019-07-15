0cabed53cdf7c75efe4bc06ec6c800e9bc006912
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-07-15 12:51:41 -0400

    creando elk-cluster 3

--- roles/elknodeserver/manifests/init.pp
+++ roles/elknodeserver/manifests/init.pp
@@ -8,6 +8,22 @@
 class elknodeserver {
 
   class {'::elasticsearchserver::common':;}~>
+ 
+  #Copy SSH Key
+  file { '/root/.ssh/id_rsa':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0600',
+    source => 'puppet:///modules/elknodeserver/ssh/id_rsa',
+  }~>
+  file { '/root/.ssh/id_rsa.pub':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/elknodeserver/ssh/id_rsa.pub',
+  }~>
   file{'/etc/elasticsearch/elasticsearch.yml':
       ensure => 'file',
       owner  => 'root',
