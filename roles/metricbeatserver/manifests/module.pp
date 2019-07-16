cb98ff9040b22f2e9901e87ee0134604670aed94
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-07-16 16:59:18 -0400

    metricbeat 5

--- roles/metricbeatserver/manifests/module.pp
+++ roles/metricbeatserver/manifests/module.pp
@@ -9,7 +9,7 @@ define metricbeatserver::module (
   #$param_module = $metricbeatserver::params::modules
 
   exec{"module_System":
-      command => "/usr/bin/sudo metricbeat modules enable {$module}",
+      command => "/usr/bin/sudo metricbeat modules enable $module",
    }
 
 }
