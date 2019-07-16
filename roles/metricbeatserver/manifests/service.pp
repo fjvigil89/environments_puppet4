8e47776f793555365c4380d7b88b50ca87ec89c3
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-07-16 16:54:14 -0400

    metricbeat 2

--- roles/metricbeatserver/manifests/service.pp
+++ roles/metricbeatserver/manifests/service.pp
@@ -9,22 +9,10 @@ class metricbeatserver::service(
  Integer $queue_size    = $metricbeatserver::queue_size,
 ) {
 
-  #class {'metricbeat':
-  #  major_version      => '6',
-  #  disable_configtest => true,
-  #  queue_size         => $queue_size, 
-  #  manage_repo        => $manage_repo,
-  #  modules            => $modules,
-  #  outputs            => $outputs, 
-  #}
-
   file {'/etc/metricbeat/metricbeat.yml':
     notify =>  Service['filebeat'],
     content => template('metricbeatserver/metricbeat.erb'),
-  }~>
-  #exec{"module_System":
-  #    command => '/usr/bin/sudo metricbeat modules enable system',
-  # }~>
+  }
   each($::metricbeatserver::modules) |Integer $index, String $value|{
     metricbeatserver::module{$::metricbeatserver::modules[$index]:
        module => $value,
