879b51d4bb3da30c3e887b0825031104c320a0de
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-07-22 11:26:18 -0400

    add puppet-new

---
+++ manifests/nodes/puppet-new.pp
@@ -0,0 +1,12 @@
+node 'puppet-new.upr.edu.cu' {  
+  package { 'lsb-release':
+          ensure => installed,
+  }~>
+  class { '::basesys':
+    uprinfo_usage   => 'servidor puppet-new',
+    application     => 'Prueba',
+    proxmox_enabled => false,
+    repos_enabled   => true,
+    mta_enabled     => false,
+  }
+}
