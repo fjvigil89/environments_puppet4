945ef2c3bb4d7e6e8d4bd75ea72a52dad941df7a
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-08-26 12:17:54 -0400

    curator 1

--- profiles/curatorserver/manifests/init.pp
+++ profiles/curatorserver/manifests/init.pp
@@ -5,38 +5,39 @@
 class curatorserver {
 
   curator::action { 'purge_logstash_over_45_days':
-  entities => {
-    1 => {
-      'action' => 'delete_indices',
-      'description' => 'Delete indices older than 45 days (based on index name)',
-      'options' => {
-        'continue_if_exception' => 'True',
-        'disable_action'        => 'False',
-        'ignore_empty_list'     => 'True',
+    config_path =>  '/etc/curator',
+    entities => {
+      1 => {
+        'action' => 'delete_indices',
+       'description' => 'Delete indices older than 45 days (based on index name)',
+       'options' => {
+         'continue_if_exception' => 'True',
+         'disable_action'        => 'False',
+         'ignore_empty_list'     => 'True',
+       },
+       'filters' => [
+         {
+           'filtertype' => 'pattern',
+           'kind'       => 'prefix',
+           'value'      => 'proxy-',
+         },
+         {
+           'filtertype' => 'age',
+           'source'     => 'name',
+           'direction'  => 'older',
+           'timestring' => '%Y.%m.%d',
+           'unit'       => 'days',
+           'unit_count' => '45',
+         },
+       ],
       },
-      'filters' => [
-        {
-          'filtertype' => 'pattern',
-          'kind'       => 'prefix',
-          'value'      => 'proxy-',
-        },
-        {
-          'filtertype' => 'age',
-          'source'     => 'name',
-          'direction'  => 'older',
-          'timestring' => '%Y.%m.%d',
-          'unit'       => 'days',
-          'unit_count' => '45',
-        },
-      ],
     },
-  },
-}
+  }
 
-curator::job { 'purge_logstash_over_45_days_everyday':
-  action => 'purge_logstash_over_45_days',
-  minute => 0,
-  hour   => 2,
-}
+  curator::job { 'purge_logstash_over_45_days_everyday':
+   action => 'purge_logstash_over_45_days',
+   minute => 0,
+   hour   => 2,
+  }
 
 }
