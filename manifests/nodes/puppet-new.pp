7cc55eba7b3976ab561a287ed20708518245611a
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-08-27 08:41:28 -0400

    edit nodo puppet-new

--- manifests/nodes/puppet-new.pp
+++ manifests/nodes/puppet-new.pp
@@ -1,5 +1,31 @@
-node 'puppet-new.upr.edu.cu' {  
-  include puppetdevserver
+node 'puppet-new.upr.edu.cu'{
+  class { 'prosody':
+    user              => 'prosody',
+    group             => 'prosody',
+    community_modules => 'mod_auth_ldap',
+    authentication    => 'ldap',
+    custom_options    => {
+                            'ldap_base'     => 'DC="upr",DC="edu",DC="cu"',
+                            'ldap_server'   => '10.2.4.35:636',
+                            'ldap_rootdn'   => 'CN="talk",OU="_Servicios",DC="upr",DC="edu",DC="cu"',
+                            'ldap_password' => '40a*talk.2k12',
+                            'ldap_scope'    => 'subtree',
+                            'ldap_tls'      => 'true',
+                          },
+  }
+
+  /* prosody::virtualhost {
+    'mydomain.com' :
+      ensure   => present,
+      ssl_key  => '/etc/ssl/key/mydomain.com.key',
+      ssl_cert => '/etc/ssl/crt/mydomain.com.crt',
+  }
+
+  prosody::user { 'foo':
+    host => 'mydomain.com',
+    pass => 'itsasecret',
+  } */
+  /* include puppetdevserver
   package { 'lsb-release':
           ensure => installed,
   }~>
@@ -9,5 +35,5 @@ node 'puppet-new.upr.edu.cu' {
     proxmox_enabled => false,
     repos_enabled   => true,
     mta_enabled     => false,
-  }
+  } */
 }
