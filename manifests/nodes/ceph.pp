17fc378aa9d68a87fedd58d1df78d398b006b5d7
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-09-04 17:02:37 -0400

    prueba de ceph 5

--- manifests/nodes/ceph.pp
+++ manifests/nodes/ceph.pp
@@ -13,7 +13,7 @@ node 'text-ceph' {
  
   class {'ceph':
     mon_hosts   => [ 'text-ceph' ],
-    release     => 'hammer',
+    release     => 'luminous',
     cluster_net => '1.2.1.0/24',
     public_net  => '1.2.4.0/23',
   }
