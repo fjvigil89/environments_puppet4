0895d5afc6e0dc79e6c182b4003b688e3a829cb7
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-09-05 12:41:55 -0400

    edit puppet-henry

--- manifests/nodes/puppet-henry.pp
+++ manifests/nodes/puppet-henry.pp
@@ -1,11 +1,25 @@
-node 'puppet-henry.upr.edu.cu' {  
+node 'puppet-henry.upr.edu.cu' {
   package { 'lsb-release':
           ensure => installed,
   }~>
   class { '::basesys':
-    uprinfo_usage   => 'Servidor HAproxy FTP',
-    application     => 'HAproxy FTP',
-    repos_enabled   => true,
-    mta_enabled     => false,
+    uprinfo_usage => 'Server Prosody',
+    application   => 'Prosody',
+    repos_enabled => true,
+    mta_enabled   => false,
+  }
+  class { 'prosody':
+    user              => 'prosody',
+    group             => 'prosody',
+    community_modules => 'mod_auth_ldap',
+    authentication    => 'ldap',
+    custom_options    => {
+                            'ldap_base'     => 'DC="upr",DC="edu",DC="cu"',
+                            'ldap_server'   => '10.2.4.35:636',
+                            'ldap_rootdn'   => 'CN="talk",OU="_Servicios",DC="upr",DC="edu",DC="cu"',
+                            'ldap_password' => '40a*talk.2k12',
+                            'ldap_scope'    => 'subtree',
+                            'ldap_tls'      => true,
+                          },
   }
 }
