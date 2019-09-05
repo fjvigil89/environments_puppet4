d0d8e2e1b753bf943b162c906777d21823c8bb01
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-09-05 14:28:08 -0400

    fix error basesys

--- manifests/nodes/puppet-henry.pp
+++ manifests/nodes/puppet-henry.pp
@@ -2,11 +2,5 @@ node 'puppet-henry.upr.edu.cu' {
   package { 'lsb-release':
           ensure => installed,
   }~>
-  class { '::basesys':
-    uprinfo_usage => 'Servidor Puppet Master',
-    application   => 'Puppetlab',
-    repos_enabled => true,
-    mta_enabled   => false,
-  }
   include puppetdevserver
 }
