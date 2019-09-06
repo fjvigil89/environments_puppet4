9b0f697441ef77894fe744d9b78498f94e43e0bb
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-09-06 17:08:34 -0400

    modulo de r10k para ansible 2

--- profiles/r10kserver/manifests/init.pp
+++ profiles/r10kserver/manifests/init.pp
@@ -2,7 +2,12 @@
 # ===========================
 # Copyright 2019 Your name here, unless otherwise noted.
 #
-class r10kserver {
+class r10kserver(
+  $r10k_basedir              => "/etc/ansible/environments"
+  $cachedir                  => "/opt/puppetlabs/r10k/cache"
+  $configfile                => "/etc/ansible/r10k/r10k.yaml"
+  $manage_configfile_symlink => true
+) {
 
   class {"r10k":
     r10k_basedir              => "/etc/ansible/environments", 
