ac71d00fefe139363db8ebfeaad770a8a5c3b224
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-09-06 10:32:48 -0400

    agregando server ansible 2

---
+++ manifests/nodes/ansible.pp
@@ -0,0 +1,35 @@
+node 'ansible.upr.edu.cu' {  
+  package { 'lsb-release':
+          ensure => installed,
+  }
+  class { '::basesys':
+    uprinfo_usage   => 'servidor ansible',
+    application     => 'Ceph',
+    proxmox_enabled => false,
+    repos_enabled   => true,
+    mta_enabled     => false,
+  }
+
+ 
+# Here is a controller node.  You could have more than one, if you like.
+  include ansible::controller
+  include ansible::target      # Just like Puppet masters also have agents.
+  include mysql::server
+# Here are three of my web servers.
+  include apache
+  ansible::add_to_group { 'webservers': }
+
+# My production databases have a predictable naming scheme.
+  ansible::add_to_group { 'production': }
+  ansible::add_to_group { 'databases': }
+
+# Development databases have similarly predictable names.
+  include mysql::client
+  ansible::add_to_group { 'development': }
+  ansible::add_to_group { 'databases': }
+ 
+
+
+
+
+}
--- manifests/nodes/ceph.pp
+++ manifests/nodes/ceph.pp
@@ -3,33 +3,13 @@ node 'text-ceph' {
           ensure => installed,
   }
   class { '::basesys':
-    uprinfo_usage   => 'servidor gestion',
-    application     => 'Proxmox Gestion',
+    uprinfo_usage   => 'servidor ceph',
+    application     => 'Ceph',
     proxmox_enabled => false,
     repos_enabled   => true,
     mta_enabled     => false,
   }
 
  
-# Here is a controller node.  You could have more than one, if you like.
-  include ansible::controller
-  include ansible::target      # Just like Puppet masters also have agents.
-  include mysql::server
-# Here are three of my web servers.
-  include apache
-  ansible::add_to_group { 'webservers': }
-
-# My production databases have a predictable naming scheme.
-  ansible::add_to_group { 'production': }
-  ansible::add_to_group { 'databases': }
-
-# Development databases have similarly predictable names.
-  include mysql::client
-  ansible::add_to_group { 'development': }
-  ansible::add_to_group { 'databases': }
- 
-
-
-
 
 }
