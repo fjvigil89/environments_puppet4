a94070c06880590cc69eb31ace60d63ef9a1e7e9
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-09-11 15:46:40 -0400

    edit server_version

--- profiles/puppetserver/manifests/init.pp
+++ profiles/puppetserver/manifests/init.pp
@@ -38,6 +38,7 @@ class puppetserver (String $puppetdb_server = 'localhost') {
     #version                     => "5.5.1-1${::lsbdistcodename}",
     #server_puppetserver_version => '5.1.0',
     #server_version              => '5.3.5-1puppetlabs1',
+    server_version              => 'latest',
     #server_common_modules_path  => '',
     #hiera_config                => '$codedir/hiera.yaml',
     # Staat op agent omdat we de server manueel upgraden!
