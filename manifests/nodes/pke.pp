75f264fc1d3338cabb86544341dc5ecdf6aa5420
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-10-01 15:05:11 -0400

    creando nodo para pke

---
+++ manifests/nodes/pke.pp
@@ -0,0 +1,12 @@
+node 'pke.upr.edu.cu' {  
+  
+ class { '::basesys':
+    uprinfo_usage   => 'servidor PKE',
+    application     => 'Instalation PKE',
+    repos_enabled   => true,
+    mta_enabled     => false,
+  }
+
+
+}
+
