1ec530fa73b3a7066e69b18dc0903d51851f9c96
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-10-04 13:39:32 -0400

    delete directory test

--- profiles/mrtgserver/manifests/mrtg.pp
+++ profiles/mrtgserver/manifests/mrtg.pp
@@ -33,7 +33,7 @@ class mrtgserver::mrtg(){
 
   exec { '10.2.0.2':
   creates => "/etc/mrtg/10.2.0.2.cfg",
-  command => "/usr/bin/cfgmaker network4core@dminUPR@10.2.0.2 > /etc/mrtg/test.cfg"
+  command => "/usr/bin/cfgmaker network4core@dminUPR@10.2.0.2 > /etc/mrtg/10.2.0.2.cfg"
   }
   ######## GENERACIÓN DE LOS ARCHIVOS .CFG ########
   #  each($::mrtgserver::ip) |Integer $index, String $value|{
