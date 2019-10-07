991994e932947e05f74399203f0659bcae015a8b
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-10-07 15:25:00 -0400

    agregando paquetes para pke 7

--- profiles/pkeserver/manifests/common.pp
+++ profiles/pkeserver/manifests/common.pp
@@ -19,24 +19,31 @@ class pkeserver::common {
 
   exec{"cp_pxlinux":
     command     => 'cp /usr/lib/PXELINUX/pxelinux.0 /var/lib/tftpboot/',
+    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
   }
   exec{"cp_menu32":
     command     => 'cp /usr/lib/syslinux/modules/bios/menu.c32 /var/lib/tftpboot/',
+    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
   }
   exec{"cp_ldlinux.c32 ":
     command     => 'cp /usr/lib/syslinux/modules/bios/ldlinux.c32 /var/lib/tftpboot/',
+    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
   }
   exec{"cp_libmenu.c32":
     command     => 'cp /usr/lib/syslinux/modules/bios/libmenu.c32 /var/lib/tftpboot/',
+    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
   }
   exec{"cp_libutil.c32":
     command     => 'cp /usr/lib/syslinux/modules/bios/libutil.c32 /var/lib/tftpboot/',
+    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
   }
   exec{"cp_libcom32":
     command     => 'cp /usr/lib/syslinux/modules/bios/libcom32.c32 /var/lib/tftpboot/',
+    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
   }
   exec{"cp_vesamenu.c32":
     command     => 'cp /usr/lib/syslinux/modules/bios/vesamenu.c32 /var/lib/tftpboot/',
+    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
   }
 
 }
