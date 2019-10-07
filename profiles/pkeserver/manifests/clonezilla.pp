28212ea9b61795eb0c3bd69eb419258261a8f44e
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-10-07 15:10:57 -0400

    agregando paquetes para pke 4

--- profiles/pkeserver/manifests/clonezilla.pp
+++ profiles/pkeserver/manifests/clonezilla.pp
@@ -6,7 +6,7 @@ class pkeserver::clonezilla {
 
 
   exec{"wget_clonezilla":
-    command     => 'wget https://ftp.acc.umu.se/mirror/osdn.net/clonezilla/71563/clonezilla-live-2.6.3-7-amd64.iso',
+    command     => '/usr/bin/wget https://ftp.acc.umu.se/mirror/osdn.net/clonezilla/71563/clonezilla-live-2.6.3-7-amd64.iso',
     #refreshonly => true;
   }
 
