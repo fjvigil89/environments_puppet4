b391e14982d2cf183d51748af590cc39f6a8cf35
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-10-07 08:50:43 -0400

    edit indexmaker

--- profiles/mrtgserver/manifests/mrtg.pp
+++ profiles/mrtgserver/manifests/mrtg.pp
@@ -101,7 +101,7 @@ class mrtgserver::mrtg(){
 
   cron {'indexmaker_device':
     user    => $owner,
-    command => '/usr/bin/indexmaker --columns=2 --addhead="<H1 align= "center" > Multi Router Traffic Grapher <H1>" --title="Tr&aacute;fico de Enlaces UPR" /etc/mrtg/192.168.200.1.cfg 2> /dev/null& /etc/mrtg/10.2.1.1.cfg 2> /dev/null& /etc/mrtg/10.2.8.2.cfg 2> /dev/null& /etc/mrtg/10.2.0.2.cfg 2> /dev/null& > /var/www/mrtg/index.html',
+    command => '/usr/bin/indexmaker --columns=2 --addhead="<H1 align= "center" > Multi Router Traffic Grapher <H1>" --title="Tr&aacute;fico de Enlaces UPR" /etc/mrtg/192.168.200.1.cfg /etc/mrtg/10.2.1.1.cfg /etc/mrtg/10.2.8.2.cfg /etc/mrtg/10.2.0.2.cfg > /var/www/mrtg/index.html',
     minute  => '*/1'
   }
 
@@ -110,7 +110,7 @@ class mrtgserver::mrtg(){
     command => '/usr/bin/indexmaker --columns=2 --title="Temperatura y Humedad DATACENTER-UPR" /etc/mrtg/mrtg.sensor1.cfg /etc/mrtg/mrtg.sensor2.cfg > /var/www/mrtg/sensores/index.html',
     minute  => '*/1'
   }
-
+#2> /dev/null&
   cron {'indexmaker_videoconferencia':
     user    => $owner,
 	  command => '/usr/bin/indexmaker --columns=2 --title="Video Conferencia UPR" /etc/mrtg/10.2.8.4.cfg /var/www/mrtg/videoconferencia/index.html',
