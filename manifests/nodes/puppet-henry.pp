1158fd406fd3ac14393d504805bd3c64fb515c06
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-10-08 10:54:29 -0400

    basesys a puppetmaster de test

--- manifests/nodes/puppet-henry.pp
+++ manifests/nodes/puppet-henry.pp
@@ -1,3 +1,12 @@
 node 'henry-test.upr.edu.cu' {
   include puppetserver
+  package { 'lsb-release':
+          ensure => installed,
+  }~>
+  class { '::basesys':
+    uprinfo_usage   => 'Servidor Puppet Master',
+    application     => 'Puppet Master Prueba',
+    repos_enabled   => true,
+    mta_enabled     => false,
+  }
 }
