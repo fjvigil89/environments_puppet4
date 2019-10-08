de9dd9b62156f3dc23445a78bac379155073af29
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-10-08 08:48:54 -0400

    agregando paquetes para pke 13

--- profiles/pkeserver/manifests/clonezilla.pp
+++ profiles/pkeserver/manifests/clonezilla.pp
@@ -6,12 +6,12 @@ class pkeserver::clonezilla {
 
 
   exec{"wget_clonezilla":
-    command     => 'wget -P /var/lib/tftpboot/ https://ftp.acc.umu.se/mirror/osdn.net/clonezilla/71563/clonezilla-live-2.6.3-7-amd64.zip',
+    command     => 'wget https://ftp.acc.umu.se/mirror/osdn.net/clonezilla/71563/clonezilla-live-2.6.3-7-amd64.zip',
     onlyif => "test -f ./clonezilla-live-2.6.3-7-amd64.zip",
     path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
   }->
   exec{"unzip_clonezilla":
-    command     => 'unzip -f /var/lib/tftpboot/clonezilla-live-2.6.3-7-amd64.zip',
+    command     => 'unzip clonezilla-live-2.6.3-7-amd64.zip -d /var/lib/tftpboot/',
     path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
     onlyif => "test -f ./clonezilla-live-2.6.3-7-amd64.zip"
   }
