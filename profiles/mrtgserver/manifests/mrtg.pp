a3d5de9437c098a34514364d1a27320199a77440
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-10-14 15:33:17 -0400

    edit mrtg.pp

--- profiles/mrtgserver/manifests/mrtg.pp
+++ profiles/mrtgserver/manifests/mrtg.pp
@@ -1,5 +1,7 @@
 #Clase MRTG
 
+#Clase MRTG
+
 class mrtgserver::mrtg(){
   package { 'mrtg':
     ensure => installed,
@@ -52,6 +54,14 @@ class mrtgserver::mrtg(){
     source => 'puppet:///modules/mrtgserver/10.2.0.2.cfg',
   }
 
+  file { '/etc/mrtg/10.2.1.1.cfg':
+    ensure => 'file',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/mrtgserver/10.2.1.1.cfg',
+  }
+
   ####SENSORES####
 
   file { '/etc/mrtg/mrtg.sensor1.cfg':
@@ -74,7 +84,7 @@ class mrtgserver::mrtg(){
 
   cron {'indexmaker_device':
     user    => $owner,
-    command => 'indexmaker --columns=2 --addhead="<H1 align= "center" > Multi Router Traffic Grapher <H1>" --title="Tr&aacute;fico de Enlaces UPR" /etc/mrtg/192.168.200.1.cfg /etc/mrtg/10.2.0.2.cfg > /var/www/mrtg/index.html',
+    command => 'indexmaker --columns=2 --addhead="<H1 align= "center" > Multi Router Traffic Grapher <H1>" --title="Tr&aacute;fico de Enlaces UPR" /etc/mrtg/192.168.200.1.cfg /etc/mrtg/10.2.0.2.cfg /etc/mrtg/10.2.1.1.cfg > /var/www/mrtg/index.html',
     minute  => '*/1'
   }
 
@@ -83,8 +93,8 @@ class mrtgserver::mrtg(){
     command => 'indexmaker --columns=2 --title="Temperatura y Humedad DATACENTER-UPR" /etc/mrtg/mrtg.sensor1.cfg /etc/mrtg/mrtg.sensor2.cfg > /var/www/mrtg/sensores/index.html',
     minute  => '*/1'
   }
+#2> /dev/null&
 }
   #Agregar en /etc/cron.d/mrtg:
   #  */1 * * * *   root   env LANG=C /usr/bin/mrtg /etc/mrtg/10.2.0.2.cfg 2> /dev/null&
   #  */1 * * * *   root   env LANG=C /usr/bin/mrtg /etc/mrtg/192.168.200.1.cfg 2> /dev/null&
-  #  */1 * * * *   root   env LANG=C /usr/bin/mrtg /etc/mrtg/10.2.1.1.cfg 2> /dev/null&
