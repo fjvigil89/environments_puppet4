56212c165496caf5c15d3729e0b3b27d9e192a5a
Henry Fleitas <henry.fleitas@upr.edu.cu>
2019-10-14 14:46:01 -0400

    edit rol whois4

--- roles/whois/manifests/init.pp
+++ roles/whois/manifests/init.pp
@@ -6,22 +6,21 @@ class whois {
    group  => 'root',
    owner  => 'root',
    mode   => '0775',
-   }~>
+   }
 class{'::php_server':
   version      => '7.0',
   packages     => ['php7.0-mbstring','r10k','php7.0','php7.0-cli','php7.0-curl','php7.0-intl','php7.0-ldap','php7.0-mysql','php7.0-sybase','libapache2-mod-php7.0','php7.0-mcrypt','phpmyadmin','freetds-bin','freetds-common','php7.0-gd','php7.0-gmp','php7.0-snmp'],
   manage_repos => true,
-  }~>
-     vcsrepo { '/var/www/whois':
-       ensure   => latest,
-       provider => 'git',
-       remote   => 'origin',
-       source   => {
-         'origin' => 'git@gitlab.upr.edu.cu:dcenter/whois.git',
-       },
-       revision => 'master',
-    }
-	}
+  }
+  vcsrepo { '/var/www/whois':
+    ensure   => latest,
+    provider => 'git',
+    remote   => 'origin',
+    source   => {
+      'origin' => 'git@gitlab.upr.edu.cu:dcenter/whois.git',
+    },
+    revision => 'master',
+  }
 #  exec {"a2enmod_php7":
 #     command => '/usr/bin/sudo a2enmod php7.0',
 #     }
@@ -55,3 +54,4 @@ file { '/root/.ssh/id_rsa.pub':
    mode   => '0644',
    source => 'puppet:///modules/whois/keys/config',
    }
+}
