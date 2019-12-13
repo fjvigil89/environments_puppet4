74436fe5c17f0f14593086f3752136ffe0c4916f
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-12-13 13:47:02 -0500

    Creando node de noticias news.pp 2

--- manifests/nodes/news.pp
+++ manifests/nodes/news.pp
@@ -1,14 +1,4 @@
 node 'news.upr.edu.cu' {
-  package { 'lsb-release':
-          ensure => installed,
-  }
-  class { '::basesys':
-    uprinfo_usage  => 'servidor de noticias',
-    application    => 'News server',
-    repos_enabled  => false,
-    mta_enabled    => false,
-
-  }
 
   class{'::wh_php_apache':;}
   
