e520ac4b50c68c60ec385113c4a7a11c1ca76fe8
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2019-12-13 10:29:26 -0500

    Creando node de noticias news.pp 1

--- manifests/nodes/news.pp
+++ manifests/nodes/news.pp
@@ -10,5 +10,33 @@ node 'news.upr.edu.cu' {
 
   }
 
+  class{'::wh_php_apache':;}
+  
+  apache::vhost { 'sync.upr.edu.cu non-ssl':
+    servername    => 'sync.upr.edu.cu',
+    serveraliases => ['www.sync.upr.edu.cu'],
+    port          => '80',
+    docroot       => '/home/Sync-UPR/master/public/',
+    directories   => [ {
+      'path'           => '/home/Sync-UPR/master/public',
+      'options'        => ['Indexes','FollowSymLinks','MultiViews'],
+      'allow_override' => 'All',
+      'directoryindex' => 'index.php',
+      },],
+      #redirect_status  => 'permanent',
+      #redirect_dest    => 'https://sync.upr.edu.cu/',
+  }
+
+  exec{"a2enmod_php7":
+    command => '/usr/bin/sudo a2enmod php7.0',
+  }
+
+  exec{"service_apache2_restart":
+    command     => '/usr/bin/sudo service apache2 restart',
+    refreshonly => true;
+  }
+
+
+
 
 }
