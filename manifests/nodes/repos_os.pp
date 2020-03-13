ac569c36f5a77ff9a48936d8c705fdb34124f16c
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-03-13 07:46:41 -0400

    dns preinstall para los repos

--- manifests/nodes/repos_os.pp
+++ manifests/nodes/repos_os.pp
@@ -8,6 +8,7 @@ node 'repos.upr.edu.cu' {
     proxmox_enabled => false,
     repos_enabled   => true,
     mta_enabled     => false,
+    dns_preinstall  => true,
   }
   #aptly::mirror { 'debian_stable':
   #  location      => 'http://repos.uclv.edu.cu',
