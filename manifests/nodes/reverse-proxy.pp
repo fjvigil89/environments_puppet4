926b8e7e56e1ea9df0cb78ba0ccea0ba17d89333
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-04-07 14:55:05 -0400

    agregando moodle pre-grado y cambiandole la visibilidad para cuba

--- manifests/nodes/reverse-proxy.pp
+++ manifests/nodes/reverse-proxy.pp
@@ -84,9 +84,9 @@ node /^reverse-proxy\d+$/{
       'red_univ',#30
       'red_univ',#31
       'red_univ',#32
-      'red_univ',#33
+      'cuba',#33
       '*',#34
-      'red_univ',#35
+      'cuba',#35
     ],
 
   }
