35c5af83e1e167838e86b548a75a7ab2f2eecf27
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-04-15 08:34:15 -0400

    quitando letsencrypt a los reverse proxy 3

--- profiles/reverseproxy_server/manifests/server.pp
+++ profiles/reverseproxy_server/manifests/server.pp
@@ -39,7 +39,7 @@ class reverseproxy_server::server{
            nginx::resource::server { $::reverseproxy_server::server_name[$index]:
              listen_port        => $::reverseproxy_server::listen_port[$index],
              ssl_port           => $::reverseproxy_server::ssl_port[$index],
-             ssl                => true,
+            #ssl                => true,
             # ssl_cert           => "/etc/letsencrypt/live/${value}/fullchain.pem",
             # ssl_key            => "/etc/letsencrypt/live/${value}/privkey.pem",
              proxy              => "https://${value}",
@@ -54,11 +54,13 @@ class reverseproxy_server::server{
            nginx::resource::server { $::reverseproxy_server::server_name[$index]:
              listen_port    => $::reverseproxy_server::listen_port[$index],
              ssl_port       => $::reverseproxy_server::ssl_port[$index],
-             ssl            => true,
+             #ssl            => true,
              #ssl_cert       => "/etc/letsencrypt/live/${value}/fullchain.pem",
              #ssl_key        => "/etc/letsencrypt/live/${value}/privkey.pem",
              proxy          => "https://${value}",
              server_name    => ["${value}"],
+	     location_allow     => $allow,
+             location_deny      => $deny,
              proxy_set_header      => ['Host $host','X-Real-IP $remote_addr'],
 
            }
