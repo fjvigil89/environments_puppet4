e59a9df05eab31d938348706dde0b18f4aea48a1
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-04-15 11:34:17 -0400

    reverse proxy 24

--- profiles/reverseproxy_server/manifests/server.pp
+++ profiles/reverseproxy_server/manifests/server.pp
@@ -54,8 +54,7 @@ class reverseproxy_server::server{
            nginx::resource::server { $::reverseproxy_server::server_name[$index]:
              listen_port    	=> $::reverseproxy_server::listen_port[$index],
              ssl_port       	=> $::reverseproxy_server::ssl_port[$index],
-             ssl            	=> true,
-	     ssl_only		=> true,
+             #ssl            	=> true,
              #ssl_cert       	=> "/etc/letsencrypt/live/${value}/fullchain.pem",
              #ssl_key        	=> "/etc/letsencrypt/live/${value}/privkey.pem",
              proxy          	=> "https://${value}",
