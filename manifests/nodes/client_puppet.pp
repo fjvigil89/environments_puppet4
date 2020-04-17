918b62e7aeac84a551a5c46eecd671304bc891d7
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-04-16 21:45:37 -0400

    agreganfo reset del jupyther

--- manifests/nodes/client_puppet.pp
+++ manifests/nodes/client_puppet.pp
@@ -9,4 +9,12 @@ node 'client-puppet.upr.edu.cu'{
   }
 $pack = ['python3-pip','build-essential','libssl-dev', 'libffi-dev','python3-dev','python3-venv']
 ensure_packages($pack)
+
+  exec{"restart_server_jupyther":
+    command     => 'source /root/environments/my_env/bin/activate && jupyter notebook  stop && jupyter notebook --allow-root --ip=10.2.4.104 --no-browser  --NotebookApp.token='<none>',
+    refreshonly => true;
+  }
+
+
+
 }
