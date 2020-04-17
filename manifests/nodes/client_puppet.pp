a391629d9b0d9e83be680ca6ba46be742f452752
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-04-16 22:11:30 -0400

    agreganfo reset del jupyther 1

--- manifests/nodes/client_puppet.pp
+++ manifests/nodes/client_puppet.pp
@@ -17,8 +17,11 @@ ensure_packages($pack)
   exec{"start_server_jupyther":
 	path	 	=> "/bin:/sbin:/usr/bin:/usr/sbin",
     	command     	=> "source /root/environments/my_env/bin/activate && jupyter notebook --allow-root --ip=10.2.4.104 --no-browser  --NotebookApp.token='<none>'",
-    	refreshonly 	=> true;
   }
+notify{'jupyther':
+    message =>  'Ejecutar en el navegador http://10.2.4.104:8888/?token=... ',
+  }
+
 
 
 }
