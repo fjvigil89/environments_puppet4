30b41e1ab9069fe2f49ff1b4af92c104549dc86a
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-05-08 12:54:33 -0400

    cambiando host del freed del wh-bk

--- manifests/nodes/client_puppet.pp
+++ manifests/nodes/client_puppet.pp
@@ -10,17 +10,6 @@ node 'client-puppet.upr.edu.cu'{
 $pack = ['python3-pip','build-essential','libssl-dev', 'libffi-dev','python3-dev','python3-venv']
 ensure_packages($pack)
 
-  exec{"stop_server_jupyther":
-	path 		=> "/bin:/sbin:/usr/bin:/usr/sbin",
-    	command     	=> "source /root/environments/my_env/bin/activate && jupyter notebook  stop ",
-  }->
-  exec{"start_server_jupyther":
-	path	 	=> "/bin:/sbin:/usr/bin:/usr/sbin",
-    	command     	=> "source /root/environments/my_env/bin/activate && jupyter notebook --allow-root --ip=10.2.4.104 --no-browser  --NotebookApp.token='<none>'",
-  }~>
-notify{'jupyther':
-    message =>  'Ejecutar en el navegador http://10.2.4.104:8888/?token=... ',
-  }
 
 
 
