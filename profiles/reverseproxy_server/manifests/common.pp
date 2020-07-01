65e51015d8328368bd45cd66e3184d26ce53157a
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-07-01 10:44:20 -0400

    agregando mas tiempo de nginx inverso

--- profiles/reverseproxy_server/manifests/common.pp
+++ profiles/reverseproxy_server/manifests/common.pp
@@ -1,7 +1,10 @@
 class reverseproxy_server::common (){
    
   class {'nginx':
-     manage_repo => $::reverseproxy_server::manage_repo,
+     manage_repo           => $::reverseproxy_server::manage_repo,
+     proxy_connect_timeout => '180s',
+     proxy_read_timeout    => '180s',
+     proxy_send_timeout    => '180s',
   }
 
 }
