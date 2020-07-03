585308dc0087add0733b6e6f2b4567939276554d
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-07-02 22:03:42 -0400

    agregando configuracion de ceph como server

--- manifests/nodes/ceph.pp
+++ manifests/nodes/ceph.pp
@@ -35,16 +35,17 @@ node /^test-ceph\d+$/ {
     mta_enabled     => false,
     dmz             => true,
   }
+}
 
-  #exec{"sysctl":
-  #  command => '/usr/bin/sudo sysctl -w vm.max_map_count=262144',
-  #}
+node 'test-ceph1.upr.edu.cu' {
+  class {'ceph':
+    mon_hosts   => [ 'test-ceph1.upr.edu.cu', 'test-ceph2.upr.edu.cu', 'test-ceph3.upr.edu.cu' ]
+    release     => 'hammer',
+    cluster_net => '192.168.2.0/28',
+    public_net  => '10.2.2.0/24',
+  }
 
-  #class {'::filebeatserver':
-  #  log_type => "syslog",
-  #}
-  #class {'::metricbeatserver':
-  # modules  => ['system','ceph']
-  #}
-
+  class {'ceph::server::mon':
+    id => 1
+  }
 }
