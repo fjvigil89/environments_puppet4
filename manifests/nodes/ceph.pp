312e7431f896896a98ff868cc91c0f8c8060842f
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-07-08 14:14:31 -0400

    know host para basesys 1

--- manifests/nodes/ceph.pp
+++ manifests/nodes/ceph.pp
@@ -10,22 +10,7 @@ node /^ceph\d+$/ {
     mta_enabled     => false,
     dmz             => true,
   }
-
-}
-    $admin_key = 'AQCTg71RsNIHORAAW+O6FCMZWBjmVfMIPk3MhQ=='
-    $mon_key = 'AQDesGZSsC7KJBAAw+W/Z4eGSQGAIbxWjxjvfw=='
-    $bootstrap_osd_key = 'AQABsWZSgEDmJhAAkAGSOOAJwrMHrM5Pz5On1A=='
-    $fsid = '066F558C-6789-4A93-AAF1-5AF1BA01A3AD'
- node /^test-ceph\d+$/ {
-     class { '::basesys':
-      uprinfo_usage   => 'servidor Ceph',
-      application     => 'Debian Ceph',
-      proxmox_enabled => false,
-      repos_enabled   => false,
-      mta_enabled     => false,
-      dmz             => true,
-    }
-      class { 'ceph':
+  class { 'ceph':
         fsid                => $fsid,
         mon_initial_members => 'test-ceph1.upr.edu.cu,test-ceph2.upr.edu.cu,test-ceph3.upr.edu.cu',
         mon_host            => '10.2.2.240,10.2.2.241,10.2.2.242',
@@ -64,6 +49,22 @@ node /^ceph\d+$/ {
       #   keyring_path => '/var/lib/ceph/bootstrap-osds/ceph.keyring',
       #   secret       => $bootstrap_osd_key,
       #}
+
+
+}
+    $admin_key = 'AQCTg71RsNIHORAAW+O6FCMZWBjmVfMIPk3MhQ=='
+    $mon_key = 'AQDesGZSsC7KJBAAw+W/Z4eGSQGAIbxWjxjvfw=='
+    $bootstrap_osd_key = 'AQABsWZSgEDmJhAAkAGSOOAJwrMHrM5Pz5On1A=='
+    $fsid = '066F558C-6789-4A93-AAF1-5AF1BA01A3AD'
+ node /^test-ceph\d+$/ {
+     class { '::basesys':
+      uprinfo_usage   => 'servidor Ceph',
+      application     => 'Debian Ceph',
+      proxmox_enabled => false,
+      repos_enabled   => false,
+      mta_enabled     => false,
+      dmz             => true,
+    }
  }
 
 
