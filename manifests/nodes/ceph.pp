2bd7b1f9a45bb0337c897518c73a2175a29e7c9c
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-07-09 11:18:30 -0400

    ceph

--- manifests/nodes/ceph.pp
+++ manifests/nodes/ceph.pp
@@ -10,7 +10,15 @@ node /^ceph\d+$/ {
     mta_enabled     => false,
     dmz             => true,
   }
-  class { 'ceph':
+
+}
+    $admin_key = 'AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFR8x2v5r4arPvuCp+oPF2fTCZZMgWayP1Pvwnu32kdF0V77D20GaqlDA7JykwZDck5iORc/HL7xlG55x5vVYmw='
+    $mon_key = 'AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIXwMZ/b9V8vMgvsh9kjEeOY6F/j1rEgiRhRUi+OSVW/uUEHnLxfwaotbSuO1bulq372YgPJL1beOOgCJeT6M7w='
+    $bootstrap_osd_key = 'AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFR8x2v5r4arPvuCp+oPF2fTCZZMgWayP1Pvwnu32kdF0V77D20GaqlDA7JykwZDck5iORc/HL7xlG55x5vVYmw='
+    $fsid = '066F558C-6789-4A93-AAF1-5AF1BA01A3AD'
+ node /^test-ceph\d+$/ {
+
+   class { 'ceph':
         fsid                => $fsid,
         mon_initial_members => 'test-ceph1.upr.edu.cu,test-ceph2.upr.edu.cu,test-ceph3.upr.edu.cu',
         mon_host            => '10.2.2.240,10.2.2.241,10.2.2.242',
@@ -50,21 +58,6 @@ node /^ceph\d+$/ {
       #   secret       => $bootstrap_osd_key,
       #}
 
-
-}
-    $admin_key = 'AQCTg71RsNIHORAAW+O6FCMZWBjmVfMIPk3MhQ=='
-    $mon_key = 'AQDesGZSsC7KJBAAw+W/Z4eGSQGAIbxWjxjvfw=='
-    $bootstrap_osd_key = 'AQABsWZSgEDmJhAAkAGSOOAJwrMHrM5Pz5On1A=='
-    $fsid = '066F558C-6789-4A93-AAF1-5AF1BA01A3AD'
- node /^test-ceph\d+$/ {
-     class { '::basesys':
-      uprinfo_usage   => 'servidor Ceph',
-      application     => 'Debian Ceph',
-      proxmox_enabled => false,
-      repos_enabled   => false,
-      mta_enabled     => false,
-      dmz             => true,
-    }
  }
 
 
