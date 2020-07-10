3178668a39e7f555dd595ed646ecfcd7458dd7ab
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-07-10 12:40:22 -0400

    trabajando con ceph 1

--- manifests/nodes/ceph.pp
+++ manifests/nodes/ceph.pp
@@ -12,9 +12,14 @@ node /^ceph\d+$/ {
   }
 
 }
-    $admin_key = 'AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFR8x2v5r4arPvuCp+oPF2fTCZZMgWayP1Pvwnu32kdF0V77D20GaqlDA7JykwZDck5iORc/HL7xlG55x5vVYmw='
-    $mon_key = 'AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIXwMZ/b9V8vMgvsh9kjEeOY6F/j1rEgiRhRUi+OSVW/uUEHnLxfwaotbSuO1bulq372YgPJL1beOOgCJeT6M7w='
-    $bootstrap_osd_key = 'AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFR8x2v5r4arPvuCp+oPF2fTCZZMgWayP1Pvwnu32kdF0V77D20GaqlDA7JykwZDck5iORc/HL7xlG55x5vVYmw='
+#$admin_key = 'AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFR8x2v5r4arPvuCp+oPF2fTCZZMgWayP1Pvwnu32kdF0V77D20GaqlDA7JykwZDck5iORc/HL7xlG55x5vVYmw='
+#$mon_key = 'AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIXwMZ/b9V8vMgvsh9kjEeOY6F/j1rEgiRhRUi+OSVW/uUEHnLxfwaotbSuO1bulq372YgPJL1beOOgCJeT6M7w='
+#$bootstrap_osd_key = 'AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFR8x2v5r4arPvuCp+oPF2fTCZZMgWayP1Pvwnu32kdF0V77D20GaqlDA7JykwZDck5iORc/HL7xlG55x5vVYmw='
+#$fsid = '066F558C-6789-4A93-AAF1-5AF1BA01A3AD'
+
+    $admin_key = 'AQCTg71RsNIHORAAW+O6FCMZWBjmVfMIPk3MhQ=='
+    $mon_key = 'AQDesGZSsC7KJBAAw+W/Z4eGSQGAIbxWjxjvfw=='
+    $bootstrap_osd_key = 'AQABsWZSgEDmJhAAkAGSOOAJwrMHrM5Pz5On1A=='
     $fsid = '066F558C-6789-4A93-AAF1-5AF1BA01A3AD'
  node /^test-ceph\d+$/ {
 
