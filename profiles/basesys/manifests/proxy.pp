7258f2a99f27b1acfd9f455d1766a97ad3f92fc9
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-07-11 12:57:01 -0400

    agregando proxy para los servers 4

--- profiles/basesys/manifests/proxy.pp
+++ profiles/basesys/manifests/proxy.pp
@@ -6,16 +6,31 @@
 class basesys::proxy (
   ){
 
+  $proxy_url    = $::basesys::proxy_url
+  $proxy_port   = $::basesys::proxy_port
+  $proxy_user   = $::basesys::proxy_user
+  $proxy_pass   = $::basesys::proxy_pass
+
   if($::basesys::proxy_enabled)
   {
-
-  file{'/etc/apt/apt.conf.d/80proxy':
-    ensure => 'file',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/basesys/80proxy',
-  }
+   
+    exec{"http_proxy":
+     command => "/usr/bin/sudo export http_proxy=${proxy_url}:${proxy_port}"
+    }
+    exec{"https_proxy":
+     command => "/usr/bin/sudo export https_proxy=${proxy_url}:${proxy_port}"
+    }
+    exec{"ftp_proxy":
+     command => "/usr/bin/sudo export ftp_proxy==${proxy_url}:${proxy_port}"
+    }
+    
+    file{'/etc/apt/apt.conf.d/80proxy':
+     ensure => absent,
+     owner  => 'root',
+     group  => 'root',
+     mode   => '0644',
+     source => 'puppet:///modules/basesys/80proxy',
+    }
 
 
   }
