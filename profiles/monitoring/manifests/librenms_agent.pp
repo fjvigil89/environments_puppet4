7618b3fc5ddc5d37a5e2d40fb046d3e4b6ce78c9
Henry Fleitas Mezquia <henry.fleitas@upr.edu.cu>
2020-07-20 13:46:30 -0400

    test comunidad

--- profiles/monitoring/manifests/librenms_agent.pp
+++ profiles/monitoring/manifests/librenms_agent.pp
@@ -9,7 +9,7 @@ class monitoring::librenms_agent {
 class { 'snmpd':
   package       => true,
   service       => true,
-  allowed_hosts => [ 'localhost','10.2.4.200/32','10.2.9.0/24','10.2.4.0/23' ],
+  allowed_hosts => [ 'localhost','10.2.0.0/21' ],
   community     => 'UPRadmin4all',
   syslocation   => 'UPR, Nodo Central',
   syscontact    => 'di@upr.edu.cu'
