32922a198eae81ccaa1287f2c368c26c11bd16fb
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-08-27 17:57:46 -0400

    cambios en basesys para la intranet

--- manifests/nodes/intranet.pp
+++ manifests/nodes/intranet.pp
@@ -1,10 +1,4 @@
 node 'intranet.upr.edu.cu' {  
-  class { '::basesys':
-    uprinfo_usage  => 'servidor intranet',
-    application    => 'Intranet',
-    puppet_enabled => false,
-    mta_enabled    => false,
-  }
 
   class{'::wh_php_apache':;}
 
