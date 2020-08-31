681f59f7d85002e52940d24af7e70db799ea5413
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-08-31 11:55:42 -0400

    agregando del manifest de los devops

---
+++ manifests/nodes/devops.pp
@@ -0,0 +1,18 @@
+node /^devops\d+$/  {  
+  include git
+  package { 'lsb-release':
+          ensure => installed,
+  }~>
+  class { '::basesys':
+    uprinfo_usage   => 'servidor de Servicios Devops',
+    application     => 'Proxmox Servicios',
+    proxmox_enabled => true,
+    repos_enabled   => false,
+    mta_enabled     => false,
+    dmz             => true,
+  }
+  exec{"sysctl":
+    command => '/usr/bin/sudo sysctl -w vm.max_map_count=262144',
+  }
+
+}
