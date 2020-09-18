b043a53c5986eaa975c80d2cd261103dcbcb70cf
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-09-18 16:06:32 -0400

    basesys para catalogos por default

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,6 +1,15 @@
 node default {
     
   notify{'default_node_inclusion':
-    message =>  'Este nodo no tiene catalogos.',
+    message =>  'Este nodo no tiene manifest.',
+  }
+class { '::basesys':
+    uprinfo_usage   => 'Default',
+    application     => 'CT y MV',
+    proxmox_enabled => false,
+    repos_enabled   => false,
+    mta_enabled     => false,
+    dmz             => false,
+    puppet_enabled  => true,
   }
 }
