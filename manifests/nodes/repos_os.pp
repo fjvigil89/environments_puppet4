c0f505290e7b0e50a366fed199c9c47c7edbaccc
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-09-21 12:05:44 -0400

    agregando r10k para server repo

--- manifests/nodes/repos_os.pp
+++ manifests/nodes/repos_os.pp
@@ -14,7 +14,7 @@ node 'repos.upr.edu.cu' {
   class {'::r10kserver' :
      r10k_basedir    => "/root/repos",
      #cachedir        => "/var/cache/r10k",
-     configfile      => "/etc/r10k.yaml",
+     configfile      => "/root/r10k.yaml",
      remote          => "git@gitlab.upr.edu.cu:dcenter/repos.git",
   }
 }
