351a06013f7da1f1dc442687037880d9cf3989bc
Yohandys Martinez Navarro <ymtnez@upr.edu.cu>
2020-09-24 15:54:37 -0400

    Quitando mysql del final de file

--- manifests/nodes/news.pp
+++ manifests/nodes/news.pp
@@ -54,12 +54,12 @@ node 'news.upr.edu.cu' {
   } 
 
 
-  class { '::mysql::server':
-    root_password           => 'news',
-    remove_default_accounts => false,
-    restart                 => true,
-    override_options        => $override_options
-  }
+#  class { '::mysql::server':
+#    root_password           => 'news',
+#    remove_default_accounts => false,
+#    restart                 => true,
+#    override_options        => $override_options
+#  }
 
 
 }
