8b2375116c611f007c4e0204b2c7f70a7dcb8e7f
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-09-25 14:29:31 -0400

    cambiando id de terraform

--- manifests/nodes/noc.pp
+++ manifests/nodes/noc.pp
@@ -12,9 +12,9 @@ node /^noc\d+$/ {
     dmz             => true,
   }
 
-  #exec{"sysctl":
-  #  command => '/usr/bin/sudo sysctl -w vm.max_map_count=262144',
-  #}
+  exec{"sysctl":
+    command => '/usr/bin/sudo sysctl -w vm.max_map_count=262144',
+  }
 
   #class {'::filebeatserver':
   #  log_type => "syslog",
