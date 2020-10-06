0500e12c680dbf03a39e06ae4e56aa5990b4c481
Henry Fleitas Mezquia <henry.fleitas@upr.edu.cu>
2020-10-06 09:03:08 -0400

    edit init2

--- profiles/librenmserver/manifests/init.pp
+++ profiles/librenmserver/manifests/init.pp
@@ -1,14 +1,6 @@
 # Class: librenmserver
 # ===========================
 #
-class librenmserver
-(
-  String $php_timezone = 'America/Havana',
-)
-class { '::librenms':
-  php_timezone => $php_timezone,
-
-class { '::apache2::monit':
-  monitor_email => 'henry.fleitas@upr.edu.cu',
-  }
+class librenmserver{
+()
 }
