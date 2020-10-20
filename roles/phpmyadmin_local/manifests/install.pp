ab5cbf00753cca863f6782bbee8dcdb1176d76d9
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-10-20 12:14:17 -0400

    agregando phpmyadmin tf-noticia 9

--- roles/phpmyadmin_local/manifests/install.pp
+++ roles/phpmyadmin_local/manifests/install.pp
@@ -12,7 +12,7 @@ define phpmyadmin_local::install($version = $title, $installdir)
 			ensure => directory,
 			owner  => 'root',
 			group  => 'root',
-			mode   => 0755,
+			mode   => '0755',
 		}
 	}
 	# Creating source directory
