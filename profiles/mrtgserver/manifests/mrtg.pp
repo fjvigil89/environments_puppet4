1e18a92ec19813cd7dc960778448b064e6dc7fea
Henry Fleitas Mezquia <henry.fleitas@upr.edu.cu>
2020-11-05 20:39:00 -0500

    edit index mrtg

--- profiles/mrtgserver/manifests/mrtg.pp
+++ profiles/mrtgserver/manifests/mrtg.pp
@@ -62,12 +62,12 @@ class mrtgserver::mrtg(){
     source => 'puppet:///modules/mrtgserver/10.2.0.1.cfg',
   }
 
-  file { '/etc/mrtg/10.2.0.250.cfg':
+  file { '/etc/mrtg/junos.cfg':
     ensure => 'file',
     owner  => 'root',
     group  => 'root',
     mode   => '0644',
-    source => 'puppet:///modules/mrtgserver/10.2.0.250.cfg',
+    source => 'puppet:///modules/mrtgserver/source.cfg',
   }
 
   ####SENSORES####
@@ -92,7 +92,7 @@ class mrtgserver::mrtg(){
 
   cron {'indexmaker_device':
     user    => $owner,
-    command => 'indexmaker --columns=2 --addhead="<H1 align= "center" > Multi Router Traffic Grapher <H1>" --title="Tr&aacute;fico de Enlaces UPR" /etc/mrtg/10.2.0.1.cfg /etc/mrtg/10.2.0.250.cfg /etc/mrtg/10.2.0.2.cfg> /var/www/mrtg/index.html',
+    command => 'indexmaker --columns=2 --addhead="<H1 align= "center" > Multi Router Traffic Grapher <H1>" --title="Tr&aacute;fico de Enlaces UPR" /etc/mrtg/junos.cfg /etc/mrtg/10.2.0.2.cfg> /var/www/mrtg/index.html',
     minute  => '*/1'
   }
 
