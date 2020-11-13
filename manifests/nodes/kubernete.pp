6888152fd5d3f42bb7a8f67e6fdca5bc79492f86
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2020-11-13 10:10:37 -0500

    agregando puppetlab-kubernete al puppetfile

--- manifests/nodes/kubernete.pp
+++ manifests/nodes/kubernete.pp
@@ -1,16 +1,3 @@
-class basesys {
-  class { '::basesys':
-    uprinfo_usage   => 'servidor Ceph',
-    application     => 'Debian Ceph',
-    proxmox_enabled => false,
-    repos_enabled   => false,
-    mta_enabled     => false,
-    dmz             => false,
-    proxy_enabled   => true,
-    proxy_url       => 'http://proxy-tor.upr.edu.cu',
-  }
-
-}
 node 'kubernete-controller.upr.edu.cu'{  
   class {'kubernetes':
     controller => true,
