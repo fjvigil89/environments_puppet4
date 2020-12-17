bb4b5bcf051e06be3f432d95d13ef3b73fb18dba
Henry Fleitas Mezquia <henry.fleitas@upr.edu.cu>
2020-12-17 10:45:31 -0500

    edit repo puppetserver

--- profiles/puppetserver/manifests/init.pp
+++ profiles/puppetserver/manifests/init.pp
@@ -7,7 +7,7 @@
 class puppetserver {
 ::apt::source { 'puppetlabs':
   location => 'http://apt.puppetlabs.com',
-  repos    => 'main',
+  repos    => 'puppet6',
   key      => {
     'id'     => '6F6B15509CF8E59E6E469F327F438280EF8D349F',
     'server' => 'pgp.mit.edu',
