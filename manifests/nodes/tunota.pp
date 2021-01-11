7c28f99ef870ae073df5fb6e3396cdfb7e48c0f2
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2021-01-11 13:06:01 -0500

    cambio de enotas por tunota

--- manifests/nodes/tunota.pp
+++ manifests/nodes/tunota.pp
@@ -1,4 +1,4 @@
-node 'enotas.upr.edu.cu' {
+node 'tunota.upr.edu.cu' {
 
   class { '::basesys':
     uprinfo_usage  => 'servidor enotas',
@@ -30,14 +30,14 @@ node 'enotas.upr.edu.cu' {
     mode   => '0644',
     source => 'puppet:///modules/reposserver/ssh_keys/config',
   }
-  file { '/home/eNotas':
+  file { '/home/tunota':
        ensure  => directory,
        group   => 'root',
        owner   => 'root',
        mode    => '0775',
      }
   
-  vcsrepo { '/home/eNotas':
+  vcsrepo { '/home/tunota':
    ensure   => latest,
    provider => git,
    remote   => 'origin',
@@ -48,11 +48,11 @@ node 'enotas.upr.edu.cu' {
   }
 
   class { 'apache': }
-  apache::vhost { 'enotas.upr.edu.cu non-ssl':
-    servername                  => 'enotas.upr.edu.cu',
-    serveraliases               => ['www.enotas.upr.edu.cu'],
+  apache::vhost { 'tunota.upr.edu.cu non-ssl':
+    servername                  => 'tunota.upr.edu.cu',
+    serveraliases               => ['www.tunota.upr.edu.cu'],
     port                        => '80',
-    docroot                     => '/home/eNotas',
+    docroot                     => '/home/tunota',
     wsgi_application_group      => '%{GLOBAL}',
     wsgi_daemon_process         => 'wsgi',
     wsgi_daemon_process_options => {
@@ -60,13 +60,13 @@ node 'enotas.upr.edu.cu' {
       threads                   => '15',
       display-name              => '%{GROUP}',
     },
-    wsgi_import_script          => '/home/eNotas/',
+    wsgi_import_script          => '/home/tunota/',
     wsgi_import_script_options  => {
       process-group             => 'wsgi',
       application-group         => '%{GLOBAL}',
     },
     wsgi_process_group          => 'wsgi',
-    wsgi_script_aliases         => { '/' => '/home/eNotas/' },
+    wsgi_script_aliases         => { '/' => '/home/tunota/' },
   }
 
 }
