24e193d8cd4b240c2c5d3b927e54789066646fdd
Henry Fleitas Mezquia <henry.fleitas@upr.edu.cu>
2021-01-12 09:14:17 -0500

    add moodle estudiantes a proxy inverso

--- manifests/nodes/reverse-proxy.pp
+++ manifests/nodes/reverse-proxy.pp
@@ -56,8 +56,8 @@ class { 'reverse_proxy_server':
       'gicacovid.upr.edu.cu',#38
       'syncassets.upr.edu.cu',#39
       'cneuro.upr.edu.cu',#40
-      'estudiantes.upr.edu.cu'#41
-      'moodle.upr.edu.cu'#42
+      'estudiantes.upr.edu.cu',#41
+      'moodle.upr.edu.cu',#42
     ],
     #                  1  -2 - 3 - 4 - 6- 7 - 8 - 9--10-11-12-13-14-15-16-17-18-19-20-21-22-23-24-25-26-27,28, 29, 30, 31, 32, 33, 34, 35   36, 37, 38, 39, 40, 41, 42
     listen_port    => [80,80 ,80 , 80, 80,80 ,443,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80, 80, 80, 80, 80, 80, 443, 80, 80, 80, 80, 80, 80, 80, 80],
