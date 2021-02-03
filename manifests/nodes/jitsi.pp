dc2439da0973cd048f4e2e86a5f913c51a0da46d
Henry Fleitas Mezquia <henry.fleitas@upr.edu.cu>
2021-02-03 13:47:51 -0500

    add jitsi2

--- manifests/nodes/jitsi.pp
+++ manifests/nodes/jitsi.pp
@@ -2,9 +2,9 @@ node 'jitsi.upr.edu.cu' {
 class { 'jitsimeet':
     jitsi_domain         => 'jitsi.upr.edu.cu',
     manage_certs         => true,
-    jitsi_vhost_ssl_key  => '/etc/letsencrypt/live/jitsi.upr.edu.cu/privkey.pem'
-    jitsi_vhost_ssl_cert => '/etc/letsencrypt/live/jitsi.upr.edu.cu/cert.pem'
-    auth_vhost_ssl_key   => '/etc/letsencrypt/live/auth.jitsi.upr.edu.cu/privkey.pem'
+    jitsi_vhost_ssl_key  => '/etc/letsencrypt/live/jitsi.upr.edu.cu/privkey.pem',
+    jitsi_vhost_ssl_cert => '/etc/letsencrypt/live/jitsi.upr.edu.cu/cert.pem',
+    auth_vhost_ssl_key   => '/etc/letsencrypt/live/auth.jitsi.upr.edu.cu/privkey.pem',
     auth_vhost_ssl_cert  => '/etc/letsencrypt/live/auth.jitsi.upr.edu.cu/cert.pem'
     jvb_secret           => 'mysupersecretstring',
     focus_secret         => 'anothersupersecretstring',
