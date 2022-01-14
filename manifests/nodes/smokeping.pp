644bd2860fd8ef5dd1f22c5a3fc78ccbdfb70b5a
Henry Fleitas Mezquia <henry.fleitas@upr.edu.cu>
2022-01-14 08:43:25 -0500

    add ip CUM smokeping

--- manifests/nodes/smokeping.pp
+++ manifests/nodes/smokeping.pp
@@ -3,7 +3,7 @@ node 'smokeping.upr.edu.cu'{
   include smokeprodserver
   class {'smokeserver':
     target           => ['Routers','L3','PAP','FCP','FCF','Docente','CUM','Vinales','Palacios','San_Luis','Minas','Guane','Mantua','La_Palma','Sandino','Consolacion','UD','Camilo','Macurije','Guanahacabibes','Soroa'],
-    menu             => ['Routers','Switch L3 Nodo Central','Router PAP','Router FCP','Router FCF','Router Docente','CUM','Viñales 49','Los Palacios','San Luis','Minas','Guane','Mantua','La Palma','Sandino','Consolacion','Unidades Docentes','Camilo Cienfuegos','Macurije','Guanahacabibes','Soroa'],
+    menu             => ['Routers','Switch L3 Nodo Central','Router PAP','Router FCP','Router FCF','Router Docente','CUM','Viñales .49','Los Palacios .129','San Luis .177','Minas .33','Guane .209','Mantua .17','La Palma .65','Sandino .1','Consolacion .145','Unidades Docentes','Camilo Cienfuegos .81','Macurije .97','Guanahacabibes .113','Soroa .161'],
     hierarchy_level  => [1,2,2,2,2,2,1,2,2,2,2,2,2,2,2,2,1,2,2,2,2],
     hierarchy_parent => ['Routers','Routers','Routers','Routers','Routers','Routers','CUM','CUM','CUM','CUM','CUM','CUM','CUM','CUM','CUM','CUM','UD','UD','UD','UD','UD'],
     host             => ['','10.2.1.1','10.2.1.2','10.2.0.161','10.2.0.200','10.2.0.128','','10.2.20.49','10.2.20.119','10.2.20.177','10.2.20.33','10.2.20.209','10.2.20.17','10.2.20.165','10.2.20.1','10.2.20.145','','10.2.21.1','10.2.20.97','10.2.20.113','10.2.20.161'],
